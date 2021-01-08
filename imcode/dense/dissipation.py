7c71a27849154190acfcaa02167f808a69482976
Michael Sonner <sonnerm@gmail.com>
2021-01-08 16:28:28 +0100

    added dissipation stubs

---
+++ imcode/dense/dissipation.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def dephase_operator(t,gamma):
+    pass
+def depolarize_operator(t,delta):
+    pass
+def ising_hr_dephase(t,J,g,gamma):
+    pass
+
+def ising_Jr_dephase(t,g,h,gamma):
+    pass
