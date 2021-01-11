047ac3b84fcd0e1be3f456af30a3365dc8d35b27
Michael Sonner <sonnerm@gmail.com>
2021-01-11 12:27:49 +0100

    started implementing observables in mps

--- imcode/dense/dissipation.py
+++ imcode/dense/dissipation.py
@@ -4,8 +4,11 @@ def dephase_operator(t,gamma):
     pass
 def depolarize_operator(t,delta):
     pass
-def ising_hr_dephase(t,J,g,gamma):
+def ising_hr_dephase_T(t,J,g,gamma):
     pass
-
-def ising_Jr_dephase(t,g,h,gamma):
+def ising_hr_dephase_Tp(t,J,g,gamma):
+    pass
+def ising_Jr_dephase_T(t,g,h,gamma):
+    pass
+def ising_Jhr_dephase_T(t,g,gamma):
     pass
--- imcode/mps/obs.py
+++ imcode/mps/obs.py
@@ -1,51 +1,76 @@
 import tenpy
-def folded_temporal_entropy(mps):
-    return mps.bond_entropy()
+from .utils import apply,multiply_mpos
+from tenpy.networks.mpo import MPOEnvironment,MPO
+from tenpy.linalg.charges import LegCharge
+def boundary_obs(im,obs):
+    im.overlap(obs)
+def embedded_obs(left_im,obs_mpo,right_im):
+    return MPOEnvironment(left_im,obs_mpo,right_im).full_contraction(0)*left_im.norm*right_im.norm
+def zz_operator(t):
+    sites=[BlipSite() for _ in range(t+1)]
+    leg_t=LegCharge.from_trivial(0)
+    leg_p=sites[0].leg
+    Ida=np.einsum("ab,cd->abcd",np.eye(1),np.eye(4))
+    Za=np.einsum("ab,cd->abcd",np.eye(1),np.diag([1.0,-1.0,0.0,0.0]))
+    Z=npc.Array.from_ndarray(Za,[leg_t,leg_t,leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) # make sure
+    Id=npc.Array.from_ndarray(Ida,[leg_t,leg_t,leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) # make sure
+    return MPO(sites,[Z]+[Id]*(T-1)+[Z])
 
-def get_lohschmidt(mps):
+def zz_state(t):
+    pass
+def embedded_czz(im,lop):
+    op=multiply_mpos([lop,zz_operator])
+    return embedded_obs(im,op,im)
+def boundary_czz(im,lop):
+    st=zz_state(t)
+    apply(lop,st)
+    return boundary_obs(im,st)
+def embedded_norm(im,lop):
+    return embedded_obs(im,lop,im)
+def boundary_norm(im,lop):
+    st=open_boundary_im(t)
+    apply(lop,st)
+    return boundary_obs(im,lop,im)
+
+def magsec_proj(T,M,branch="fw"):
+    chinfo=ChargeInfo([1],[branch])
+    if branch=="fw":
+        leg_p=LegCharge.from_qflat(chinfo,[1,0,1,0])
+    else:
+        leg_p=LegCharge.from_qflat(chinfo,[1,0,0,1])
+    leg_t=LegCharge.from_trivial(1,chinfo)
+    leg_pt=LegCharge.from_trivial(4,chinfo)
+    leg_rt=LegCharge.from_qflat(chinfo,[M])
+    legr=[(max(0,i-T+M+2),min(i+1,M)+1) for i in range(T-2)]
+    legs=[leg_t]+[LegCharge.from_qflat(chinfo,list(range(*x))) for x in legr]+[leg_rt]
+    Id_a=np.eye(4)
+    Id_m=[get_proj(Id_a,ll,lr.conj(),leg_pt,leg_p).drop_charge() for ll,lr in zip(legs[:-1],legs[1:])]
+    Id_l=npc.Array.from_ndarray(np.einsum("ab,cd->abcd",np.eye(1),np.eye(4)),[leg_t,leg_t.conj(),leg_pt,leg_pt.conj()],labels=["wL","wR","p","p*"],dtype=complex)
+    Id_r=npc.Array.from_ndarray(np.einsum("ab,cd->abcd",np.eye(1),np.eye(4)),[leg_rt,leg_t.conj(),leg_pt,leg_pt.conj()],labels=["wL","wR","p","p*"],dtype=complex,qtotal=[M])
+    return MPO([BlipSite(False) for t in range(T+1)],[Id_l.drop_charge()]+Id_m+[Id_r.drop_charge()])
+def embedded_magsec(im,s,lop):
+    pass
+def boundary_magsec(im,s,lop):
+    pass
+def lohschmidt(im):
     lstate= [[1,0,0,0]]+[[0,0,1,0]]*(mps.L-2)+[[1,0,0,0]]
     lmps=MPS.from_product_state(mps.sites,lstate)
-    return mps.overlap(lmps)
-def get_blip_dist1(mps):
-    if mps.L<4:
-        return 0.0
-    lstate= [[1,0,0,0],[0,0,1,0]]+[[1,1,0,0]]*(mps.L-4)+[[0,0,0,1],[1,0,0,0]]
-    lmps=MPS.from_product_state(mps.sites,lstate)
-    return mps.overlap(lmps)
-def get_blip_dist2(mps):
-    if mps.L<6:
-        return 0.0
-    lstate= [[1,0,0,0],[0,0,1,0],[0,0,0,1]]+[[1,1,0,0]]*(mps.L-6)+[[0,0,1,0],[0,0,0,1],[1,0,0,0]]
-    lmps=MPS.from_product_state(mps.sites,lstate)
-    return mps.overlap(lmps)
-def get_czz(mps,W_mpo,h_mpo):
-    mpc=mps.copy()
-    W_mpo.apply_naively(mpc)
-    h_mpo.apply_naively(mpc)
-    leg_p=mps.sites[0].leg
-    zop=npc.Array.from_ndarray(np.diag([1,-1,1,1]),[leg_p,leg_p.conj()],labels=["p","p*"])
-    mpc.apply_local_op(0,zop,True)
-    mpc.apply_local_op(mps.L-1,zop,True)
-    return mps.overlap(mpc)/2
+    return boundary_obs(im,lmps)
 
-def get_norm(mps,W_mpo,h_mpo):
-    mpc=mps.copy()
-    W_mpo.apply_naively(mpc)
-    h_mpo.apply_naively(mpc)
-    leg_p=mps.sites[0].leg
-    return mps.overlap(mpc)/2
-def get_czz_norm(mps,W_mpo,h_mpo):
-    mpc=mps.copy()
-    W_mpo.apply_naively(mpc)
-    h_mpo.IdL[0]=0
-    h_mpo.IdR[mps.L]=0
-    norm=MPOEnvironment(mps,h_mpo,mpc).full_contraction(0)*mps.norm*mpc.norm
-    leg_p=mps.sites[0].leg
-    zop=npc.Array.from_ndarray(np.diag([1,-1,1,1]),[leg_p,leg_p.conj()],labels=["p","p*"])
-    mpc.apply_local_op(0,zop,True)
-    mpc.apply_local_op(mps.L-1,zop,True)
-    czz=MPOEnvironment(mps,h_mpo,mpc).full_contraction(0)*mps.norm*mpc.norm
-    return czz,norm
+
+# def get_blip_dist1(mps):
+#     if mps.L<4:
+#         return 0.0
+#     lstate= [[1,0,0,0],[0,0,1,0]]+[[1,1,0,0]]*(mps.L-4)+[[0,0,0,1],[1,0,0,0]]
+#     lmps=MPS.from_product_state(mps.sites,lstate)
+#     return mps.overlap(lmps)
+# def get_blip_dist2(mps):
+#     if mps.L<6:
+#         return 0.0
+#     lstate= [[1,0,0,0],[0,0,1,0],[0,0,0,1]]+[[1,1,0,0]]*(mps.L-6)+[[0,0,1,0],[0,0,0,1],[1,0,0,0]]
+#     lmps=MPS.from_product_state(mps.sites,lstate)
+#     return mps.overlap(lmps)
+
 
 def get_magsec(mps,W_mpo,options):
     T=mps.L-1
@@ -82,15 +107,6 @@ def get_magsec(mps,W_mpo,options):
         msud.append(MPOEnvironment(mpc,msp,mpcud).full_contraction(0)*normud)
     return msuu,msud#,normuu,normud,normuue,normude
 
-def get_zz_mpo(sites):
-    T=len(sites)-1
-    leg_t=tenpy.linalg.charges.LegCharge.from_qflat(chinfo,[0])
-    leg_p=sites[0].leg
-    Ida=np.einsum("ab,cd->abcd",np.eye(1),np.eye(4))
-    Za=np.einsum("ab,cd->abcd",np.eye(1),np.diag([1.0,-1.0,0.0,0.0]))
-    Z=npc.Array.from_ndarray(Za,[leg_t,leg_t,leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) # make sure
-    Id=npc.Array.from_ndarray(Ida,[leg_t,leg_t,leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) # make sure
-    return MPO(sites,[Z]+[Id]*(T-1)+[Z])
 
 def magsec_proj(T,M,branch="fw"):
     chinfo=ChargeInfo([1],[branch])
@@ -109,10 +125,6 @@ def magsec_proj(T,M,branch="fw"):
     Id_r=npc.Array.from_ndarray(np.einsum("ab,cd->abcd",np.eye(1),np.eye(4)),[leg_rt,leg_t.conj(),leg_pt,leg_pt.conj()],labels=["wL","wR","p","p*"],dtype=complex,qtotal=[M])
     return MPO([BlipSite(False) for t in range(T+1)],[Id_l.drop_charge()]+Id_m+[Id_r.drop_charge()])
 
-def get_czz_oneside(mps):
-    lstate= [[1,-1,0,0]]+[[1,1,1,1]]*(mps.L-2)+[[1,-1,0,0]]
-    lmps=MPS.from_product_state(mps.sites,lstate)
-    return mps.overlap(lmps)/2
 def get_blip_average_uu(mps):
     lstate= [[1,0,0,0]]+[[0,0,1,1]]*(mps.L-2)+[[1,0,0,0]]
     lmps=MPS.from_product_state(mps.sites,lstate)
