69cdcf2abc75fc6cd94b4bdc2c410f45583854af
Michael Sonner <sonnerm@gmail.com>
2021-01-20 12:58:34 +0100

    free ising_H works

--- imcode/free/ising.py
+++ imcode/free/ising.py
@@ -2,22 +2,25 @@ import numpy as np
 import scipy.linalg as la
 def ising_H(J,g):
     if len(J)<len(g):
-        J=list(J)+[0.0]
+        J=np.array(list(J)+[0.0])
+    J=np.array(J)
+    g=np.array(g)
     diage=np.zeros(len(g)+len(J)-1,dtype=np.common_type(J,g))
     diage[::2]=g
     diage[1::2]=J[:-1]
-    rete=0.5j*(np.diag(diage,1)+np.diag(diage,-1))
-    rete[0,-1]=0.5j*J[-1]
-    rete[-1,0]=0.5j*J[-1]
-    reto=0.5j*(np.diag(diage,1)+np.diag(diage,-1))
-    reto[0,-1]=-0.5j*J[-1]
-    reto[-1,0]=-0.5j*J[-1]
-    return (rete,reto)
+    rete=0.5j*(np.diag(diage,1)-np.diag(diage,-1))
+    if len(g)>1:
+        rete[0,-1]=-0.5j*J[-1]
+        rete[-1,0]=0.5j*J[-1]
+    reto=0.5j*(np.diag(diage,1)-np.diag(diage,-1))
+    if len(g)>1:
+        reto[0,-1]=0.5j*J[-1]
+        reto[-1,0]=-0.5j*J[-1]
+    return (rete*4,reto*4)
 
 def ising_F(J,g):
     if len(J)<len(g):
         J=list(J)+[0.0]
-
     diagg=np.zeros(len(g)+len(J)-1,dtype=np.common_type(J,g))
     diagg[::2]=g
     U1h=np.diag(diagg,1)+np.diag(diagg,-1)
