4d999c504e0f213cdb5c77c89a6f3da97a29f043
Michael Sonner <sonnerm@gmail.com>
2021-02-08 15:41:23 +0100

    added mps.ising_H

--- imcode/mps/ising.py
+++ imcode/mps/ising.py
@@ -1,4 +1,51 @@
-def ising_H():
-    pass
-def ising_F():
-    pass
+import numpy as np
+from .flat import FlatSite
+from tenpy.linalg.charges import LegCharge
+from tenpy.networks.mpo import MPO
+import tenpy.linalg.np_conserved as npc
+from ..dense import SZ,ID,SX
+ZE=np.zeros_like(ID)
+def ising_H(J,g,h):
+    L=len(h) # maybe change to explicit length?
+    sites=[FlatSite() for _ in range(L)]
+    J=np.array(J)
+    g=np.array(g)
+    h=np.array(h)
+    leg_p=LegCharge.from_trivial(2)
+    leg_m=LegCharge.from_trivial(3)
+    leg_b=LegCharge.from_trivial(1)
+    if L==1:
+        Wsn=np.array([[h[0]*SZ+g[0]*SX]])
+        Ws=[npc.Array.from_ndarray(Wsn,[leg_b,leg_b.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"])]
+        return MPO(sites,Ws)
+    if len(J)==L and L>1:
+        raise NotImplementedError("PBE's don't work yet")
+    Wsa=np.array([[ID,J[0]*SZ,h[0]*SZ+g[0]*SX]])
+    Ws=[npc.Array.from_ndarray(Wsa,[leg_b,leg_m.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"])]
+    for i in range(1,L-1):
+        Wsc=np.array([[ID,J[i]*SZ,h[i]*SZ+g[i]*SX],[ZE,ZE,SZ],[ZE,ZE,ID]])
+        Ws.append(npc.Array.from_ndarray(Wsc,[leg_m,leg_m.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]))
+    Wsb=np.array([[h[-1]*SZ+g[-1]*SX],[SZ],[ID]])
+    Ws.append(npc.Array.from_ndarray(Wsb,[leg_m,leg_b.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]))
+    return MPO(sites,Ws,bc="finite")
+def ising_F(J,g,h):
+    L=len(h) # maybe change to explicit length?
+    sites=[FlatSite() for _ in range(L)]
+    J=np.array(J)
+    g=np.array(g)
+    h=np.array(h)
+    leg_p=LegCharge.from_trivial(2)
+    leg_m=LegCharge.from_trivial(3)
+    leg_b=LegCharge.from_trivial(1)
+    Wg=[np.array([[[[np.cos(gc),1.0j*np.sin(gc)],[1.0j*np.sin(gc),np.cos(gc)]]]]) for gc in g]
+    Wg=[npc.Array.from_ndarray(w,[leg_b,leg_b.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) for w in Wg]
+    mpg=MPO(Sites,Wg,bc="finite")
+    Wh=[np.array([[[[np.exp(1.0j*hc),0],[0,np.exp(-1.0j*hc)]]]]) for hc in h]
+    Wh=[npc.Array.from_ndarray(w,[leg_b,leg_b.conj(),leg_p,leg_p.conj()],labels=["wL","wR","p","p*"]) for w in Wh]
+    mph=MPO(Sites,Wh,bc="finite")
+    if len(J)==L and L>1:
+        raise NotImplementedError("PBE's don't work yet")
+    if L==1:
+        return multiply_mpos([mph,mpg])
+    WJ=[]
+    return multiply_mpos([mpJ,mph,mpg])
--- tests/test_operator/test_ising_H.py
+++ tests/test_operator/test_ising_H.py
@@ -22,6 +22,16 @@ def dense_ising_H_real():
     g=np.random.normal(size=(L,))
     h=np.random.normal(size=(L,))
     return (dense.ising_H(J,g,h),(J,g,h))
+
+@pytest.fixture(scope="module")
+def dense_ising_H_complex_obc():
+    L=5
+    seed_rng("dense_ising_H_complex_obc")
+    J=np.random.normal(size=(L-1,))+np.random.normal(size=(L-1,))*1.0j
+    g=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
+    h=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
+    return (dense.ising_H(J,g,h),(J,g,h))
+
 def test_dense_ising_H_real(dense_ising_H_real):
     diH=dense_ising_H_real[0]
     assert diH.dtype==np.float_
@@ -46,12 +56,6 @@ def test_sparse_ising_H_complex(dense_ising_H_complex):
     siH=sparse.ising_H(*dense_ising_H_complex[1])
     assert siH.dtype==np.complex_
     assert sparse.sparse_to_dense(siH)==pytest.approx(dense_ising_H_complex[0])
-@pytest.mark.xfail
-def test_mps_ising_H_real(dense_ising_H_real):
-    miH=mps.ising_H(*dense_ising_H_real[1])
-    assert mps.mps_to_dense(siH)==pytest.approx(dense_ising_H_real[0])
-
-@pytest.mark.xfail
-def test_mps_ising_H_complex(dense_ising_H_complex):
-    miH=mps.ising_H(*dense_ising_H_complex[1])
-    assert mps.mps_to_dense(siH)==pytest.approx(dense_ising_H_complex[0])
+def test_mps_ising_H_complex_obc(dense_ising_H_complex_obc):
+    miH=mps.ising_H(*dense_ising_H_complex_obc[1])
+    assert mps.mpo_to_dense(miH)==pytest.approx(dense_ising_H_complex_obc[0])
