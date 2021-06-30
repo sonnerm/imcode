f927f4a2cee9216ad9a2afdbb35528518289dbff
jthoenniss <julthoe@gmail.com>
2021-06-30 16:08:50 +0200

    outsourced function to compute the Ising response function gamma

---
+++ imcode/correlation_approach/ising_gamma.py
@@ -0,0 +1,11 @@
+import numpy as np
+def ising_gamma(M_fw, eigenvalues_G_eff_fw):#computes response function gamma that appears in the Ising chain
+    nsites = len(M_fw) / 2
+    gamma_test_range = 100
+    gamma_test_vals = np.zeros(gamma_test_range)
+    for i in range(gamma_test_range):
+        gamma_test = 0
+        for k in range(nsites):
+            gamma_test += abs(M_fw[0, k] - M_fw[nsites, k])**2 * np.cos(eigenvalues_G_eff_fw[k] * i)
+        gamma_test_vals[i] = gamma_test
+    return np.arange(0, gamma_test_range), gamma_test_vals
