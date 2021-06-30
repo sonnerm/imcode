76ed7491061eff34314ede67abcab259e093e984
jthoenniss <julthoe@gmail.com>
2021-06-30 16:05:36 +0200

    compute correlation block in seperate function

--- imcode/correlation_approach/create_correlation_block.py
+++ imcode/correlation_approach/create_correlation_block.py
@@ -7,72 +7,57 @@ from scipy import linalg
 from scipy.sparse.linalg import eigsh
 import matplotlib.pyplot as plt
 np.set_printoptions(suppress=False, linewidth=np.nan)
-from IM_exponent import IM_exponent
-from add_cmplx_random_antisym import add_cmplx_random_antisym
 from rotation_matrix_for_schur import rotation_matrix_for_schur
 
-def create_correlation_block(M_fw, M_fw_inverse, M_bw, M_bw_inverse, N_t, eigenvalues_G_eff_fw, eigenvalues_G_eff_bw, nsites, ntimes, Jx, Jy, T_xy, rho_t):
-    print('Creating Greens function for time ', ntimes)
+def create_correlation_block(B):
+    dim_B = len(B)
 
-    # compute all correlators from which we can construct Keldysh Greens functions
-    B = IM_exponent(M_fw, M_fw_inverse, M_bw, M_bw_inverse, N_t, eigenvalues_G_eff_fw, eigenvalues_G_eff_bw, nsites, ntimes, Jx, Jy, T_xy, rho_t)
-    
-    B = add_cmplx_random_antisym(B, 1e-8)#add random antisymmetric part to matrix to lift degeneracies and stabilize numerics
-
-    print('computing all temporal correlations')
-
-    print('B\n')
-    print B
-
+    print('Creating correlation block for total time', dim_B /4)
     R, ews_sorted = rotation_matrix_for_schur(B)
-    corr_block_diag = np.zeros((8 * ntimes, 8 * ntimes))
+
+    corr_block_diag = np.zeros((2 * dim_B, 2*dim_B))#REAL - WORKS ONLY IN ISING CASE
 
     ews_sorted = np.real(ews_sorted)##enable these to be complex!!!!!
-    for i in range(0, 2 * ntimes):
+    for i in range(0, dim_B / 2):
         #Theta = np.arctan(G_schur[i,i + 1])
         Theta = np.arctan(ews_sorted[i])
         corr_block_diag[2 * i, 2 * i] = np.cos(Theta)**2
         corr_block_diag[2 * i + 1, 2 * i + 1] = np.cos(Theta)**2
 
-        corr_block_diag[2 * i, 2 * i + 4 * ntimes + 1] = - \
+        corr_block_diag[2 * i, 2 * i + dim_B + 1] = - \
             np.cos(Theta)*np.sin(Theta)
-        corr_block_diag[2 * i + 1, 2 * i + 4 *
-                        ntimes] = np.cos(Theta)*np.sin(Theta)
+        corr_block_diag[2 * i + 1, 2 * i + dim_B] = np.cos(Theta)*np.sin(Theta)
 
-        corr_block_diag[2 * i + 4 * ntimes, 2 *
-                        i + 1] = np.cos(Theta)*np.sin(Theta)
-        corr_block_diag[2 * i + 4 * ntimes + 1, 2 * i] = - \
+        corr_block_diag[2 * i + dim_B, 2 * i + 1] = np.cos(Theta)*np.sin(Theta)
+        corr_block_diag[2 * i + dim_B + 1, 2 * i] = - \
             np.cos(Theta)*np.sin(Theta)
 
-        corr_block_diag[2 * i + 4 * ntimes, 2 *
-                        i + 4 * ntimes] = np.sin(Theta)**2
-        corr_block_diag[2 * i + 4 * ntimes + 1, 2 * i +
-                        4 * ntimes + 1] = np.sin(Theta)**2
+        corr_block_diag[2 * i + dim_B, 2 * i + dim_B] = np.sin(Theta)**2
+        corr_block_diag[2 * i + dim_B + 1, 2 * i + dim_B + 1] = np.sin(Theta)**2
 
-    corr_block_diag2 = np.zeros((8 * ntimes, 8 * ntimes), dtype=np.complex_)
-    for i in range(0, 2 * ntimes):
+    corr_block_diag2 = np.zeros((2 * dim_B, 2 * dim_B), dtype=np.complex_)
+    for i in range(0, dim_B / 2):
         #Theta = np.arctan(G_schur[i,i + 1])
         ew = ews_sorted[i]
         norm = 1 + abs(ew)**2
         corr_block_diag2[2 * i, 2 * i] = 1/norm
         corr_block_diag2[2 * i + 1, 2 * i + 1] = 1/norm
 
-        corr_block_diag2[2 * i, 2 * i + 4 * ntimes + 1] = - ew/norm
-        corr_block_diag2[2 * i + 1, 2 * i + 4 *
-                         ntimes] = ew/norm
+        corr_block_diag2[2 * i, 2 * i + dim_B + 1] = - ew/norm
+        corr_block_diag2[2 * i + 1, 2 * i + dim_B] = ew/norm
 
-        corr_block_diag2[2 * i + 4 * ntimes, 2 *
+        corr_block_diag2[2 * i + dim_B, 2 *
                          i + 1] = ew.conj()/norm
-        corr_block_diag2[2 * i + 4 * ntimes + 1, 2 * i] = - ew.conj()/norm
+        corr_block_diag2[2 * i + dim_B + 1, 2 * i] = - ew.conj()/norm
 
-        corr_block_diag2[2 * i + 4 * ntimes, 2 *
-                         i + 4 * ntimes] = abs(ew)**2/norm
-        corr_block_diag2[2 * i + 4 * ntimes + 1, 2 * i +
-                         4 * ntimes + 1] = abs(ew)**2/norm
+        corr_block_diag2[2 * i + dim_B, 2 *
+                         i + dim_B] = abs(ew)**2/norm
+        corr_block_diag2[2 * i + dim_B + 1, 2 * i +
+                         dim_B + 1] = abs(ew)**2/norm
 
     compare_blocks = 0
-    for i in range(0, 8*ntimes):
-        for j in range(0, 8*ntimes):
+    for i in range(0, 2 * dim_B):
+        for j in range(0, 2 * dim_B):
             compare_blocks += abs(corr_block_diag[i,
                                   j] - corr_block_diag2[i, j])
     print 'compare_block', compare_blocks
@@ -80,22 +65,22 @@ def create_correlation_block(M_fw, M_fw_inverse, M_bw, M_bw_inverse, N_t, eigenv
     #corr_block_diag = corr_block_diag2
 
     # print 'corr_block_diag\n', corr_block_diag
-    double_R = np.bmat([[R, np.zeros((4 * ntimes, 4 * ntimes))],
-                       [np.zeros((4 * ntimes, 4 * ntimes)), R]])
+    double_R = np.bmat([[R, np.zeros((dim_B, dim_B))],
+                       [np.zeros((dim_B, dim_B)), R]])
 
     # print 'double R\n', double_R
     identity_check2 = np.dot(double_R.conj().T, double_R)
-    print 'unity_check2\n', np.trace(identity_check2)/(8 * ntimes)
+    print 'unity_check2\n', np.trace(identity_check2)/(2 * dim_B)
 
     corr_block_back_rotated = np.dot(double_R, corr_block_diag)
     corr_block_back_rotated = np.dot(corr_block_back_rotated, double_R.T)
     # print 'corr_block_back_rotated\n', corr_block_back_rotated
 
     eigenvalues_correlations, ev_correlations = eigsh(
-        corr_block_diag, 8*ntimes)
-    print(ntimes, eigenvalues_correlations)
+        corr_block_diag, 2 * dim_B)
+    print(dim_B/4, eigenvalues_correlations)
     eigenvalues_correlations, ev_correlations = eigsh(
-        corr_block_back_rotated, 8*ntimes)
+        corr_block_back_rotated, 2 * dim_B)
     print(eigenvalues_correlations)
 
     return corr_block_back_rotated
