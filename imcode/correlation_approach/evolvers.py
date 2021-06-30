3adf2c292b194498bc549732f55d42f38b9e80ef
jthoenniss <julthoe@gmail.com>
2021-06-30 17:32:31 +0200

    fixed typo in branch specification

--- imcode/correlation_approach/evolvers.py
+++ imcode/correlation_approach/evolvers.py
@@ -34,12 +34,13 @@ def evolvers(M_fw, M_fw_inverse, M_bw, M_bw_inverse,  N_t, eigenvalues_G_eff_fw,
     # initialize evolvers
     # first dimension 2 is for forward/backward branch (0 is forward, 1 is backward), second dimension 2 is because we do not compute correlations at every site in the environment but only at site 1 -> two relevant lines (site 0 and 0+L in env.) are extracted (in principle one would have dimension 2*nsites here,too)
     T_tilde = np.zeros((2, ntimes, 2, 2*nsites), dtype=np.complex_)
+    #T_tilde_c = np.zeros((2, ntimes, 2, 2*nsites), dtype=np.complex_)
 
     # fill with values (individually for each branch)
+    #branch, tau = np.mgrid[0:2,0:ntimes]
     for tau in range(0, ntimes):
-        T_tilde[0, tau] = np.einsum('ij,jk,kl,lm,mn->in', D_beta_fw, M_fw,
-                                    matrix_power(D_phi_fw, tau + 1), M_bw_inverse, N_t)  # forward branch
-        T_tilde[1, tau] = np.einsum('ij,jk,kl,lm,mn->in', D_beta_bw, M_bw,
-                                    matrix_power(D_phi_bw, tau + 1), M_bw_inverse, N_t)  # backward branch
+        T_tilde[0, tau] = np.einsum('ij,jk,kl,lm,mn->in', D_beta_fw, M_fw,matrix_power(D_phi_fw, tau + 1), M_fw_inverse, N_t)  # forward branch
+        T_tilde[1, tau] = np.einsum('ij,jk,kl,lm,mn->in', D_beta_bw, M_bw,matrix_power(D_phi_bw, tau + 1), M_bw_inverse, N_t)  # backward branch
+    #T_tilde_c[branch,tau] = np.einsum('ij,jk,kl,lm,mn->in', D_beta_fw, M_fw,matrix_power(D_phi_fw, tau + 1), M_bw_inverse, N_t)  # forward branch
 
     return T_tilde
