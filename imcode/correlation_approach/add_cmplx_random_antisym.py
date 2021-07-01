9df4f101b31f1d3c2be9f38afc64c953b89bba08
jthoenniss <julthoe@gmail.com>
2021-07-01 10:20:42 +0200

    add only real part to antisymmetric matrix

--- imcode/correlation_approach/add_cmplx_random_antisym.py
+++ imcode/correlation_approach/add_cmplx_random_antisym.py
@@ -8,18 +8,15 @@ def add_cmplx_random_antisym(matrix, magnitude):
     else:    
         dim = len(matrix) # matrix is square matrix with dimension dim
         random_part_real = np.random.rand(dim,dim) * magnitude
-        random_part_imag = np.random.rand(dim,dim) * magnitude * 1j
-
+        
         #antisymmetrize random part
         for i in range(dim):
             for j in range(i, dim):
                 if i == j:
-                    random_part_real[i, j] = 0
-                    random_part_imag[i, j] = 0
+                    random_part_real[i, j] = 0   
                 else:
                     random_part_real[j, i] = - random_part_real[i, j]
-                    random_part_imag[j, i] = - random_part_imag[i, j]
-
+                    
         matrix += (random_part_real)
-
+    
     return matrix
