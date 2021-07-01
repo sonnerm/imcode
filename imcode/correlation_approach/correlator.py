fixed sign error in correlator function

imcode/correlation_approach/correlator.py
@@ -11,6 +11,6 @@ def correlator(A, rho_t_diag, branch1, majorana_type1, tau_1, branch2, majorana_
     result = 0
     for k in range(nsites):
         n_F = 1. / (1.+np.exp(rho_t_diag[k, k]))  # fermi-dirac distribution
-        result += A[branch2, majorana_type2, tau_2, k+nsites]*A[branch1, majorana_type1, tau_1, k]*n_F - A[branch2, majorana_type2, tau_2, k]*A[branch1, majorana_type1, tau_1, k+nsites]*(1-n_F)
+        result += A[branch2, majorana_type2, tau_2, k+nsites]*A[branch1, majorana_type1, tau_1, k]*n_F + A[branch2, majorana_type2, tau_2, k]*A[branch1, majorana_type1, tau_1, k+nsites]*(1-n_F)
 
     return result