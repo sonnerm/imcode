d2514d11d32b4bca558bf00f8a573f5c1cfd7c72
Michael Sonner <sonnerm@gmail.com>
2021-07-02 17:15:09 +0200

    continue to mps

--- tests/dense/ising/test_contract.py
+++ tests/dense/ising/test_contract.py
@@ -43,24 +43,6 @@ def test_contract_3x3_mixed(seed_rng):
         initv=F@initv@F.T.conj()
     direct=np.trace(finalv@initv)
     assert transverse==pytest.approx(direct)
-# def test_contract_3x3_periodic(seed_rng):
-#     L=3
-#     t=3
-#     init=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
-#     final=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
-#     Js=np.random.normal(size=(L,))
-#     gs=np.random.normal(size=(L,))
-#     hs=np.random.normal(size=(L,))
-#     Ts=[dense.ising.ising_T(t,J,g,h,i,f) for J,g,h,i,f in zip(Js,gs,hs,init,final)]
-#     F=dense.ising.ising_F(L,Js,gs,hs)
-#     bc=dense.ising.open_boundary_im(t)
-#     initv=dense.kron(init)
-#     finalv=dense.kron(final)
-#     transverse=np.trace(reduce(lambda x,y:x@y,Ts))
-#     for _ in range(t):
-#         initv=F@initv@F.T.conj()
-#     direct=np.trace(finalv@initv)
-#     assert transverse==pytest.approx(direct)
 
 def test_contract_unity(seed_rng):
     L=3
@@ -80,29 +62,27 @@ def test_contract_unity(seed_rng):
     transverse=dense.ising.open_boundary_im(t)@bc
     assert transverse==pytest.approx(1.0)
 
-# def test_contract_3x3_pure(seed_rng):
+# def test_contract_3x3_boundary(seed_rng):
 #     L=3
 #     t=3
-#     init=[np.random.normal(size=(2,))+1.0j*np.random.normal((2,)) for _ in range(L)]
-#     final=[np.random.normal(size=(2,))+1.0j*np.random.normal((2,)) for _ in range(L)]
-#     init=[i/np.sqrt(i.conj()@i) for i in init]
-#     final=[f/np.sqrt(f.conj()@f) for f in final]
+#     init=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
+#     final=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
+#     bdl=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(t)]]
+#     bdr=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(t)]]
 #     Js=np.random.normal(size=(L-1,))
 #     gs=np.random.normal(size=(L,))
 #     hs=np.random.normal(size=(L,))
 #     Ts=[dense.ising.ising_T(t,J,g,h,i,f) for J,g,h,i,f in zip(Js,gs[:-1],hs[:-1],init[:-1],final[:-1])]
 #     Ts.append(dense.ising.ising_W(t,gs[-1],init[-1],final[-1])@dense.ising.ising_h(t,hs[-1]))
 #     F=dense.ising.ising_F(L,Js,gs,hs)
-#     bc=dense.ising.open_boundary_im(t)
-#     initv=dense.outer(init)
-#     finalv=dense.outer(final)
+#     bc=dense.kron(bdr)
+#     initv=dense.kron(init)
+#     finalv=dense.kron(final)
 #     for T in Ts:
 #         bc=T@bc
-#     transverse=dense.ising.open_boundary_im(t)@bc
+#     transverse=dense.kron(bdl)@bc
+#
 #     for _ in range(t):
-#         initv=F@initv
-#     direct=finalv@initv
+#         initv=F@initv@F.T.conj()
+#     direct=np.trace(finalv@initv)
 #     assert transverse==pytest.approx(direct)
-
-def test_contract_3x3_boundary(seed_rng):
-    pass
--- todo.md
+++ todo.md
@@ -2,7 +2,8 @@ tests for channel in dense
 
 tests for im_triangle in dense,sparse,mps
 
-dual-unitary W?
+tests for dual-unitary W?
+tests for periodic lattices
 
 im_triangle for brickwork
 time dependent h
