7777341adb762985aa05a45265c3af3559c54d54
jthoenniss <julthoe@gmail.com>
2021-07-02 11:26:35 +0200

    if real, add only real part, if complex, add complex

--- imcode/correlation_approach/add_cmplx_random_antisym.py
+++ imcode/correlation_approach/add_cmplx_random_antisym.py
@@ -7,16 +7,16 @@ def add_cmplx_random_antisym(matrix, magnitude):
 
     else:    
         dim = len(matrix) # matrix is square matrix with dimension dim
-        random_part_real = np.random.rand(dim,dim) * magnitude
+        random_part = np.random.rand(dim,dim) * magnitude * matrix[0,0]
         
         #antisymmetrize random part
         for i in range(dim):
             for j in range(i, dim):
                 if i == j:
-                    random_part_real[i, j] = 0   
+                    random_part[i, j] = 0   
                 else:
-                    random_part_real[j, i] = - random_part_real[i, j]
+                    random_part[j, i] = - random_part[i, j]
                     
-        matrix += (random_part_real)
+        matrix += (random_part)
     
     return matrix
