68d96da318e6c076a9e48b9d18690a507eb97130
Michael Sonner <sonnerm@gmail.com>
2021-07-06 10:53:09 +0200

    mps contract works

--- tests/mps/ising/test_contract.py
+++ tests/mps/ising/test_contract.py
@@ -7,22 +7,24 @@ def test_contract_6x6_mixed(seed_rng):
     chi=64
     init=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
     final=[np.random.normal(size=(2,2))+1.0j*np.random.normal((2,2)) for _ in range(L)]
+    init=[i.T.conj()+i for i in init]
+    final=[f.T.conj()+f for f in final]
     Js=np.random.normal(size=(L-1,))
     gs=np.random.normal(size=(L,))
     hs=np.random.normal(size=(L,))
     Ts=[mps.ising.ising_T(t,J,g,h,i,f) for J,g,h,i,f in zip(Js,gs[:-1],hs[:-1],init[:-1],final[:-1])]
     Ts.append((mps.ising.ising_W(t,gs[-1],init[-1],final[-1])@mps.ising.ising_h(t,hs[-1])).contract())
-    F=mps.ising.ising_F(L,Js,gs,hs)
     bc=mps.ising.open_boundary_im(t)
     for T in Ts:
         bc=(T@bc).contract(chi_max=chi)
+    F=mps.ising.ising_F(L,Js,gs,hs)
     Fc=mps.unitary_channel(F)
     transverse=mps.ising.open_boundary_im(t)@bc
     initv=mps.MPS.from_product_state([i.ravel() for i in init])
     for _ in range(t):
         initv=(Fc@initv).contract(chi_max=chi)
-    finalv=mps.MPS.from_product_state([f.ravel() for f in final])
-    direct=initv@finalv
+    finalv=mps.MPS.from_product_state([f.T.ravel() for f in final])
+    direct=finalv@initv
     assert transverse==pytest.approx(direct)
 
 def test_contract_unity(seed_rng):
