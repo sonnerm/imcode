b96b7c7bf566b9f788bb47de7624d21543220c42
Michael Sonner <sonnerm@gmail.com>
2021-07-06 10:52:55 +0200

    added dense channel+test

--- imcode/dense/__init__.py
+++ imcode/dense/__init__.py
@@ -1,3 +1,4 @@
 from .utils import SX,SY,SZ,SM,SP,ID,sx,sy,sz,sm,sp,one,rdm,kron,outer
 from . import ising
 from . import brickwork
+from .channel import unitary_channel
---
+++ tests/dense/test_channel.py
@@ -0,0 +1,16 @@
+import imcode.dense as dense
+import pytest
+import numpy as np
+def test_unitary_channel(seed_rng):
+    L=4
+    F=np.random.normal(size=(2**L,2**L))+1.0j*np.random.normal(size=(2**L,2**L))
+    init=[np.random.normal(size=(2,2))+1.0j*np.random.normal(size=(2,2)) for _ in range(L)]
+    final=[np.random.normal(size=(2,2))+1.0j*np.random.normal(size=(2,2)) for _ in range(L)]
+    initm=dense.kron(init)
+    finalm=dense.kron(final)
+    initv=dense.outer([i.ravel() for i in init])
+    finalv=dense.outer([f.T.conj().ravel() for f in final])
+    trad=np.trace(finalm@F@initm@F.T.conj())
+    Fc=dense.unitary_channel(F)
+    newm=finalv.conj()@Fc@initv
+    assert trad==pytest.approx(newm)
