ac7086d33bb5798a8c225c4b5c8bc855da9f80f4
Michael Sonner <sonnerm@gmail.com>
2021-07-08 18:28:41 +0200

    test_dephaser channel

--- tests/mps/ising/test_im_channel.py
+++ tests/mps/ising/test_im_channel.py
@@ -4,18 +4,22 @@ import pytest
 
 def test_open_boundary_channel(seed_rng):
     t=4
-    init=np.random.normal(size=(2,2))+np.random.normal(size=(2,2))*1.0j
-    init=init+init.T.conj()
     im=mps.ising.open_boundary_im(t)
     for i in range(t):
         imc=mps.ising.im_channel_dense(im,i)
         assert imc==pytest.approx(np.eye(4))
 
-def test_pd_channel(seed_rng):
+def test_pd_channel():
     t=4
-    init=np.random.normal(size=(2,2))+np.random.normal(size=(2,2))*1.0j
-    init=init+init.T.conj()
     im=mps.ising.perfect_dephaser_im(t)
     for i in range(t):
         imc=mps.ising.im_channel_dense(im,i)
         assert imc==pytest.approx(np.diag([1.0,0.0,0.0,1.0]))
+
+def test_dephaser_channel(seed_rng):
+    t=4
+    gamma=np.random.uniform(0,1)
+    im=mps.ising.dephaser_im(t,gamma)
+    for i in range(t):
+        imc=mps.ising.im_channel_dense(im,i)
+        assert imc==pytest.approx(np.diag([1.0,1-gamma,1-gamma,1.0]))
