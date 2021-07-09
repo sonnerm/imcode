85afd2469ea6d3bf4fd08b908a0839904f6176f6
Michael Sonner <sonnerm@gmail.com>
2021-07-09 15:10:23 +0200

    test unitarity

--- tests/dense/brickwork/test_brickwork_F.py
+++ tests/dense/brickwork/test_brickwork_F.py
@@ -15,8 +15,12 @@ def test_four_site_open_brickwork_F(seed_rng):
     diF=dense.brickwork.brickwork_F(4,egates)
     assert diF==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
     assert diF==pytest.approx(dense.brickwork.brickwork_F(4,[g.T for g in egates],True).T)
+    assert diF!=pytest.approx(diF.T.conj())
+    assert diF@diF.T.conj()!=pytest.approx(np.eye(16))
     diFT=dense.brickwork.brickwork_F(4,egates,True)
     assert diFT==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
+    assert diFT!=pytest.approx(diF.T.conj())
+    assert diFT@diFT.T.conj()!=pytest.approx(np.eye(16))
 
 
 def test_four_site_per_brickwork_F(seed_rng):
@@ -27,5 +31,25 @@ def test_four_site_per_brickwork_F(seed_rng):
     diF=dense.brickwork.brickwork_F(4,egates)
     assert diF==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
     assert diF==pytest.approx(dense.brickwork.brickwork_F(4,[g.T for g in egates],True).T)
+    assert diF!=pytest.approx(diF.T.conj())
+    assert diF@diF.T.conj()!=pytest.approx(np.eye(16))
     diFT=dense.brickwork.brickwork_F(4,egates,True)
     assert diFT==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
+    assert diFT!=pytest.approx(diF.T.conj())
+    assert diFT@diFT.T.conj()!=pytest.approx(np.eye(16))
+
+def test_four_site_per_brickwork_F_real(seed_rng):
+    gates = [np.random.normal(size=(4,4)) + 1.0j * np.random.normal(size=(4,4)) for _ in range(4)]
+    gates=[g/2+g.T.conj()/2 for g in gates]
+    diH=dense.brickwork.brickwork_H(4,gates)
+    dt=1e-4
+    egates=[la.expm(1.0j*dt*g) for g in gates]
+    diF=dense.brickwork.brickwork_F(4,egates)
+    assert diF==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
+    assert diF==pytest.approx(dense.brickwork.brickwork_F(4,[g.T for g in egates],True).T)
+    assert diF!=pytest.approx(diF.T.conj())
+    assert diF@diF.T.conj()==pytest.approx(np.eye(16))
+    diFT=dense.brickwork.brickwork_F(4,egates,True)
+    assert diFT==pytest.approx(la.expm(1.0j*dt*diH),rel=10*dt,abs=10*dt)
+    assert diFT!=pytest.approx(diF.T.conj())
+    assert diFT@diFT.T.conj()==pytest.approx(np.eye(16))
