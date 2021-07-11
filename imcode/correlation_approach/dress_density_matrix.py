f5236a1106fa24479bae82d1953c8ff1179079ae
Julian Thoenniss <julthoe@gmail.com>
2021-07-11 20:12:01 +0200

    generalize dressing of density matrix

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -6,7 +6,12 @@ from scipy import linalg
 
 
 def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers):
-    #assume that rho_0 is given in real space basis ..this need to be generalized for more general density matrices
-    rho_dressed = - linalg.logm( matrix_power(F_E_prime,nbr_Floquet_layers)  @ expm(- rho_0)  @ matrix_power(F_E_prime_dagger,nbr_Floquet_layers)) 
 
-    return rho_dressed #exponent of dressed density matrix
+    #assume that rho_0 is given in real space basis ..this need to be generalized for more general density matrices
+    rho_dressed = matrix_power(F_E_prime,nbr_Floquet_layers)  @ expm(- rho_0)  @ matrix_power(F_E_prime_dagger,nbr_Floquet_layers)
+
+    #diagonalize dressed density matrix:
+    exp_eigvals, N_t = linalg.eig(rho_dressed)
+    eigvals = -np.log(exp_eigvals)#eigenvalues of exponent of gaussian density matrix
+    
+    return  eigvals, N_t #exponent of dressed density matrix, exponent of diagonalized dressed density matrix, matrix that diagonalizes dressed density matrix rho_dresse
