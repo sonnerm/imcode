9ab87c1661580a65e7c38c2bd1645edd93c792f3
Julian Thoenniss <julthoe@gmail.com>
2021-07-11 16:48:44 +0200

    clean print output

--- imcode/correlation_approach/evolvers.py
+++ imcode/correlation_approach/evolvers.py
@@ -22,15 +22,12 @@ def evolvers(evolution_matrix,  N_t, nsites, nbr_Floquet_layers, beta_tilde):
     for branch in range (2):
         for tau_index in range(0, nbr_Floquet_layers):
             T_tilde[branch, tau_index] =  D_beta[branch] @ matrix_power(evolution_matrix[branch], tau_index + 1) #@ N_t
-    print ('T_tilde[0,0]')
-    print (T_tilde[0,0])
     result = 0
     for k in range(nsites):
         #n_F = 1. / (1.+np.exp(rho_t_diag[k, k]))  # fermi-dirac distribution
         #result += A[branch2, majorana_type2, tau_2_index, k+nsites]*A[branch1, majorana_type1, tau_1_index, k]*n_F + A[branch2, majorana_type2, tau_2_index, k]*A[branch1, majorana_type1, tau_1_index, k+nsites]*(1-n_F)
         
         # infinite temperature limit
-        print (k, T_tilde[0,0, 0,k],T_tilde[0,1,0,k + nsites])
         result += (T_tilde[0,0, 0,k]*T_tilde[0,0,0,k + nsites] + T_tilde[0,0, 0,k+ nsites]*T_tilde[0,0,0,k ] )  #edit
     print (result)
     #test if evolvers reproduce correct relations for Ising limit
