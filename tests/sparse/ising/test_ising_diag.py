450e804427e8e4ee0cf1ec916c71fb979599c82d
Michael Sonner <sonnerm@gmail.com>
2021-07-12 17:51:28 +0200

    adjusted test_ising_diag to new signature

--- tests/sparse/ising/test_ising_diag.py
+++ tests/sparse/ising/test_ising_diag.py
@@ -6,11 +6,11 @@ def test_sparse_ising_diag(seed_rng):
     L=5
     J=np.random.normal(size=L)+1.0j*np.random.normal(size=L)
     h=np.random.normal(size=L)+1.0j*np.random.normal(size=L)
-    sdi=sparse.ising.ising_diag(J,h)
+    sdi=sparse.ising.ising_diag(L,J,h)
     assert sdi.dtype==np.complex_
-    assert np.diag(sdi)==pytest.approx(dense.ising.ising_H(J,[0.0]*L,h))
+    assert np.diag(sdi)==pytest.approx(dense.ising.ising_H(L,J,[0.0]*L,h))
     J=np.random.normal(size=L)
     h=np.random.normal(size=L)
-    sdi=sparse.ising.ising_diag(J,h)
+    sdi=sparse.ising.ising_diag(L,J,h)
     assert sdi.dtype==np.float_
-    assert np.diag(sdi)==pytest.approx(dense.ising.ising_H(J,[0.0]*L,h))
+    assert np.diag(sdi)==pytest.approx(dense.ising.ising_H(L,J,[0.0]*L,h))
