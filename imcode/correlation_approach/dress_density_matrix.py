111a2540f0a331dc67b847768510dd95d9fb5d4d
Julian Thoenniss <julthoe@gmail.com>
2021-07-14 11:27:49 +0200

    return log of determined eigenvalues

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -12,6 +12,7 @@ def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
     eigenvals = np.zeros(len(rho_0[0]))
     N_t = np.identity(len(rho_0[0]), dtype=np.complex_)
 
+
     # assume that rho_0 is given in real space basis ..this need to be generalized for more general density matrices
     rho_dressed = matrix_power(
         F_E_prime, nbr_Floquet_layers)  @ expm(- rho_0)  @ matrix_power(F_E_prime_dagger, nbr_Floquet_layers)
@@ -21,10 +22,11 @@ def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
     #if (linalg.norm(F_E_prime)**nbr_Floquet_layers < 1e18):
     print ('F_Enorm',linalg.norm(F_E_prime) )
     # diagonalize dressed density matrix:
-    rho_dressed_herm = 0.5*(rho_dressed + rho_dressed.T.conj())
+    rho_dressed_herm = 0.5*(rho_dressed + rho_dressed.T.conj())#this stabilizes search for eigenvectors
+    
     eigenvals, eigenvecs = linalg.eigh(rho_dressed_herm)
 
-    argsort = np.argsort(- np.real(eigenvals))
+    argsort = np.argsort(- np.real(-np.log(eigenvals)))
     for i in range(int(len(rho_0[0])/2)):  # sort eigenvectors and eigenvalues such that the first half are the ones with positive real part, and the second half have negative real parts
         N_t[:, i] = eigenvecs[:, argsort[i]]
         N_t[:, len(rho_0[0]) - 1 - i] = eigenvecs[:, argsort[i + int(len(rho_0[0])/2)]]
@@ -33,9 +35,9 @@ def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
 
     diag = N_t.T.conj() @ rho_dressed_herm @ N_t
     eigenvals = diag.diagonal()
-
-    
+   
     print('dressed_matrix_test')
+    print(rho_dressed)
     print(rho_dressed_herm)
     print('N_t')
     print(N_t)
@@ -43,4 +45,6 @@ def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
     print( diag)
     print('eigenvalues')
     print(eigenvals)
-    return eigenvals, N_t  # exponent of dressed density matrix, exponent of diagonalized dressed density matrix, matrix that diagonalizes dressed density matrix rho_dresse
+    print('eigenvalues')
+    print(-np.log(eigenvals))
+    return (-np.log(eigenvals)), N_t  # eigenvalues of exponent of dressed DM, matrix that diagonalizes dressed density matrix rho_dresse
