34d3884476e1cdd4f5d070bf13cd30de259b3aec
Julian Thoenniss <julthoe@gmail.com>
2021-07-14 10:04:28 +0200

    adjust phases in N_t to allow for computation of spin correlation functions

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -22,14 +22,25 @@ def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
     print ('F_Enorm',linalg.norm(F_E_prime) )
     # diagonalize dressed density matrix:
     rho_dressed_herm = 0.5*(rho_dressed + rho_dressed.T.conj())
-    eigenvals, N_t = linalg.eigh(rho_dressed_herm)
+    eigenvals, eigenvecs = linalg.eigh(rho_dressed_herm)
 
+    argsort = np.argsort(- np.real(eigenvals))
+    for i in range(int(len(rho_0[0])/2)):  # sort eigenvectors and eigenvalues such that the first half are the ones with positive real part, and the second half have negative real parts
+        N_t[:, i] = eigenvecs[:, argsort[i]]
+        N_t[:, len(rho_0[0]) - 1 - i] = eigenvecs[:, argsort[i + int(len(rho_0[0])/2)]]
+
+    N_t = reorder_eigenvecs(N_t, int(len(rho_0[0])/2))
+
+    diag = N_t.T.conj() @ rho_dressed_herm @ N_t
+    eigenvals = diag.diagonal()
+
+    
     print('dressed_matrix_test')
     print(rho_dressed_herm)
     print('N_t')
     print(N_t)
     print('rho_diagonalized')
-    #print( diag)
+    print( diag)
     print('eigenvalues')
     print(eigenvals)
     return eigenvals, N_t  # exponent of dressed density matrix, exponent of diagonalized dressed density matrix, matrix that diagonalizes dressed density matrix rho_dresse
