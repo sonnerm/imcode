e0e83b96f4e877aaac214b3ba8d29db85d679634
Michael Sonner <sonnerm@gmail.com>
2021-07-15 17:22:10 +0200

    cleanup

--- imcode/mps/ising/obs.py
+++ imcode/mps/ising/obs.py
@@ -15,29 +15,6 @@ def boundary_dm_evolution(im,lop,init):
         dms.append(imc@dms[-1])
     return [dense.state_to_operator(np.sum(d.reshape((d.shape[0]//(dim**2),dim**2)),axis=0)) for d in dms]
 
-# def embedded_dm_evolution(left,lop,right,init):
-#     dms=[init.ravel()]
-#     dmsd=[(1,init.ravel().shape[0],1)]
-#     dim=init.shape[0]
-#     if isinstance(lop,np.ndarray):
-#         lop=[lop for _ in range(left.L)]
-#     for i in range(left.L):
-#         limc=im_channel_dense(left,i)
-#         rimc=im_channel_dense(right,i)
-#         rimc=rimc.reshape((rimc.shape[0]//4,4,rimc.shape[1]//4,4)).transpose([1,3,0,2]).reshape((rimc.shape[0],rimc.shape[1]))
-#         if dim>2:
-#             imc=dense.kron([limc,np.eye((dim//4)**2),rimc])
-#         else:
-#             imc=np.einsum("abcd,becf->abecdf",limc.reshape(limc.shape[0]//4,4,limc.shape[1]//4,4),rimc.reshape(4,rimc.shape[0]//4,4,rimc.shape[1]//4)).reshape((limc.shape[0]*rimc.shape[0]//4,limc.shape[1]*rimc.shape[1]//4))
-#             print(limc.shape)
-#             print(imc.shape)
-#         lopc=dense.kron([np.eye(limc.shape[1]//4),dense.unitary_channel(lop[i]),np.eye(rimc.shape[1]//4)])
-#         dms.append(lopc@dms[-1])
-#         dmsd.append(dmsd[-1])
-#         dms.append(imc@dms[-1])
-#         dmsd.append((limc.shape[0]//4,dim**2,rimc.shape[0]//4))
-#     return [np.sum(d.reshape((dd[0],dd[1],dd[2])),axis=(0,2)).reshape((dim,dim)) for d,dd in zip(dms,dmsd)]
-
 def embedded_dm_evolution(left,lop,right,init):
     dms=[dense.operator_to_state(init)]
     dim=init.shape[0]
