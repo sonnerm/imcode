55c3e25f46a2526fd5bfce4739a25bb3053b50d5
Julian Thoenniss <julthoe@gmail.com>
2021-07-16 18:28:06 +0200

    correct fermi dirac distributions, (this module is currently not needed)

--- imcode/correlation_approach/fermi_distr.py
+++ imcode/correlation_approach/fermi_distr.py
@@ -1,9 +1,11 @@
 import numpy as np
+from numpy.linalg import eig
 
 
 def n_F(eigvals, k):
+    nsites = int(len(eigvals) / 2)
     if k >= len(eigvals):
         print('Cannot compute Dirac distribution since argument is out of range+')
 
     else:
-        return 1. / (1 + np.exp(eigvals[k]))
+        return 1. / (1 + np.exp(eigvals[k] - eigvals[k + nsites]))
