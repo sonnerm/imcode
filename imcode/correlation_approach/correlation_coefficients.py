4d949ba064c5565b9234833bc5a197d724b5d5c9
Julian Thoenniss <julthoe@gmail.com>
2021-07-22 17:04:05 +0200

    clean print output

--- imcode/correlation_approach/correlation_coefficients.py
+++ imcode/correlation_approach/correlation_coefficients.py
@@ -6,7 +6,7 @@ def correlation_coefficients(T_tilde, nsites, nbr_Floquet_layers):
     # initialize correlation coefficients
     # first dimension 2 is for no_dressing (=0) or dressing (=1), second dimension 2 is for no_hermitian_conj_of_N (=0) or hermitian_conj_of_N (=1), third dimension 2 is for forward/backward branch (0 is forward, 1 is backward), fourth dimension 2 is for "Majorana" type (0 is THETA (relative MINUS sign), 1 is ZETA (relative PLUS sign)), fifth axis is for evolution times, sixth axis is for site arguments: (A_ij)-> site argument i=0 always, j in range (0,2L)
     A = np.zeros((2, 2, 2, 2, nbr_Floquet_layers, 2*nsites), dtype=np.complex_)
-    print (A.shape)
+
     # fill with values (individually for each branch)
     for tau in range(0, nbr_Floquet_layers):
         # buffer evolver values that are combined to give correlation coefficients A
@@ -23,8 +23,7 @@ def correlation_coefficients(T_tilde, nsites, nbr_Floquet_layers):
       
         # A-arguments: dressing, hermitian_conj_of_N, backward branch, "Zeta/+ Majorana", time, lattice index
         A[:, :, 1, 1, tau,:] = T_tilde[:, :, 1, tau, 0, :] + T_tilde[:, :, 1, tau, 1, :]
-        print ('array-shapes_cooo:', A.shape)
-        print (A[0, 0, 1, 1, tau,:],A[1, 0, 1, 1, tau,:])
+
 
     return A
 
