f2300ae37db6012993097d9d86899f45b067569a
Julian Thoenniss <julthoe@gmail.com>
2021-07-22 16:33:51 +0200

    restructure to make B_schur always real. This results automatically in a real correlation block in the diagonal basis. Using the unitary matrix R, it is rotated back to the original fermion basis.

--- imcode/correlation_approach/create_correlation_block.py
+++ imcode/correlation_approach/create_correlation_block.py
@@ -9,16 +9,10 @@ def create_correlation_block(B, ntimes):
 
     print('Creating correlation block for total time', ntimes)
     R = np.zeros((dim_B, dim_B), dtype=np.complex_)
-    B_schur = np.zeros((dim_B, dim_B), dtype=np.complex_)
-    R, B_schur = rotation_matrix_for_schur(B)
+    R, eigenvalues = rotation_matrix_for_schur(B)#the eigenvalues given out here are always real positive (R is defined in such a way). The negetiave counterparts are not included in this vector.
 
-    eigenvalues = np.zeros(2 * ntimes, dtype=np.complex_)
-    for i in range(0,2*ntimes):
-        eigenvalues[i] = B_schur[2 * i,2 * i + 1]#define eigenvalues from Schur form of matrix such that the order is in correspondence with the order of the eigenvectors in R.
-  
-
-    #compute correation block in diagonal basis with complex entries (is only used as check)
-    corr_block_diag = np.zeros((8 * ntimes, 8 * ntimes), dtype=np.complex_)
+    #compute correation block in diagonal basis with only entries this phases of fermionic operators are defined such that the eigenvalues of B are real
+    corr_block_diag = np.zeros((8 * ntimes, 8 * ntimes))
     for i in range(0, 2 * ntimes):
         ew = eigenvalues[i]
         norm = 1 + abs(ew)**2
@@ -26,58 +20,20 @@ def create_correlation_block(B, ntimes):
         corr_block_diag[2 * i + 1, 2 * i + 1] = 1/norm
         corr_block_diag[2 * i, 2 * i + dim_B + 1] = - ew/norm
         corr_block_diag[2 * i + 1, 2 * i + dim_B] = ew/norm
-        corr_block_diag[2 * i + dim_B, 2 * i + 1] = ew.conj()/norm
+        corr_block_diag[2 * i + dim_B, 2 * i + 1] = ew.conj()/norm#conjugation is formally correct but has no effect since eigenvalues are real anyways
         corr_block_diag[2 * i + dim_B + 1, 2 * i] = - ew.conj()/norm
         corr_block_diag[2 * i + dim_B, 2 * i + dim_B] = abs(ew)**2/norm
         corr_block_diag[2 * i + dim_B + 1, 2 * i + dim_B + 1] = abs(ew)**2/norm
-
+    print ('corr_block_diag\n', corr_block_diag)
     
-
-    #compute correation block in diagonal basis with phases redefined such that all entries are real (this is the correlation block that is actually used for further computation)
-    corr_block_diag2 = np.zeros((8 * ntimes, 8 * ntimes), dtype=np.complex_)
-    for i in range(0, 2 * ntimes):
-        ew = eigenvalues[i]
-        norm = 1 + abs(ew)**2
-        corr_block_diag2[2 * i, 2 * i] = 1/norm
-        corr_block_diag2[2 * i + 1, 2 * i + 1] = 1/norm
-        corr_block_diag2[2 * i, 2 * i + dim_B + 1] = - abs(ew)/norm
-        corr_block_diag2[2 * i + 1, 2 * i + dim_B] = abs(ew)/norm
-        corr_block_diag2[2 * i + dim_B, 2 * i + 1] = abs(ew)/norm
-        corr_block_diag2[2 * i + dim_B + 1, 2 * i] = - abs(ew)/norm
-        corr_block_diag2[2 * i + dim_B, 2 * i + dim_B] = abs(ew)**2/norm
-        corr_block_diag2[2 * i + dim_B + 1, 2 * i + dim_B + 1] = abs(ew)**2/norm
-
-    #this is the matrix that contains the phases, such that D_phases @ corr_block_diag2 @ D_phases.T.conj() = corr_block_diag
-    D_phases = np.zeros((4 * ntimes, 4 * ntimes), dtype=np.complex_)
-    for i in range(2 * ntimes):
-        D_phases[2 * i,2 * i] = np.exp(0.5j * np.angle(eigenvalues[i]))
-        D_phases[2 * i + 1,2 * i + 1] = np.exp(0.5j * np.angle(eigenvalues[i]))
-
-
-    double_phases = np.bmat([[D_phases, np.zeros((dim_B, dim_B),dtype=np.complex_)],[np.zeros((dim_B, dim_B),dtype=np.complex_), D_phases.conj()]])
-
-    #Check that correlation matrix with phases is reproduced correctly
-    C_test = double_phases @ corr_block_diag2 @ double_phases.T.conj()#check that this is equivalent to corr_block_diag
-    diff = 0
-    for i in range (len(C_test)):
-        for j in range(len(C_test)):
-            diff += abs(C_test[i,j] - corr_block_diag[i,j])
-    print ('corr_block_diag (complex)\n', corr_block_diag)
-    print ('corr_block_diag2 (real)\n', corr_block_diag2)
-    print ('corr_block_test (should be equivalent to corr_block_diag)\n', C_test)
-    print ('correlation difference', diff)
-
-
-    R = R @ D_phases#redefine rotation matrix R to contains the phases.
-
     #matrix that rotates the correlation block between the diagonal basis and the original fermion basis
     double_R = np.bmat([[R, np.zeros((dim_B, dim_B),dtype=np.complex_)],[np.zeros((dim_B, dim_B),dtype=np.complex_), R.conj()]])
 
-    corr_block_back_rotated = double_R @ corr_block_diag2 @ double_R.T.conj()#rotate correlation block back from diagonal basis to origianal fermion basis
+    corr_block_back_rotated = double_R @ corr_block_diag @ double_R.T.conj()#rotate correlation block back from diagonal basis to original fermion basis
     print ('corr_block_back_rotated\n', corr_block_back_rotated)
 
     #check that eigenvalues coincide in both bases (half of them should be 0 and half of them should be 1 in both cases)
-    eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_diag2)
+    eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_diag)
     print(eigenvalues_correlations)
     eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_back_rotated)
     print(eigenvalues_correlations)
--- imcode/correlation_approach/rotation_matrix_for_schur.py
+++ imcode/correlation_approach/rotation_matrix_for_schur.py
@@ -1,85 +1,46 @@
 from add_cmplx_random_antisym import add_cmplx_random_antisym
-import numpy as np
-#from numpy.core.einsumfunc import einsum
 from scipy import linalg
-from scipy.sparse.linalg import eigsh
+import numpy as np
 def rotation_matrix_for_schur(B):#this funciton computes the orthogonal matrix that brings B into Schur form 
     dim_B = len(B)
-    """
-    #Ising case:
-    eigenvalues, eigenvectors = linalg.eig(np.dot(1j, B))
 
-    argsort = np.argsort(- np.sign(np.real(eigenvalues)) * np.abs(eigenvalues))
-    eigenvectors_sorted = np.zeros((dim_B,dim_B), dtype=np.complex_)
-    eigenvalues_sorted = np.zeros(dim_B, dtype=np.complex_)
-
-    for i in range(dim_B / 2):
-        eigenvectors_sorted[:, i] = eigenvectors[:, argsort[i]]
-        eigenvectors_sorted[:, dim_B - 1 - i] = eigenvectors[:, argsort[i + dim_B / 2]]
-        eigenvalues_sorted[i] = eigenvalues[argsort[i]]
-        eigenvalues_sorted[dim_B - 1 - i] = eigenvalues[argsort[i + dim_B / 2]]
-
-    R = np.zeros((dim_B, dim_B))#this is the matrix that brings B into Schur form by R^T B R
-    for i in range(0, dim_B / 2):
-        R[:, 2 * i] = np.real(eigenvectors_sorted[:, i]) * 2**0.5#this work only in Ising case where B is real 
-        R[:, 2 * i + 1] = np.imag(eigenvectors_sorted[:, i]) * 2**0.5#this work only in Ising case where B is real 
-
-    schur = np.dot(R.T, B)
-    schur = np.dot(schur, R)
-    print 'Matrix B brought into Schur form:' ,schur
-
-
-    #for general case:
-    T, Z = linalg.schur(1j*B, output='complex')
-    print np.diag(T)
-    argsort2 = np.argsort(- np.sign(np.real(np.diag(T))) * np.abs(np.diag(T)))
-
-    eigenvectors_sorted2 = np.zeros((dim_B, dim_B), dtype=np.complex_)
-    eigenvalues_sorted2 = np.zeros(dim_B, dtype=np.complex_)
-    
-    for i in range(dim_B / 2):
-        eigenvectors_sorted2[:, (2 * i) + 1] = (Z[:, argsort2[(dim_B) - 1 - i]])
-        eigenvectors_sorted2[:, 2 * i] = (Z[:, argsort2[i]])
-        eigenvalues_sorted2[(2 * i) + 1] = np.diag(T)[argsort2[(dim_B) - 1 - i]]
-        eigenvalues_sorted2[2 * i] = np.diag(T)[argsort2[i]]
-    
-    for i in range(dim_B / 2):
-        eigenvectors_sorted2[:, 2*i] *= np.exp(-1j * np.angle(eigenvectors_sorted2[0, 2*i]))
-        eigenvectors_sorted2[:, 2*i + 1] *= np.exp(1j * np.angle(eigenvectors_sorted2[0, 2*i]/eigenvectors_sorted2[0, 2*i + 1]))
-    eigenvectors_sorted2 *= np.exp(-1j * np.pi/4)
-    
-    B_schur = np.dot(eigenvectors_sorted2.T, B)
-    B_schur = np.dot(B_schur, eigenvectors_sorted2)
-    schur_check = 0
-
-    for i in range(dim_B):
-        for j in range(i, dim_B):
-            schur_check += abs(B_schur[i, j])
-    for i in range(dim_B / 2):
-        schur_check -= eigenvalues_sorted2[i]
-    print eigenvalues_sorted2
-    print('Check that R yields Schur form \n', schur_check, '\n B_schur\n')
-    print(B_schur)
-    """
     hermitian_matrix = add_cmplx_random_antisym(B,1.e-8)
     hermitian_matrix = B.T @ B.conj()
-    eigenvalues_hermitian_matrix, R = linalg.eigh(hermitian_matrix)#eigsh is not as reliable
-
-    eigenvalues_B, eigenvectors_B = linalg.eig(1j*B)
-    print(eigenvalues_B)
-    B_schur = np.zeros((dim_B, dim_B), dtype=np.complex_)
+    eigenvalues_hermitian_matrix, R = linalg.eigh(hermitian_matrix)#compute rotation matrix as eigenvalues of hermitian_matrix, (eigsh is not as reliable)
 
     B_schur = R.T.conj() @ B @ R.conj()
-
-    print('Schur form of B \n')
+    print('Schur form of B (complex eigenvalues):')
     print(B_schur)
-    B_check = np.zeros((dim_B, dim_B), dtype=np.complex_)
+
+    #check that B_schur is indeed in the desired Schur form
     B_schur_check = B_schur
     for i in range (dim_B):
         for j in range (dim_B):
             if abs(i-j) != 1 or i+j+1%4 == 0:
                 B_schur_check[i,j] = 0
-
     print ('schur-test', linalg.norm(R @ B_schur_check @ R.T - B))
-  
-    return R, B_schur
+
+    eigenvalues_hermitian_matrix = np.zeros(int(dim_B/2), dtype=np.complex_)    
+    for i in range(0,int(dim_B/2)):
+        eigenvalues_hermitian_matrix[i] = B_schur[2 * i,2 * i + 1]#define eigenvalues from Schur form of matrix such that the order is in correspondence with the order of the eigenvectors in R.
+    
+    #this is the matrix that contains the phases, which can be absorbed in R, such that  R.T.conj() @ B @ R.conj() is real and all entries in the upper right triangular matrix are positive.
+    D_phases = np.zeros((dim_B, dim_B), dtype=np.complex_)
+    for i in range(int(dim_B/2)):
+        D_phases[2 * i,2 * i] = np.exp(0.5j * np.angle(eigenvalues_hermitian_matrix[i]))
+        D_phases[2 * i + 1,2 * i + 1] = np.exp(0.5j * np.angle(eigenvalues_hermitian_matrix[i]))
+
+    R = R @ D_phases #R is generally complex
+    print('rotation matrix R')
+    print (R)
+    B_schur = R.T.conj() @ B @ R.conj()
+    print('Schur form of B (real eigenvalues):')
+    print(B_schur)
+
+    eigenvalues_real = np.zeros(int(dim_B/2))
+    for i in range(0,int(dim_B/2)):
+        eigenvalues_real[i] = B_schur[2 * i,2 * i + 1]#define eigenvalues from Schur form of matrix such that the order is in correspondence with the order of the eigenvectors in R.
+    print ('eigenvalues (real): ',eigenvalues_real)
+
+
+    return R,  eigenvalues_real #return rotation matrix R and schur form of B, as well as eigenvalues which are all real with the rotation matrix R
