9cf5818be4df166c4d4d26b9d2e1aec88b111728
Julian Thoenniss <julthoe@gmail.com>
2021-07-22 14:46:25 +0200

    extract eigenvalues from hermitian matrix (exponent of dressed density matrix)

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -8,46 +8,32 @@ from scipy import linalg
 from reorder_eigenvecs import reorder_eigenvecs
 
 
-def dress_density_matrix(rho_0, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers):
-    nsites = int (len(rho_0[0]) / 2)
-    eigenvals = np.zeros(len(rho_0[0]))
-    N_t = np.identity(len(rho_0[0]), dtype=np.complex_)
+def dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers):
+    nsites = int (len(rho_0_exponent[0]) / 2)
+   
+    # the following line assumes that rho_0_exponent is given in real space basis..
+    rho_dressed = matrix_power(F_E_prime, nbr_Floquet_layers)  @ expm(- rho_0_exponent)  @ matrix_power(F_E_prime_dagger, nbr_Floquet_layers)
 
-
-    # assume that rho_0 is given in real space basis ..this need to be generalized for more general density matrices
-    rho_dressed = matrix_power(
-        F_E_prime, nbr_Floquet_layers)  @ expm(- rho_0)  @ matrix_power(F_E_prime_dagger, nbr_Floquet_layers)
-
-    # else, eigenvalues are zero and N_t is identity
-    #if linalg.norm(rho_dressed - identity(len(rho_0[0]))) > 1e-10:
-    #if (linalg.norm(F_E_prime)**nbr_Floquet_layers < 1e18):
-    print ('F_Enorm',linalg.norm(F_E_prime) )
     # diagonalize dressed density matrix:
-    rho_dressed_herm = 0.5*(rho_dressed + rho_dressed.T.conj())#this stabilizes search for eigenvectors
-    rho_0_herm = 0.5*(rho_0 + rho_0.T.conj())#this stabilizes search for eigenvectors
-    eigenvals, eigenvecs = linalg.eigh(-linalg.logm(rho_dressed_herm))
-    eigenvals_0, eigenvecs_0 = linalg.eigh(rho_0_herm)
+    rho_dressed_exponent = -linalg.logm(rho_dressed)#find exponent of density operator
+    rho_dressed_exponent_herm = 0.5*(rho_dressed_exponent + rho_dressed_exponent.T.conj())#this stabilizes search for eigenvectors
+    rho_0_exponent_herm = 0.5*(rho_0_exponent + rho_0_exponent.T.conj())#this stabilizes search for eigenvectors
+    eigenvals_dressed, eigenvecs_dressed = linalg.eigh(rho_dressed_exponent_herm)
+    eigenvals_0, eigenvecs_0 = linalg.eigh(rho_0_exponent_herm)
 
-    N_t = eigenvecs
-    
+    N_t = eigenvecs_dressed#this is the matrix that diagonalizes the dressed density matrix is N_t.T.conj() @ rho_dressed @ N_t
+    #check:
+    diag_check = N_t.T.conj() @ rho_dressed @ N_t
+    print('Dressed density matrix diagonalized', diag_check )
+    print('Eigenvalues of exponent:', -np.log(np.diag(diag_check)))
 
-
-    Z_dressed_over_Z_0 = 1
-    
-    n_expect = np.zeros((2 * nsites),dtype=np.complex_)
+    Z_dressed_over_Z_0 = 1#define ratio of partition functions (dressed rho vs. bare rho) -> will be computed in the lines below: Product of partition sums for every mode k  
     for k in range (nsites):
-        Z_dressed_over_Z_0 *= (np.exp(-eigenvals[k + nsites]) + np.exp(-eigenvals[k])) / (np.exp(-eigenvals_0[k + nsites]) + np.exp(-eigenvals_0[k])) 
+        Z_dressed_over_Z_0 *= (np.exp(-eigenvals_dressed[k + nsites]) + np.exp(-eigenvals_dressed[k])) / (np.exp(-eigenvals_0[k + nsites]) + np.exp(-eigenvals_0[k])) 
        
+    n_expect = np.zeros((2 * nsites),dtype=np.complex_)#fermi-Dirac distribution for modes (basis in which dressed density matrix is diagonal)
     for k in range (nsites):
-        n_expect[k] =  np.exp(-eigenvals[k]) /  (np.exp(-eigenvals[k + nsites]) + np.exp(-eigenvals[k])) 
-        n_expect[k + nsites] =  np.exp(-eigenvals[k + nsites]) /  (np.exp(-eigenvals[k + nsites]) + np.exp(-eigenvals[k])) 
-
-    print('dressed_matrix_test')
-    print(rho_dressed)
-    print(rho_dressed_herm)
-    print('N_t')
-    print(N_t)
-    print('eigenvalues')
-    print(eigenvals)
+        n_expect[k] =  np.exp(-eigenvals_dressed[k]) /  (np.exp(-eigenvals_dressed[k + nsites]) + np.exp(-eigenvals_dressed[k])) 
+        n_expect[k + nsites] = np.exp(-eigenvals_dressed[k + nsites]) /  (np.exp(-eigenvals_dressed[k + nsites]) + np.exp(-eigenvals_dressed[k])) 
     
-    return n_expect, N_t , Z_dressed_over_Z_0 # eigenvalues of exponent of dressed DM, matrix that diagonalizes dressed density matrix rho_dresse
+    return n_expect, N_t , Z_dressed_over_Z_0
