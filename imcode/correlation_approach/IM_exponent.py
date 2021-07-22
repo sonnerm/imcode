59d077372306facb2769bc3940afe755b5088d81
Julian Thoenniss <julthoe@gmail.com>
2021-07-22 11:38:13 +0200

    equal time correlators for different fields are nonzero

--- imcode/correlation_approach/IM_exponent.py
+++ imcode/correlation_approach/IM_exponent.py
@@ -69,7 +69,7 @@ def IM_exponent(evolution_matrix, N_t, nsites, nbr_Floquet_layers, Jx, Jy, n_exp
                 G_Feynman_thetatheta = -1j * correlator(A, n_expect, 0, 0, time_1, 0, 0, time_2, nsites)
                 G_AntiFeynman_thetatheta = 1j * correlator(A, n_expect, 1, 0, time_2, 1, 0, time_1, nsites)
 
-            elif tau_2 < tau_1:  # for i=j, G_Feynman and G_AntiFeynman are zero
+            elif tau_2 < tau_1:  
                 G_Feynman_zetazeta = -1j * correlator(A, n_expect, 0, 1, time_2, 0, 1, time_1, nsites)
                 G_AntiFeynman_zetazeta = 1j * correlator(A, n_expect, 1, 1, time_1, 1, 1, time_2, nsites)
 
@@ -81,6 +81,15 @@ def IM_exponent(evolution_matrix, N_t, nsites, nbr_Floquet_layers, Jx, Jy, n_exp
 
                 G_Feynman_thetatheta = 1j * correlator(A, n_expect, 0, 0, time_2, 0, 0, time_1, nsites)
                 G_AntiFeynman_thetatheta = -1j * correlator(A, n_expect, 1, 0, time_1, 1, 0, time_2, nsites)
+
+            else:# for tau_1 = tau_2, G_Feynman and G_AntiFeynman are zero for same fields
+                G_Feynman_zetatheta = - correlator(A, n_expect, 0, 0, time_2, 0, 1, time_1, nsites)
+                G_AntiFeynman_zetatheta = correlator(A, n_expect, 1, 1, time_1, 1, 0, time_2, nsites)
+
+                G_Feynman_thetazeta = - correlator(A, n_expect, 0, 1, time_2, 0, 0, time_1, nsites)
+                G_AntiFeynman_thetazeta = correlator(A, n_expect, 1, 0, time_1, 1, 1, time_2, nsites)
+
+
             
             prefac_x = alpha * np.tan(Jx)
             prefac_y = alpha * np.tan(Jy)
@@ -95,20 +104,27 @@ def IM_exponent(evolution_matrix, N_t, nsites, nbr_Floquet_layers, Jx, Jy, n_exp
             B[4 * tau_1, 4 * tau_2 + 2] = -1 * G_Feynman_thetazeta * prefac_x * prefac_y * 2
             if tau_1 == tau_2:
                 B[4 * tau_1, 4 * tau_2 + 2] *= 1. / alpha
+                print ('t1,', B[4 * tau_1, 4 * tau_2 + 2])
+
             B[4 * tau_1, 4 * tau_2 + 3] = G_lesser_thetazeta * prefac_x * prefac_y * 2
             B[4 * tau_1 + 1, 4 * tau_2 + 2] = -1 * G_greater_thetazeta * prefac_x * prefac_y * 2
             B[4 * tau_1 + 1, 4 * tau_2 + 3] = G_AntiFeynman_thetazeta * prefac_x * prefac_y * 2
             if tau_1 == tau_2:
                 B[4 * tau_1 + 1, 4 * tau_2 + 3] *= 1. / alpha
+                print ('t2,', B[4 * tau_1 + 1, 4 * tau_2 + 3])
+
 
             B[4 * tau_1 + 2, 4 * tau_2] = -1 * G_Feynman_zetatheta * prefac_x * prefac_y * 2
             if tau_1 == tau_2:
                 B[4 * tau_1 + 2, 4 * tau_2] *= 1. / alpha
+                print ('t3,', B[4 * tau_1 + 2, 4 * tau_2])
+
             B[4 * tau_1 + 2, 4 * tau_2 + 1] = -1 * G_lesser_zetatheta * prefac_x * prefac_y * 2
             B[4 * tau_1 + 3, 4 * tau_2] = G_greater_zetatheta * prefac_x * prefac_y * 2
             B[4 * tau_1 + 3, 4 * tau_2 + 1] = G_AntiFeynman_zetatheta * prefac_x * prefac_y * 2
             if tau_1 == tau_2:
                 B[4 * tau_1 + 3, 4 * tau_2 + 1] *= 1. / alpha
+                print ('t4,', B[4 * tau_1 + 3, 4 * tau_2 + 1])
 
             B[4 * tau_1 + 2, 4 * tau_2 + 2] = 1j * G_Feynman_zetazeta * prefac_x**2 * 2
             B[4 * tau_1 + 2, 4 * tau_2 + 3] = - 1j * G_lesser_zetazeta * prefac_x**2 * 2
@@ -122,6 +138,6 @@ def IM_exponent(evolution_matrix, N_t, nsites, nbr_Floquet_layers, Jx, Jy, n_exp
         B[4 * tau + 3, 4 * tau + 1] += 1
 
     print('B\n')
-    print(B)
+    print(B)# this matrix is twice the exponent of the influence matrix.
 
     return B
