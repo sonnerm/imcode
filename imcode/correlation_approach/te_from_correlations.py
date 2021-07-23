313224b063eb3fb1f4be9e8db6ac2579c784198c
Julian Thoenniss <julthoe@gmail.com>
2021-07-23 17:52:33 +0200

    make more readable

--- imcode/correlation_approach/te_from_correlations.py
+++ imcode/correlation_approach/te_from_correlations.py
@@ -39,22 +39,27 @@ rho_0_exponent = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
 # must be initialized as matrix that diagonalizes dressed density matrix
 N_t = np.identity(2 * nsites, dtype=np.complex_)
 
+
+
+
 # initialize arrays in which entropy values and corresponding time-cuts are stored
-entropy_values = np.zeros((int(max_time1/stepsize1) +
-                          int(max_time2/stepsize2) + 3, max_time2 + stepsize2))  # entropies
-times = np.zeros(int(max_time1/stepsize1) +
-                 int(max_time2/stepsize2))  # time cuts
+entropy_values = np.zeros((int(max_time1/stepsize1) +int(max_time2/stepsize2) + 3, max_time2 + stepsize2))  # entropies
+times = np.zeros(int(max_time1/stepsize1) + int(max_time2/stepsize2))  # time cuts
 ising_gamma_times = np.zeros(gamma_test_range)
 ising_gamma_values = np.zeros(gamma_test_range)
-# find generators and matrices which diagonalize them:
+
+
+# find generators and matrices which diagonalize the composite Floquet operator:
 G_XY_even, G_XY_odd, G_g, G_1 = compute_generators(nsites, Jx, Jy, g, beta_tilde)
 evolution_matrix, F_E_prime, F_E_prime_dagger = evolution_matrix(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx, Jy, g)
+
 #M, eigenvalues_G_eff, f= matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx, Jy, g)
 #ising_gamma_times, ising_gamma_values = ising_gamma(M,eigenvalues_G_eff, nsites, gamma_test_range)
 
 iterator = 1
 # total_time = 0 means one floquet-layer
 for total_time in range(2, max_time1, stepsize1):
+
     nbr_Floquet_layers = total_time + 1
     correlation_block = np.identity(8 * total_time, dtype=np.complex_)
 
@@ -67,9 +72,9 @@ for total_time in range(2, max_time1, stepsize1):
     time_cuts = np.arange(1, nbr_Floquet_layers)
 
     entropy_values[iterator, 0] = nbr_Floquet_layers
+
     for cut in time_cuts:
-        entropy_values[iterator, cut] = entropy(
-            correlation_block, nbr_Floquet_layers, cut)
+        entropy_values[iterator, cut] = entropy(correlation_block, nbr_Floquet_layers, cut)
     iterator += 1
 """
 
@@ -96,5 +101,4 @@ for total_time in range(max_time1, max_time2 + stepsize2, stepsize2):  # 90, nsi
 
 print(entropy_values)
 
-plot_entropy(entropy_values, ising_gamma_times,
-             ising_gamma_values, iterator, Jx, Jy, g, beta, nsites)
+plot_entropy(entropy_values, ising_gamma_times, ising_gamma_values, iterator, Jx, Jy, g, beta, nsites)
