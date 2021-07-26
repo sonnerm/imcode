cbbbaaa7a0849c3315a3e803ac0752b0fe4909c0
Julian Thoenniss <julthoe@gmail.com>
2021-07-26 18:35:18 +0200

    take rho_exponent instead of rho_exponent_herm

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -18,18 +18,18 @@ def dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger, nbr_Floque
     rand_A = np.random.rand(nsites,nsites) * rand_magn
     rand_B = add_cmplx_random_antisym(np.zeros((nsites,nsites), dtype=np.complex_), rand_magn)
     rand_C = add_cmplx_random_antisym(np.zeros((nsites,nsites), dtype=np.complex_), rand_magn)
-    random_part = np.bmat([[rand_A,rand_B], [rand_C, -rand_A.T]])
+    random_part = np.bmat([[rand_A,rand_B], [rand_C, -rand_A.T]]) 
+    random_part = random_part + random_part.T.conj()
 
-    rho_dressed_exponent += random_part#stabilize numerical diagonalization by adding random part
-    rho_dressed_exponent_herm = 0.5 * (rho_dressed_exponent + rho_dressed_exponent.T.conj())#this stabilizes search for eigenvectors
-    eigenvals_dressed, eigenvecs_dressed = linalg.eigh(rho_dressed_exponent_herm)
+    rho_dressed_exponent += random_part#stabilize numerical diagonalization by adding random part with correct symmetry properties
+    eigenvals_dressed, eigenvecs_dressed = linalg.eigh(rho_dressed_exponent)
 
     #By knowledge of the structure of N, i.e. (N = [[A, B^*],[B, A^*]]), we can construct the right part of the matrix from the left part of the matrix "eigenvecs_dressed", such that all phase factors and the order of eigenvectors are as desired.
     N_t = np.bmat([[eigenvecs_dressed[0:nsites,0:nsites], eigenvecs_dressed[nsites:2*nsites,0:nsites].conj()],[eigenvecs_dressed[nsites:2*nsites,0:nsites], eigenvecs_dressed[0:nsites,0:nsites].conj()]])
     print ('N_t\n',N_t)
     #check:
-    diag_check = N_t.T.conj() @ rho_dressed_exponent_herm @ N_t
-    eigenvals_dressed = -np.diag(N_t.T.conj() @ rho_dressed_exponent_herm @ N_t)
+    diag_check = N_t.T.conj() @ rho_dressed_exponent @ N_t
+    eigenvals_dressed = -np.diag(N_t.T.conj() @ rho_dressed_exponent @ N_t)
     print('Dressed density matrix diagonalized', diag_check )
     print('Eigenvalues of exponent:', eigenvals_dressed)
 
@@ -37,7 +37,8 @@ def dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger, nbr_Floque
     n_expect = np.zeros((2 * nsites))#fermi-Dirac distribution for modes (basis in which dressed density matrix is diagonal). 
     #Note that the true expectation value has an additional factor Z_over_Z0. This factor, however, does not enter the exponent of the IM and is therefore not included here.
     for k in range (nsites):
-        n_expect[k] = np.exp(-eigenvals_dressed[k]) / (np.exp(-eigenvals_dressed[k]) + np.exp(-eigenvals_dressed[k + nsites]))# for < c^dagger c >
+        eval = eigenvals_dressed[k]
+        n_expect[k] = np.exp(- eval) / (2 * np.cosh(eval) ) # for < c^dagger c >
         n_expect[k + nsites] = 1 - n_expect[k] # for < c c^dagger >
     
     return n_expect, N_t 
