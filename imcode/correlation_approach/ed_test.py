784ae3ad55d5eebcf01961690795bf0dec2718c7
Julian Thoenniss <julthoe@gmail.com>
2021-08-09 10:24:47 +0200

    finish contractions to obtain ancilla state

--- imcode/correlation_approach/ed_test.py
+++ imcode/correlation_approach/ed_test.py
@@ -8,7 +8,7 @@ from scipy import sparse
 from quimb import *
 np.set_printoptions(linewidth=np.nan, precision=2, suppress=True)
 
-L = 6 # number of sites INCLUDING THE SYSTEM, i.e. L = 1 + n_environment
+L = 6 # number of sites of the spin chain (i.e. INCLUDING THE SYSTEM)
 #compute initial state (2^(L-1) indices)
 #Hamiltonian of XX model:
 ham = ham_XY((L - 1), jxy= 1.0, bz = 0.0)#create XY-Hamiltonian using quimb
@@ -23,48 +23,65 @@ gs_energy, gs = eigsh(ham, 1) #yields ground state vector and corresponding eige
 J = 0.31
 g = np.pi/4
 
-print (gs)
 #density matarix for pure ground state
 state = gs @ gs.T.conj()
+state = np.reshape(state, (2**(L-1) , 1, 2**(L-1)))
 
 F_odd = expm(1j * ham_ising(2,g)) @ expm(1j * ham_ising(2,J))
 print('gate_odd\m', F_odd.shape)
 F_even = expm(1j * ham_ising(2,J))
 print('gate_even\m', F_even.shape)
 
+
+n_traced = 0#this variable keeps track of the number of spins in the spin chain that have been integrated out 
+
+
+for i in range (int(L/2) - 1):#iteratibely apply Floquet layer and trace out last two spins until the spin chain has become a pure ancilla spin chain.
+
 #odd layer
-layer_odd = F_odd
-for i in range (int(L/2) - 1):
-    print (i)
-    layer_odd = kron(layer_odd, F_odd)
+    layer_odd = F_odd
+    for i in range (int((L - n_traced)/2) - 1 ):
+        layer_odd = kron(layer_odd, F_odd)
+    print('layer odd shape',layer_odd.shape)
 
-#even layer
-layer_even = F_even
-for i in range (int(L/2 ) - 2):
-    print (i)
-    layer_even = kron(layer_even, F_even)
-print('layer even shape',layer_even.shape)
-layer_even = kron(layer_even, np.identity(2))
+    #even layer
+    layer_even = F_even
+    for i in range (int((L - n_traced)/2) - 2):
+        layer_even = kron(layer_even, F_even)
+    print('layer even shape',layer_even.shape)
+    layer_even = kron(layer_even, np.identity(2))
+  
+    #reshape layer such that it can be multiplied to state
+    F = np.einsum('ij, ajbk -> aibk', layer_even , np.reshape(layer_odd, (2, 2**(L-1 - n_traced), 2, 2**(L-1 - n_traced))))
 
-print('layer even shape',layer_even.shape)
-print('layer odd shape',layer_odd.shape)
+    #apply double layer of gates, F
+    state = np.einsum('aibj, jck, eldk  -> iabcdel', F,state, F.conj())
 
-print (2**(L-1))
-print(np.reshape(layer_odd, (2, 2**(L-1), 2, 2**(L-1))).shape)
-F = np.einsum('ij, ajbk -> aibk', layer_even , np.reshape(layer_odd, (2, 2**(L-1), 2, 2**(L-1))))
+    #update number of open legs (in spatial direction)
+    dim_open_legs = 2**(2 * n_traced + 4)
 
-#apply double layer of gates, F
-state = np.einsum('aibj, jk, dlck  -> abicdl', F,state, F.conj())
-print(state.shape)
-n_traced = 0
-dim_open_legs = 2**(2 * n_traced + 2)
-state = np.reshape(state,(2, 2**(L - 1 - n_traced - 2),2 ,2, dim_open_legs, 2 , 2**(L - 1 - n_traced - 2), 2, 2))
-#trace out last spin
+    #reshape state such that last spin can be traced out
+    state = np.reshape(state,(2**(L - 1 - n_traced - 2),2 ,2, dim_open_legs, 2**(L - 1 - n_traced - 2), 2, 2))
+  
+    #trace out the two last spins
+    state = np.trace(state, axis1 = 2, axis2 = 6)# trace out last spin
+    state = np.trace(state, axis1 = 1, axis2 = 4)# trace out second to last spin
 
-print (state.shape)
+    #update number of "non-ancilla"-spins of the original chain that have been traced out
+    n_traced += 2
 
-state = np.trace(state, axis1 = 3, axis2 = 7)# trace out last spin
-state = np.trace(state, axis1 = 2, axis2 = 6)# trace out second to last spin
-print (state.shape)
+#last layer consists only of single gate that coupled system and bath (i.e. "odd layer")
 
-state = np.reshape(state, )
+#reshape last gate such that it can be mutliplied to state
+last_gate =  np.reshape(F_odd, (2,2,2,2))
+
+#apply last gate
+state = np.einsum('aibj, jck, eldk  -> iabcdel', last_gate, state, last_gate.conj())
+
+#reshape such that last "non-ancilla" - spin can be traced out. After this step, only open legs in temporal direction remain.
+state = np.reshape(state,(2, 2**(2 * L),2))
+#trace out last "non-ancilla" - spin
+state = np.trace(state, axis1 = 0, axis2 = 2)
+
+
+state = np.reshape(state, np.tile(2,2 * L))
