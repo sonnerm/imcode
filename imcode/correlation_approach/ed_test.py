5e6a744e190172c82a746486a14d8b6cbd4fd9c1
Julian Thoenniss <julthoe@gmail.com>
2021-08-19 19:26:29 +0200

    simpifly tracing out

--- imcode/correlation_approach/ed_test.py
+++ imcode/correlation_approach/ed_test.py
@@ -1,4 +1,5 @@
 from math import e
+from os import stat_result
 from quimb.evo import schrodinger_eq_dop
 from scipy.linalg import expm
 from scipy.linalg import eigvalsh
@@ -7,9 +8,25 @@ from scipy.linalg import block_diag
 import  numpy as np
 from scipy import sparse
 from quimb import *
+
+def rdm(vec,sites):
+    '''
+        Calculates the reduced density matrix of the subsystem defined by ``sites``
+    '''
+    L=int(np.log2(len(vec)))
+    complement=sorted(list(set(range(L))-set(sites)))
+    vec=vec/np.sqrt(np.sum(vec.conj()*vec))
+    print(sites+complement)
+    print(sites)
+    vec=vec.reshape((2,)*L)
+    vec=np.transpose(vec,sites+complement)
+    vec=vec.reshape((2**len(sites),2**(len(complement))))
+    ret=np.einsum("ij,kj->ik",vec.conj(),vec)
+    return ret
+
 np.set_printoptions(linewidth=np.nan, precision=2, suppress=True)
 
-L = 8# number of sites of the spin chain (i.e. INCLUDING THE SYSTEM)
+L = 6# number of sites of the spin chain (i.e. INCLUDING THE SYSTEM)
 
 #------------------------------------------------------------ Define initial state density matrix--------------------------------------------------------------------
 
@@ -35,13 +52,16 @@ state = np.identity(2**(L-1)) / 2**(L-1)
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #Parameters for KIC Floquet evolution
-J = 0.31
+J = np.pi/4
 g = np.pi/4
 
 F_odd = expm(1j * ham_ising(2,0,g)) @ expm(1j * ham_ising(2,J,0))
 print('gate_odd\m', F_odd.shape)
+print(F_odd)
+print(ham_ising(2,J,0))
 F_even = expm(1j * ham_ising(2,J,0))
 print('gate_even\m', F_even.shape)
+print(F_even)
 
 n_traced = 0#this variable keeps track of the number of spins in the spin chain that have been integrated out 
 
@@ -57,7 +77,7 @@ for i in range (int(L/2) - 1):#iteratibely apply Floquet layer and trace out las
     for i in range (int((L - n_traced)/2) - 1 ):
         layer_odd = kron(layer_odd, F_odd)
     print('layer odd shape',layer_odd.shape)
-
+    print(layer_odd)
     #even layer
     layer_even = kron(np.identity(2), F_even)
     for i in range (int((L - n_traced)/2) - 2):
@@ -75,16 +95,15 @@ for i in range (int(L/2) - 1):#iteratibely apply Floquet layer and trace out las
     #update number of "non-ancilla"-spins of the original chain that will be traced out after this cycle
     n_traced += 2
 
-    #update number of open legs (in spatial direction)
+    #update dimensionality of open legs (in temporal direction)
     dim_open_legs_per_branch = 2**n_traced
 
     #reshape state such that last spin can be traced out
-    trace_shape = (2**(L - 1 - n_traced),2 ,2, dim_open_legs_per_branch, dim_open_legs_per_branch, 2**(L - 1 - n_traced), 2, 2)
+    trace_shape = (2**(L - 1 - n_traced), 4, dim_open_legs_per_branch, dim_open_legs_per_branch, 2**(L - 1 - n_traced), 4)
     state = np.reshape(state, trace_shape)
   
     #trace out the two last spins
-    state = np.trace(state, axis1 = 2, axis2 = 7)# trace out last spin
-    state = np.trace(state, axis1 = 1, axis2 = 5)# trace out second to last spin   
+    state = np.trace(state, axis1 = 1, axis2 = 5)# trace out last and second to last spin 
 
 
 #last layer consists only of single gate that coupled system and bath (i.e. "odd layer")
@@ -98,15 +117,17 @@ state = np.reshape(state,(2, 2**(2 * L),2))
 #trace out last "non-ancilla" - spin
 state = np.trace(state, axis1 = 0, axis2 = 2)
 
-state = np.reshape(state, (2**L, 2**L))
 
+#state = state.reshape(2**(L//2),2**(L//2),2**(L//2),2**(L//2) )
+#rdm_dm = np.trace(state, axis1 = 1, axis2 = 3) # view state as dm and trace out complementary times
 
-entr_eigvals = eigvalsh(state)
+rdm_state = rdm(state.reshape(-1), list(range(0,int(L/2))))
+
+entr_eigvals = eigvalsh(rdm_state)
 print (entr_eigvals)
-entropy_values = []
-for i in range (L):
-    entropy_values.append(-np.sum(np.maximum(entropy_values,0.0)*np.log(np.minimum(np.maximum(entropy_values,1e-32),1.0))))
-print (entropy_values)
+
+entropy_value = - np.sum(entr_eigvals * np.log(np.clip(entr_eigvals, 1e-30, 1.0)))
+print (entropy_value)
 
 
 print('Terminated..')
