06d700252f02bef95930f4c2e6c79345efac3615
Julian Thoenniss <julthoe@gmail.com>
2021-08-22 15:18:22 +0200

    correct evolution matrix

--- imcode/correlation_approach/evolution_matrix.py
+++ imcode/correlation_approach/evolution_matrix.py
@@ -3,15 +3,14 @@ from scipy.linalg import expm
 from scipy import linalg
 
 def evolution_matrix(nsites, G_XY_even, G_XY_odd, G_g, G_1):
-
-    F_E_prime = expm(0.5j*G_g) @ expm(0.5j * G_XY_even) @ expm(0.5j * G_XY_odd) @ expm(0.5 * G_1)
+    F_E_prime = expm(1j*G_g) @ expm(1j * G_XY_even) @ expm(1j * G_XY_odd) @ expm(1 * G_1)
     F_E_prime_dagger = F_E_prime.T.conj()
     F_E_prime_inverse = linalg.inv(F_E_prime)
 
     # compute evolution matrix:
     evolution_matrix = np.zeros((2, 2 * nsites, 2 * nsites), dtype=np.complex_)
 
-    evolution_matrix[0] = F_E_prime_inverse @ F_E_prime_inverse # forward branch
-    evolution_matrix[1] = F_E_prime_dagger @ F_E_prime_dagger # backward branch
+    evolution_matrix[0] = F_E_prime_inverse # forward branch
+    evolution_matrix[1] =  F_E_prime_dagger # backward branch
 
     return evolution_matrix, F_E_prime, F_E_prime_dagger
