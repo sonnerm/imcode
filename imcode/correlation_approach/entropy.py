d72670dbc2c643beeb646ddea40a93b704c0f8ed
Julian Thoenniss <julthoe@gmail.com>
2021-09-27 15:12:03 +0200

    add routine for entropy calculation from GM approach

--- imcode/correlation_approach/entropy.py
+++ imcode/correlation_approach/entropy.py
@@ -8,8 +8,18 @@ def entropy(correlation_block, ntimes, time_cut):
         # take this as default value if nothing has been specified otherwise
         time_cut = max(ntimes / 2, 1)
 
+    
+    #for correlation approach
     correlation_block_reduced = np.bmat([[correlation_block[0: 4 * time_cut, 0:  4 * time_cut], correlation_block[0: 4 * time_cut, 4 * ntimes: 4 * (time_cut + ntimes)]], [
         correlation_block[4 * ntimes: 4 * (time_cut + ntimes), 0:  4 * time_cut], correlation_block[4 * ntimes: 4 * (time_cut + ntimes), 4 * ntimes: 4 * (time_cut + ntimes)]]])
+    
+    #for GM-approach
+    Delta_half = 2 * time_cut#half the inverval that we define as subsystem
+    correlation_block_reduced = np.bmat([[correlation_block[2 * ntimes - Delta_half: 2 * ntimes + Delta_half, 2 * ntimes - Delta_half: 2 * ntimes + Delta_half], correlation_block[2 * ntimes - Delta_half: 2 * ntimes + Delta_half, 6 * ntimes - Delta_half:6 * ntimes + Delta_half]], [
+        correlation_block[6 * ntimes - Delta_half: 6 * ntimes + Delta_half,2 * ntimes - Delta_half: 2 * ntimes + Delta_half], correlation_block[6 * ntimes - Delta_half:6 * ntimes + Delta_half, 6 * ntimes - Delta_half: 6 * ntimes + Delta_half]]])
+    
+    
+    
     # print (correlation_block_reduced)
     eigenvalues_correlations, ev_correlations = linalg.eig(correlation_block_reduced)
     eigenvalues_correlations[::-1].sort()
