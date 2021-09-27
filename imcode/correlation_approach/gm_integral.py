e6b4db3496c44465f84f3614c4918a85cc9fca0e
Julian Thoenniss <julthoe@gmail.com>
2021-09-27 15:14:09 +0200

    start to implement general initial state for GM approach

--- imcode/correlation_approach/gm_integral.py
+++ imcode/correlation_approach/gm_integral.py
@@ -3,6 +3,7 @@ import numpy as np
 import sys
 
 from scipy import linalg
+from DM_kernel import compute_Kernel, find_index_dm
 
 
 np.set_printoptions(threshold=sys.maxsize)
@@ -56,7 +57,7 @@ def gm_integral(Jx, Jy, N_l, t):
 
     #Matrix that couples only environment variables xsi
     A_E = np.zeros((nbr_xsi, nbr_xsi),dtype=np.complex_)
-    for x in range (0,N_l - 1):#this covers only positive times
+    for x in range (0,N_l - 1):
         for tau in range (2 * t -1, -2 * t, -1):
             if (x+tau) % 2 == 0 and tau != 0:
                 i = find_index(x,tau,1,t)
@@ -64,19 +65,34 @@ def gm_integral(Jx, Jy, N_l, t):
                 A_E[i:i+2, j:j+2] = np.dot(np.sign(tau),R_quad)#if tau is negative, sign of coupling is switched
                 A_E[i,i+1] += gamma
                 A_E[j,j+1] += gamma
-            elif tau == 0:
-                i = find_index(x,0,1,t)
-                A_E[i,i+1] += 1#infinite temperature initial state
 
+    #initial state
+    #infinite temperature initial state
+    for x in range (0,N_l):   
+        i = find_index(x,0,1,t)
+        A_E[i,i+1] += 1
+    """
+    #general initial state
+    DM_compact = compute_Kernel(N_l)
+    #integrate into bigger matrix for gm_integral code:
+    for x in range (0,N_l):
+        for y in range (x,N_l):
+            for bar1 in range(2):
+                for bar2 in range(2):
+                    i = find_index(x,(bar1-1),bar1,t)
+                    j = find_index(y,(bar2-1),bar2,t)
+                    k = find_index_dm(x,bar1,N_l)
+                    l = find_index_dm(y,bar2,N_l)
+                    if j>i:
+                        A_E[i,j] += DM_compact[k,l]
+    """
     #for boundary spin on right side, insert identity gate
     x_edge_right = N_l - 1 
     for tau in range (2 * t -1, -2 * t, -1):
             if (x_edge_right+tau) % 2 == 0 and tau != 0:
                 i = find_index(x_edge_right,tau,1,t)
                 A_E[i,i+1] += 1
-    i = find_index(x_edge_right,0,1,t)
-    A_E[i,i+1] += 1#infinite temperature initial state at right boundary
-
+    
     #for boundary spin on right side, insert gamma from gate
     x_edge_left = 0
     for tau in range (2 * t -1, -2 * t, -1):
