9878e34492fd9896fd67d7d9f2a3106ed9ef471f
Julian Thoenniss <julthoe@gmail.com>
2021-10-05 13:36:52 +0200

    suppress unnecessary parts and output

--- imcode/correlation_approach/DM_kernel.py
+++ imcode/correlation_approach/DM_kernel.py
@@ -60,15 +60,18 @@ def compute_Kernel_XX(beta, N_l):
         for i in range(N_l):  # sort eigenvectors and eigenvalues such that the first half are the ones with positive real part, and the second half have negative real parts
             M[:, i] = eigvecs[:, argsort[i]]
             M[:, 2 * N_l - 1 - i] = eigvecs[:, argsort[i + N_l]]
-    print('M')
-    print(M)
+    
     diag = M.T.conj() @ A @ M
     eigvals = diag.diagonal()
+    """
+    print('M')
+    print(M)
     print('daig')
     print(eigvals)
     print(M.T.conj() @ A @ M)
+    """
     corr_block = np.zeros((2*N_l, 2*N_l),dtype=np.complex_)
-    corr_matrix = np.zeros((2*N_l, 2*N_l),dtype=np.complex_)
+    #corr_matrix = np.zeros((2*N_l, 2*N_l),dtype=np.complex_)
 
     for i in range(N_l):
         for j in range(N_l):
@@ -80,8 +83,8 @@ def compute_Kernel_XX(beta, N_l):
                 Z_k = 2*np.cosh(eigvals[k])
                 #dagger, no-dagger
 
-                sum21 += M[i, k].conj() * M[j, k] * np.exp(eigvals[k])/ Z_k
-                sum21 += M[i, k+N_l].conj() * M[j, k + N_l] * np.exp(eigvals[k + N_l]) / Z_k
+                #sum21 += M[i, k].conj() * M[j, k] * np.exp(eigvals[k])/ Z_k
+                #sum21 += M[i, k+N_l].conj() * M[j, k + N_l] * np.exp(eigvals[k + N_l]) / Z_k
                 # no-dagger, dagger
                 sum12 += M[i+ N_l, k].conj() * M[j+N_l, k] * np.exp(eigvals[k])/ Z_k
                 sum12 += M[i+N_l, k+ N_l].conj() * M[j+ N_l, k+ N_l] * np.exp(eigvals[k + N_l])/ Z_k
@@ -92,18 +95,20 @@ def compute_Kernel_XX(beta, N_l):
                 sum22 += M[i, k].conj() * M[j+N_l, k] * np.exp(eigvals[k])/ Z_k
                 sum22 += M[i+N_l, k] * M[j, k].conj() * np.exp(eigvals[k + N_l])/ Z_k
 
-            corr_block[j+ N_l,i] = sum12 #sum12#correct
+            corr_block[j+ N_l,i] = sum12 #correct
             corr_block[i+ N_l,j + N_l] = - sum22
             corr_block[i,j+ N_l] = - sum12 #correct
             corr_block[i,j] = - sum11
 
+            """
             corr_matrix[i,j] = sum11
             corr_matrix[i+N_l,j] = sum21
             corr_matrix[i,j+N_l] = sum12
             corr_matrix[i+N_l,j+N_l] = sum22
-
+            """
     #corr_block[N_l:2*N_l,0:N_l] =  - corr_block[0:N_l,N_l:2*N_l].T
 
+    """
     #check that correlations fulfill relations in correlaion matrix:
     check1 = corr_matrix[0:N_l,0:N_l] + corr_matrix[N_l:2*N_l,N_l:2*N_l].T - np.identity(N_l) 
     check2 = corr_matrix[0:N_l,N_l:2*N_l] - corr_matrix[N_l:2*N_l,0:N_l].T.conj()
@@ -112,20 +117,19 @@ def compute_Kernel_XX(beta, N_l):
     print(check2)
     print('corr block')
     print(corr_block)
+    """
 
-
-    
+    """
     rand_magn = 0
     rand_A = np.random.rand(N_l,N_l) * rand_magn
     rand_B = add_cmplx_random_antisym(np.zeros((N_l,N_l)), rand_magn)
     rand_C = add_cmplx_random_antisym(np.zeros((N_l,N_l)), rand_magn)
     random_part = np.bmat([[rand_A,rand_B], [rand_C, -rand_A.T]])
     corr_block += random_part
+    """
 
-    B_inv = linalg.inv(corr_block)
+    B_inv = linalg.inv(corr_block) + measure
    
-    B_inv = B_inv + measure
-
     #adjust signs
     for i in range(N_l):
         for j in range(N_l, 2*N_l):
@@ -133,6 +137,5 @@ def compute_Kernel_XX(beta, N_l):
             B_inv[j,i] = -B_inv[j,i]
 
     print('B_inv')
-    print(B_inv)
     return B_inv
 
