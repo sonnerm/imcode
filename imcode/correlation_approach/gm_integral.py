05f94afe4d028f582315191f79f17f0cf4ad8c19
Julian Thoenniss <julthoe@gmail.com>
2021-11-15 17:29:53 +0100

    use sparse matrix operations

--- imcode/correlation_approach/gm_integral.py
+++ imcode/correlation_approach/gm_integral.py
@@ -7,6 +7,9 @@ from scipy.linalg import det
 from ham_gs import compute_BCS_Kernel
 import multiprocessing as mp
 from scipy import linalg
+import scipy.sparse
+import scipy.sparse.linalg
+import scipy.sparse as sps
 from joblib import Parallel, delayed
 
 now = datetime.now().time() # time object
@@ -25,10 +28,10 @@ def find_index(x, tau, bar, t):
 def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
 
     #boundary couplings
-    Jx_boundary = 0
-    Jy_boundary = 0
+    Jx_boundary = Jx
+    Jy_boundary = Jy
     delta_blip = t
-    g_boundary = np.array(t*[1] + delta_blip * [-1] + (t-delta_blip)*[1])     #np.zeros(2*t)
+    g_boundary = np.zeros(2*t)#np.array(t*[1] + delta_blip * [-1] + (t-delta_blip)*[1])     #np.zeros(2*t)
 
     N_t = 4 * t 
     nbr_xsi = N_l * 2 * (N_t - 1) #factor 2 since there are two types of grassmanns (with and without bar). This is true if the last and first layers are erased
@@ -62,7 +65,10 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
 
     
     #Matrix that couples only environment variables xsi
-    A_E = np.zeros((nbr_xsi, nbr_xsi),dtype=np.complex_)
+    #A_E = np.zeros((nbr_xsi, nbr_xsi),dtype=np.complex_)
+  
+    A_E = sps.dok_matrix((nbr_xsi, nbr_xsi),dtype=np.complex_)
+
     #INITIAL STATE 
     #infinite temperature initial state
     #for x in range (0,N_l):   
@@ -90,13 +96,12 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
         i = find_index(N_l - 1 ,0,1,t)
         A_E[i,i+1] += 1
     """
-    
-    
-    
+  
     #general initial state
     #DM_compact = compute_Kernel_XX(beta, N_l)
     DM_compact , normalization_state = compute_BCS_Kernel(Jx,Jy,g,mu_initial_state, N_l, filename)
     det_factor_state = 0.25
+ 
     #integrate into bigger matrix for gm_integral code:
     for x in range (0,N_l):
         for y in range (x,N_l):
@@ -106,12 +111,11 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
                     j = find_index(y,(bar2-1),bar2,t)
                     k = find_index_dm(x,bar1,N_l)
                     l = find_index_dm(y,bar2,N_l)
-                    if j>=i:
+
+                    if j>i:
                         A_E[i,j] += DM_compact[k,l]
-    
-    
+
     gate_counter = 0
-    
 
     #write in matrix A_E that couples only environment variables
     for x in range (0,N_l - 1):
@@ -139,10 +143,7 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
                         A_E[i+1,j] *= np.exp(2.j* g)
                         A_E[i,i+1] *= np.exp(2.j* g)
                         A_E[j,j+1] *= np.exp(2.j* g)
-
-
-    
-    
+  
     #for boundary spin on right side, insert identity gate
     x_edge_right = N_l - 1 
     for tau in range (2 * t -1, -2 * t, -1):
@@ -154,7 +155,6 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
                 else: 
                     A_E[i,i+1] += 1
                 
-    
     #for boundary spin on left side, insert gamma from gate
     x_edge_left = 0
     bound_iter = 0
@@ -173,36 +173,34 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
     #temporal boundary condition for measure
     for i in range(N_l):
         A_E[i * 2 * (N_t - 1),(i+1) * 2 * (N_t - 1) -1] += 1
-
+   
+    """
     A_E_norm = A_E.copy()
-     
+    
     bound_iter = 0
     for tau in range (2 * t -1, -2 * t, -1):
             if (x_edge_left+tau) % 2 == 1 and tau != 0:
                 i = find_index(x_edge_left,tau,1,t)
                 A_E_norm[i,i+1] = A_E_norm[i,i+1] *  np.exp(2.j* (g_boundary[bound_iter] - abs(g_boundary[bound_iter])) * np.sign(tau))
                 bound_iter += 1
+    """
     
-    #antisymmetrize
-    for i in range(len(A_E[0])):
-        for j in range(i,len(A_E[0])):
-            A_E[j,i] = - A_E[i,j]
-            A_E_norm[j,i] = - A_E_norm[i,j]
-
 
     #Matrix that couples the system to the first site of the environment
-    R = np.zeros((nbr_eta, nbr_xsi),dtype=np.complex_)
+    R = sps.dok_matrix((nbr_eta, nbr_xsi),dtype=np.complex_)
     for i in range (t):#2t
         R[2*i:2*i+2, 4*i:4*i+2] = np.dot(1.,R_quad_boundary)
     for i in range (t,2*t):#2t
         R[2*i:2*i+2, 4*i:4*i+2] = np.dot(-1.,R_quad_boundary)
  
     #Matrix that couples system variables to system variables
-    A_s = np.zeros((nbr_eta, nbr_eta),dtype=np.complex_)
+    A_s = sps.dok_matrix((nbr_eta, nbr_eta),dtype=np.complex_)
     for i in range (2 * t):
         A_s[2*i, 2*i+1] = gamma_val_boundary
         A_s[2*i+1, 2*i] = - gamma_val_boundary
  
+
+    """
     norm_gate = 1. / (np.cos(Jx)*np.cos(Jy) * (1 + np.tan(Jx)*np.tan(Jy)))
     normalization_circuit = norm_gate**(gate_counter % N_t) #this is the norm such that when divided by this, the unitary circuit gives 1 without eternal legs
     det_factor_circ = norm_gate**(- (gate_counter - gate_counter%N_t) / N_t)
@@ -213,40 +211,52 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
     N = normalization_state * normalization_circuit
     
    
-    A_E_norm_inv = linalg.inv(A_E_norm)
+    #A_E_norm_inv = linalg.inv(A_E_norm)
     #A_E = np.identity(len(A_E[0]))
-    IM_value = np.sqrt(abs(det( pow(det_factor_state**2, N_l  / nbr_xsi) * pow(det_factor_circ**2, 1. * N_t / nbr_xsi)  * A_E)))/N 
-    IM_value2 = np.sqrt(abs(det( A_E @ A_E_norm_inv  )))
-    print('IM_value:',IM_value )
+    #IM_value = np.sqrt(abs(det( pow(det_factor_state**2, N_l  / nbr_xsi) * pow(det_factor_circ**2, 1. * N_t / nbr_xsi)  * A_E)))/N 
+    #IM_value2 = np.sqrt(abs(det( A_E @ A_E_norm_inv  )))
+    #print('IM_value:',IM_value )
 
-    print('IM_value2:',IM_value2 )
-
+    #print('IM_value2:',IM_value2 )
+    """
     
     #solve for certain columns of inverted matrix A_E:
-    identity_matrix = np.identity(len(A_E[0]))
-
-    now = datetime.now()
-    print(now)
-    A_inv = np.zeros(A_E.shape,dtype=np.complex_)
+    identity_matrix = np.identity(A_E.shape[0])
+    
+    A_inv = sps.dok_matrix(A_E.shape,dtype=np.complex_)
 
     #num_cores = mp.cpu_count()
     #if __name__ == "__main__":
     #    A_inv[:,0:2 * (N_t - 1)] = np.array(Parallel(n_jobs=num_cores)(delayed(np.linalg.solve)(A_E, identity_matrix[:,i]) for i in range(2 * (N_t - 1)) )).T
+   
   
-    A_inv[:,0:2 * (N_t - 1)] = np.linalg.solve(A_E,identity_matrix[:,0:2 * (N_t - 1)])
-  
-    B =  A_s +  R @ A_inv @ R.T
+    A_E_sparse = sps.csr_matrix(A_E)
+    R_sparse = sps.csr_matrix(R)
+    A_s_sparse = sps.csr_matrix(A_s)
 
+
+    A_E_sparse += -A_E_sparse.T
+
+
+    A_inv[:,0:2 * (N_t - 1)] = scipy.sparse.linalg.spsolve(A_E_sparse,identity_matrix[:,0:2 * (N_t - 1)]) #A_E is automatically converted to CSR or SCS by this function
+    #A_inv[:,0:2 * (N_t - 1)] = np.linalg.solve(A_E,identity_matrix[:,0:2 * (N_t - 1)]) 
+
+
+    A_inv_sparse = sps.csr_matrix(A_inv)
+
+    B =  (A_s_sparse  +  R_sparse  @ A_inv_sparse @ R_sparse.T).toarray()
+    now = datetime.now()
+ 
     #write A_inv and B to file
     with h5py.File(filename + '.hdf5', 'a') as f:
         IM_data = f['IM_exponent']
         edge_corr_data = f['edge_corr']
         #bulk_corr_data = f['bulk_corr']
         const_blip_data = f['const_blip']
-        IM_data[iterator,:len(B[1]),:len(B[0])] = B[:,:]
+        IM_data[iterator,:B.shape[0],:B.shape[0]] = B[:,:]
         #bulk_corr_data[iterator,:len(A_inv[1]),:len(A_inv[0])] = A_inv[:,:]
-        edge_corr_data[iterator,0:2 * (N_t - 1),0:2 * (N_t - 1)] = A_inv[0:2 * (N_t - 1),0:2 * (N_t - 1)]
-        const_blip_data[iterator] = IM_value
+        edge_corr_data[iterator,0:2 * (N_t - 1),0:2 * (N_t - 1)] = A_inv.toarray()[0:2 * (N_t - 1),0:2 * (N_t - 1)]
+        #const_blip_data[iterator] = IM_value
        
     
     """
@@ -259,5 +269,6 @@ def gm_integral(Jx, Jy,g,mu_initial_state, beta, N_l, t, filename, iterator):
             abs += np.abs(B[i,j] - B_comp[i,j])
     print('abs',abs)
     """
+ 
     return B
 
