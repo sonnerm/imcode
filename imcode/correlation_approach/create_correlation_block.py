4440cd75a31206651f1d388d454902f14dc8ed4c
Julian Thoenniss <julthoe@gmail.com>
2021-11-17 18:55:45 +0100

    time stamps in create correlation block and take out test diagonalizations

--- imcode/correlation_approach/create_correlation_block.py
+++ imcode/correlation_approach/create_correlation_block.py
@@ -1,15 +1,20 @@
 from rotation_matrix_for_schur import rotation_matrix_for_schur
 import numpy as np
+from datetime import datetime
+#from memory_profiler import profile
 from scipy import linalg
 np.set_printoptions(suppress=False, linewidth=np.nan)
-
+#@profile
 def create_correlation_block(B, ntimes):
     dim_B = 4 * ntimes
 
     print('Creating correlation block for total time', ntimes)
     R = np.zeros((dim_B, dim_B), dtype=np.complex_)
+    now = datetime.now()
+    print('Diagonalize B', now)
     R, eigenvalues = rotation_matrix_for_schur(B)#the eigenvalues given out here are always real positive (R is defined in such a way). The negetiave counterparts are not included in this vector.
-
+    now = datetime.now()
+    print('End diag of B', now)
     #compute correation block in diagonal basis with only entries this phases of fermionic operators are defined such that the eigenvalues of B are real
     corr_block_diag = np.zeros((8 * ntimes, 8 * ntimes))
     for i in range(0, 2 * ntimes):
@@ -27,14 +32,14 @@ def create_correlation_block(B, ntimes):
     
     #matrix that rotates the correlation block between the diagonal basis and the original fermion basis
     double_R = np.bmat([[R, np.zeros((dim_B, dim_B),dtype=np.complex_)],[np.zeros((dim_B, dim_B),dtype=np.complex_), R.conj()]])
-
+    
     corr_block_back_rotated = double_R @ corr_block_diag @ double_R.T.conj()#rotate correlation block back from diagonal basis to original fermion basis
     #print ('corr_block_back_rotated\n', corr_block_back_rotated)
 
     #check that eigenvalues coincide in both bases (half of them should be 0 and half of them should be 1 in both cases)
-    eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_diag)
+    #eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_diag)
     #print(eigenvalues_correlations)
-    eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_back_rotated)
+    #eigenvalues_correlations, ev_correlations = linalg.eigh(corr_block_back_rotated)
     #print(eigenvalues_correlations)
 
     return corr_block_back_rotated
