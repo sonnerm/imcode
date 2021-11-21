e98d97f4e579575fab0b3d22333573b2c63d9d7f
Julian Thoenniss <julthoe@gmail.com>
2021-11-21 12:14:50 +0100

    simplify main loop and allow to continue job

--- imcode/correlation_approach/te_from_correlations.py
+++ imcode/correlation_approach/te_from_correlations.py
@@ -13,30 +13,32 @@ from compute_generators import compute_generators
 from ising_gamma import ising_gamma
 import sys
 from gm_integral import gm_integral
+import os
 import math
 
 
 
-np.set_printoptions(linewidth=np.nan, precision=10, suppress=True)
+np.set_printoptions(linewidth=np.nan, precision=1, suppress=True)
 
 # define fixed parameters:
 # step sizes for total times t
-max_time1 = 2
-max_time2 = 6
+max_time1 = 4
+max_time2 = 40
 stepsize1 = 1
-stepsize2 = 1
+stepsize2 = 4
 
+write_mode = sys.argv[1] #if the argument is 'w', overwrite file if it exists, otherwise append if it exists
 # lattice sites (in the environment):
-nsites = int(sys.argv[1])
+nsites = int(sys.argv[2])
 # model parameters:
-del_t = float(sys.argv[2])
-Jx = float(sys.argv[3]) * del_t #0.5# 0.31 # 0.31
-Jy =float(sys.argv[4])* del_t#np.pi/4+0.3#np.pi/4
-g =float(sys.argv[5])* del_t #np.pi/4+0.3
-beta = float(sys.argv[6])#0.4  # temperature
+del_t = float(sys.argv[3])
+Jx = float(sys.argv[4]) * del_t #0.5# 0.31 # 0.31
+Jy =float(sys.argv[5])* del_t#np.pi/4+0.3#np.pi/4
+g =float(sys.argv[6])* del_t #np.pi/4+0.3
+beta = float(sys.argv[7])#0.4  # temperature
 mu_initial_state = 0
-if len(sys.argv) > 7:
-    mu_initial_state = float(sys.argv[7])
+if len(sys.argv) > 8:
+    mu_initial_state = float(sys.argv[8])
 
 print('nsites', nsites)
 print('del_t', del_t)
@@ -45,7 +47,6 @@ print('Jy', Jy)
 print('g', g)
 print('beta', beta)
 print('mu_init_state', mu_initial_state)
-
 
 beta_tilde = np.arctanh(np.tan(Jx) * np.tan(Jy))
 alpha_0_square = (np.cos(2 * Jx) + np.cos(2 * Jy)) / 2.
@@ -74,14 +75,31 @@ print('filename:', filename)
 ising_gamma_times = np.zeros(gamma_test_range)
 ising_gamma_values = np.zeros(gamma_test_range)
 
-with h5py.File(filename + ".hdf5", 'w') as f:
-    dset_temp_entr = f.create_dataset('temp_entr', (max_time1//stepsize1 + (max_time2- max_time1)//stepsize2 + 1, max_time2 + stepsize2),dtype=np.float_)
-    dset_entangl_specrt = f.create_dataset('entangl_spectr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, max_time2 + stepsize2, 8*(max_time2 + stepsize2)),dtype=np.float_)
-    dset_IM_exponent = f.create_dataset('IM_exponent', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 4 * (max_time2 + stepsize2), 4 * (max_time2 + stepsize2)),dtype=np.complex_)
-    dset_edge_corr = f.create_dataset('edge_corr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 2 * (4*(max_time2 + stepsize2) - 1), 2 * (4*(max_time2 + stepsize2) - 1) ),dtype=np.complex_)
-    #dset = f.create_dataset('bulk_corr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 2 * (4*(max_time2 + stepsize2) - 1) *nsites, 2 * (4*(max_time2 + stepsize2) - 1) *nsites),dtype=np.complex_)
-    dset_init_BCS_state = f.create_dataset('init_BCS_state', (nsites, nsites),dtype=np.complex_)
-    dset_const_blip = f.create_dataset('const_blip', (max_time1//stepsize1 + (max_time2- max_time1)//stepsize2 + 1,),dtype=np.float_)
+
+iterator = 0 # iterator at beginning of job. If file exist and job is continued, this will be set to the appropraite value below
+# total_time = 0 means one floquet-layer
+
+if os.path.isfile(filename+".hdf5") and write_mode != 'w':
+    with h5py.File(filename + '.hdf5', 'r') as f:
+        crit = True
+        print(f['temp_entr'][:,0])
+        while crit:
+            crit = (f['temp_entr'][iterator,0] > 0)
+            iterator += 1
+        iterator -= 1
+    print('File exists: continuing from iteration' , iterator, '..')
+
+else:
+    print('Create/Truncate File..')
+    with h5py.File(filename + ".hdf5", 'w') as f:
+        dset_temp_entr = f.create_dataset('temp_entr', (max_time1//stepsize1 + (max_time2- max_time1)//stepsize2 + 1, max_time2 + stepsize2),dtype=np.float_)
+        dset_entangl_specrt = f.create_dataset('entangl_spectr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, max_time2 + stepsize2, 8*(max_time2 + stepsize2)),dtype=np.float_)
+        dset_IM_exponent = f.create_dataset('IM_exponent', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 4 * (max_time2 + stepsize2), 4 * (max_time2 + stepsize2)),dtype=np.complex_)
+        dset_edge_corr = f.create_dataset('edge_corr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 2 * (4*(max_time2 + stepsize2) - 1), 2 * (4*(max_time2 + stepsize2) - 1) ),dtype=np.complex_)
+        #dset = f.create_dataset('bulk_corr', (int(max_time1/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 2 * (4*(max_time2 + stepsize2) - 1) *nsites, 2 * (4*(max_time2 + stepsize2) - 1) *nsites),dtype=np.complex_)
+        dset_init_BCS_state = f.create_dataset('init_BCS_state', (nsites, nsites),dtype=np.complex_)
+        dset_const_blip = f.create_dataset('const_blip', (max_time1//stepsize1 + (max_time2- max_time1)//stepsize2 + 1,),dtype=np.float_)
+
 # find generators and matrices which diagonalize the composite Floquet operator:
 #G_XY_even, G_XY_odd, G_g, G_1 = compute_generators(nsites, Jx, Jy, g, beta_tilde)
 #evolution_matrix, F_E_prime, F_E_prime_dagger = evolution_matrix(nsites, G_XY_even, G_XY_odd, G_g, G_1)
@@ -89,13 +107,11 @@ with h5py.File(filename + ".hdf5", 'w') as f:
 #M, M_E, eigenvalues_G_eff, f= matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx, Jy, g)
 #ising_gamma_times, ising_gamma_values = ising_gamma(M,eigenvalues_G_eff, nsites, gamma_test_range)
 
-iterator = 1
-# total_time = 0 means one floquet-layer
+time_array = np.append(np.arange(2, max_time1, stepsize1) , np.arange(max_time1, max_time2, stepsize2))
+print(time_array)
 
-for total_time in range(1, max_time1, stepsize1):
+for nbr_Floquet_layers in time_array[iterator:]:
 
-    nbr_Floquet_layers = total_time + 1
-    
     #n_expect, N_t = dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
     
     #B = IM_exponent(evolution_matrix, N_t, nsites,nbr_Floquet_layers, Jx, Jy, beta_tilde, n_expect)
@@ -110,28 +126,7 @@ for total_time in range(1, max_time1, stepsize1):
             entr_data = f['temp_entr']
             entr_data[iterator,0] = nbr_Floquet_layers
             entr_data[iterator,cut] = float(entropy(correlation_block, nbr_Floquet_layers, cut, iterator, filename))
-    iterator += 1
-
-final_iter_one = iterator - 1
-for total_time in range(final_iter_one + stepsize2, max_time2 + stepsize2, stepsize2):  # 90, nsites = 200,
-#for total_time in range(1, max_time1, stepsize1):
-    nbr_Floquet_layers = total_time + 1
-   
-    #n_expect, N_t = dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger, nbr_Floquet_layers)
-    
-    #B = IM_exponent(evolution_matrix, N_t, nsites,nbr_Floquet_layers, Jx, Jy, beta_tilde, n_expect)
-    N_sites_needed_for_entr = nsites#2*nbr_Floquet_layers 
-    B = gm_integral(Jx,Jy,g,mu_initial_state, beta,N_sites_needed_for_entr,nbr_Floquet_layers, filename, iterator)
-    correlation_block = create_correlation_block(B, nbr_Floquet_layers)
-    time_cuts = np.arange(1, nbr_Floquet_layers)
-    #entropy_values[iterator, 0] = nbr_Floquet_layers
-
-    for cut in time_cuts:
-        #entropy_values[iterator, cut] = entropy(correlation_block, nbr_Floquet_layers, cut)
-        with h5py.File(filename + '.hdf5', 'a') as f:
-            entr_data = f['temp_entr']
-            entr_data[iterator,0] = nbr_Floquet_layers
-            entr_data[iterator,cut] = float(entropy(correlation_block, nbr_Floquet_layers, cut, iterator, filename))
+    print('Finished writing data at iteration', iterator)
     iterator += 1
 
 
