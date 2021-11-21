4b04f7978ca0a3462ff450e1344a4542361a7951
Julian Thoenniss <julthoe@gmail.com>
2021-11-21 13:44:29 +0100

    more efficient writing to hdf5

--- imcode/correlation_approach/te_from_correlations.py
+++ imcode/correlation_approach/te_from_correlations.py
@@ -27,6 +27,9 @@ max_time2 = 40
 stepsize1 = 1
 stepsize2 = 4
 
+time_array = np.append(np.arange(2, max_time1, stepsize1) , np.arange(max_time1, max_time2, stepsize2))
+print(time_array)
+
 write_mode = sys.argv[1] #if the argument is 'w', overwrite file if it exists, otherwise append if it exists
 # lattice sites (in the environment):
 nsites = int(sys.argv[2])
@@ -48,15 +51,15 @@ print('g', g)
 print('beta', beta)
 print('mu_init_state', mu_initial_state)
 
-beta_tilde = np.arctanh(np.tan(Jx) * np.tan(Jy))
-alpha_0_square = (np.cos(2 * Jx) + np.cos(2 * Jy)) / 2.
-gamma_test_range = 6
+#beta_tilde = np.arctanh(np.tan(Jx) * np.tan(Jy))
 
+iterator = 0 # iterator at beginning of job. If file exist and job is continued, this will be set to the appropraite value below
+# total_time = 0 means one floquet-layer
 # define initial density matrix and determine matrix which diagonalizes it:
 # this is the EXPONENT of the BARE gaussian density matrix
-rho_0_exponent = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
+#rho_0_exponent = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
 # must be initialized as matrix that diagonalizes dressed density matrix
-N_t = np.identity(2 * nsites, dtype=np.complex_)
+#N_t = np.identity(2 * nsites, dtype=np.complex_)
 
 
 #set location for data storage
@@ -72,12 +75,9 @@ print('filename:', filename)
 # initialize arrays in which entropy values and corresponding time-cuts are stored
 #entropy_values = np.zeros((int(max_time1/stepsize1) +int(max_time2/stepsize2) + 3, max_time2 + stepsize2))  # entropies
 #times = np.zeros(int(max_time1/stepsize1) + int(max_time2/stepsize2))  # time cuts
-ising_gamma_times = np.zeros(gamma_test_range)
-ising_gamma_values = np.zeros(gamma_test_range)
 
 
-iterator = 0 # iterator at beginning of job. If file exist and job is continued, this will be set to the appropraite value below
-# total_time = 0 means one floquet-layer
+
 
 if os.path.isfile(filename+".hdf5") and write_mode != 'w':
     with h5py.File(filename + '.hdf5', 'r') as f:
@@ -107,8 +107,6 @@ else:
 #M, M_E, eigenvalues_G_eff, f= matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx, Jy, g)
 #ising_gamma_times, ising_gamma_values = ising_gamma(M,eigenvalues_G_eff, nsites, gamma_test_range)
 
-time_array = np.append(np.arange(2, max_time1, stepsize1) , np.arange(max_time1, max_time2, stepsize2))
-print(time_array)
 
 for nbr_Floquet_layers in time_array[iterator:]:
 
@@ -120,12 +118,14 @@ for nbr_Floquet_layers in time_array[iterator:]:
     correlation_block = create_correlation_block(B, nbr_Floquet_layers)
     time_cuts = np.arange(1, nbr_Floquet_layers)
     #entropy_values[iterator, 0] = nbr_Floquet_layers
-    for cut in time_cuts:
+    print('Starting to writw data at iteration', iterator)
+    with h5py.File(filename + '.hdf5', 'a') as f:
+        entr_data = f['temp_entr']
+        for cut in time_cuts:
         #entropy_values[iterator, cut] = entropy(correlation_block, nbr_Floquet_layers, cut)
-        with h5py.File(filename + '.hdf5', 'a') as f:
-            entr_data = f['temp_entr']
-            entr_data[iterator,0] = nbr_Floquet_layers
             entr_data[iterator,cut] = float(entropy(correlation_block, nbr_Floquet_layers, cut, iterator, filename))
+        
+        entr_data[iterator,0] = nbr_Floquet_layers#write array with times
     print('Finished writing data at iteration', iterator)
     iterator += 1
 
