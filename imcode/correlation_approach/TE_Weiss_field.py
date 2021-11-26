7a92b9caa73976ba3ebf97dffcf5de085b466024
Julian Thoenniss <julthoe@gmail.com>
2021-11-26 13:15:07 +0100

    compute TE of Weiss field

---
+++ imcode/correlation_approach/TE_Weiss_field.py
@@ -0,0 +1,93 @@
+from Lohschmidt import Lohschmidt
+from evolution_matrix import evolution_matrix
+from compute_generators import compute_generators
+import numpy as np
+import h5py
+from create_correlation_block import create_correlation_block
+from entropy import entropy
+from IM_exponent import IM_exponent
+from add_cmplx_random_antisym import add_cmplx_random_antisym
+from plot_entropy import plot_entropy
+from matrix_diag import matrix_diag
+from dress_density_matrix import dress_density_matrix
+from compute_generators import compute_generators
+from Lohschmidt import Lohschmidt
+from ising_gamma import ising_gamma
+import sys
+import pandas as pd
+from gm_integral import gm_integral
+import os
+import math
+
+
+def guess(text):
+    for t in text.split(','):
+        for typ in (int, float, str):
+            try:
+                yield typ(t)
+                break
+            except ValueError as e:
+                pass
+
+np.set_printoptions(linewidth=np.nan, precision=5, suppress=True)
+
+# define fixed parameters:
+# step sizes for total times t
+
+
+
+#set location for data storage
+mac_path = '/Users/julianthoenniss/Documents/Studium/PhD/data/correlation_approach/'
+work_path = '/Users/julianthoenniss/Documents/PhD/data/'
+fiteo1_path = '/home/thoennis/data/correlation_approach/'
+baobab_path = '/home/users/t/thoennis/scratch/'
+
+filename = work_path + 'TE_from_weiss'
+
+
+Weiss_data_file = '/Users/julianthoenniss/Downloads/weiss_field_test/Weiss_tt.dat'
+
+ntimes = 512
+# Read the data.
+with open(Weiss_data_file, 'r') as fh:
+    lines = fh.readlines()
+
+# Remove newlines, tabs, and split each string separated by spaces.
+clean = [line.strip().replace('\t', '').split() for line in lines]
+
+# Feed the data into a DataFrame.
+data = pd.DataFrame(clean[:], columns=clean[0]).to_numpy()
+
+B = np.zeros((ntimes,ntimes))
+
+for i in range(ntimes):
+    B[i,:] = data[i*ntimes + i:(i+1)*ntimes +i,2]
+
+
+with h5py.File(filename + ".hdf5", 'w') as f:
+    dset_temp_entr = f.create_dataset('temp_entr', (1,ntimes),dtype=np.float_)
+    dset_entangl_specrt = f.create_dataset('entangl_spectr', (1,2 * ntimes,2 * ntimes),dtype=np.float_)
+    dset_IM_exponent = f.create_dataset('IM_exponent', (ntimes,ntimes),dtype=np.float_)
+
+correlation_block = create_correlation_block(B, ntimes)
+time_cuts = np.arange(1, 50)
+
+with h5py.File(filename + '.hdf5', 'a') as f:
+    entr_data = f['temp_entr']
+    Weiss_data = f['IM_exponent']
+
+    Weiss_data[:,:] = B[:,:]
+    
+    for cut in time_cuts:
+        print('calculating entropy at time cut:', cut)
+        ent_val = float(entropy(correlation_block, ntimes, cut,0, filename))
+        entr_data[0,cut] = ent_val
+        print('ent_val', ent_val)
+
+
+
+
+with h5py.File(filename + '.hdf5', 'r') as f:
+   entr_data = f['temp_entr']
+   np.set_printoptions(linewidth=np.nan, precision=10, suppress=True)
+   print(entr_data[:])
