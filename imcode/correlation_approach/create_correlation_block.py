50b71307d843008dfa224293360bf04e3746dd29
Julian Thoenniss <julthoe@gmail.com>
2021-11-26 13:19:23 +0100

    make size of correlation block independent of DoF per time

--- imcode/correlation_approach/create_correlation_block.py
+++ imcode/correlation_approach/create_correlation_block.py
@@ -6,7 +6,8 @@ from scipy import linalg
 np.set_printoptions(suppress=False, linewidth=np.nan)
 #@profile
 def create_correlation_block(B, ntimes):
-    dim_B = 4 * ntimes
+    dim_B = B.shape[0]
+    print('dimB',dim_B,B.shape[0],4*ntimes)
 
     print('Creating correlation block for total time', ntimes)
     R = np.zeros((dim_B, dim_B), dtype=np.complex_)
@@ -16,8 +17,8 @@ def create_correlation_block(B, ntimes):
     now = datetime.now()
     print('End diag of B', now)
     #compute correation block in diagonal basis with only entries this phases of fermionic operators are defined such that the eigenvalues of B are real
-    corr_block_diag = np.zeros((8 * ntimes, 8 * ntimes))
-    for i in range(0, 2 * ntimes):
+    corr_block_diag = np.zeros((2 * dim_B, 2 * dim_B))
+    for i in range(0, dim_B // 2):
         ew = eigenvalues[i]
         norm = 1 + abs(ew)**2
         corr_block_diag[2 * i, 2 * i] = 1/norm
