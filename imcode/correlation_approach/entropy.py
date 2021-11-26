dbc08ea1cca2c2749a8b844339791a56a31e54cf
Julian Thoenniss <julthoe@gmail.com>
2021-11-26 13:18:51 +0100

    allow to compute TE for Weiss field

--- imcode/correlation_approach/entropy.py
+++ imcode/correlation_approach/entropy.py
@@ -5,33 +5,39 @@ np.set_printoptions(suppress=False, linewidth=np.nan)
 
 
 def entropy(correlation_block, ntimes, time_cut, iterator,filename):
+
+    half_dim_CB = correlation_block.shape[0] // 2
+
     if time_cut == 0:
         # take this as default value if nothing has been specified otherwise
         time_cut = max(ntimes / 2, 1)
 
-    
-    """#for correlation approach
-    correlation_block_reduced = np.bmat([[correlation_block[0: 4 * time_cut, 0:  4 * time_cut], correlation_block[0: 4 * time_cut, 4 * ntimes: 4 * (time_cut + ntimes)]], [
-        correlation_block[4 * ntimes: 4 * (time_cut + ntimes), 0:  4 * time_cut], correlation_block[4 * ntimes: 4 * (time_cut + ntimes), 4 * ntimes: 4 * (time_cut + ntimes)]]])
+    """
+     #for correlation approach
+    Delta_half = 2. * time_cut / (8. * ntimes / correlation_block.shape[0])#half the inverval that we define as subsystem    
+    print(Delta_half)
+    correlation_block_reduced = np.bmat([[correlation_block[0: int(2 * Delta_half), 0:  int(2 * Delta_half)], correlation_block[0: int(2 * Delta_half), half_dim_CB: half_dim_CB + int(2 * Delta_half)]], [
+        correlation_block[half_dim_CB: half_dim_CB + int(2 * Delta_half), 0:  int(2 * Delta_half)], correlation_block[half_dim_CB: half_dim_CB + int(2 * Delta_half), half_dim_CB: half_dim_CB + int(2 * Delta_half)]]])
     """
     
+    
     #for GM-approach
-    Delta_half = 2 * time_cut#half the inverval that we define as subsystem
-    correlation_block_reduced = np.bmat([[correlation_block[2 * ntimes - Delta_half: 2 * ntimes + Delta_half, 2 * ntimes - Delta_half: 2 * ntimes + Delta_half], correlation_block[2 * ntimes - Delta_half: 2 * ntimes + Delta_half, 6 * ntimes - Delta_half:6 * ntimes + Delta_half]], [
-        correlation_block[6 * ntimes - Delta_half: 6 * ntimes + Delta_half,2 * ntimes - Delta_half: 2 * ntimes + Delta_half], correlation_block[6 * ntimes - Delta_half:6 * ntimes + Delta_half, 6 * ntimes - Delta_half: 6 * ntimes + Delta_half]]])
     
+    Delta_half = 2 * time_cut
+    correlation_block_reduced = np.bmat([[correlation_block[half_dim_CB // 2 - Delta_half: half_dim_CB // 2 + Delta_half, half_dim_CB // 2 - Delta_half: half_dim_CB // 2 + Delta_half], correlation_block[half_dim_CB // 2 - Delta_half: half_dim_CB // 2 + Delta_half, 3 * (half_dim_CB // 2) - Delta_half:3 * (half_dim_CB // 2) + Delta_half]], [
+        correlation_block[3 * (half_dim_CB // 2) - Delta_half: 3 * (half_dim_CB // 2) + Delta_half, (half_dim_CB // 2) - Delta_half: (half_dim_CB // 2) + Delta_half], correlation_block[3 * (half_dim_CB // 2) - Delta_half:3 * (half_dim_CB // 2) + Delta_half, 3 * (half_dim_CB // 2) - Delta_half: 3 * (half_dim_CB // 2) + Delta_half]]])
     
-    
+
     # print (correlation_block_reduced)
     eigenvalues_correlations, ev_correlations = linalg.eig(correlation_block_reduced)
     eigenvalues_correlations[::-1].sort()
-
+    print(eigenvalues_correlations)
     with h5py.File(filename + '.hdf5', 'a') as f:
             data = f['entangl_spectr']
             data[iterator, time_cut,0:len(eigenvalues_correlations)] = np.array(np.real(eigenvalues_correlations))
 
     entropy = 0
-    for i in range(0, 4 * time_cut, 1):
+    for i in range(correlation_block_reduced.shape[0] // 2):
         kappa = eigenvalues_correlations[i]
         if kappa < 1:  # for kappa = 1, entropy has no contribution
             entropy += - kappa * np.log(kappa) - (1-kappa) * np.log(1-kappa)
