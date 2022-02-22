72f55553316b1b3b39a8336984363ffa8e661568
Julian Thoenniss <julthoe@gmail.com>
2022-02-22 16:03:32 +0100

    correct for FS initial state

--- imcode/correlation_approach/dress_density_matrix.py
+++ imcode/correlation_approach/dress_density_matrix.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.linalg import eig, matrix_power
+from numpy.linalg import eig, matrix_power, det
 from scipy.linalg import expm,logm
 from scipy import linalg
 import matplotlib.pyplot as plt
@@ -11,48 +11,51 @@ from scipy.linalg import sqrtm
 def dress_density_matrix(rho_0_exponent, F_E_prime, F_E_prime_dagger,M,M_E, eigenvalues_G_eff,  eigenvalues_G_eff_E, beta_tilde, nbr_Floquet_layers,init_state):
     nsites = int (len(rho_0_exponent[0]) / 2)
 
-    rho_0_single_body = expm(- rho_0_exponent) @ expm(- rho_0_exponent)
+    rho_0_single_body = 0.25*expm(- rho_0_exponent) @ expm(- rho_0_exponent)#rho_exponent = 0 corresponds to infinite temperature
     if init_state == 2:
         rho_0_single_body = M_E @ np.bmat([[np.identity(nsites),np.zeros((nsites,nsites))],[np.zeros((nsites,nsites)),np.zeros((nsites,nsites))] ]) @ M_E.T.conj() 
    
     # the following line assumes that rho_0_exponent is given in real space basis..
-    rho_dressed =  sqrtm(matrix_power(F_E_prime, nbr_Floquet_layers)  @ rho_0_single_body @  matrix_power( F_E_prime_dagger, nbr_Floquet_layers) )
+    rho_dressed =  sqrtm(matrix_power(F_E_prime,nbr_Floquet_layers)  @ rho_0_single_body @  matrix_power( F_E_prime_dagger,nbr_Floquet_layers) )
 
     eigenvals_dressed, eigenvecs_dressed = linalg.eigh(rho_dressed)
 
-    argsort = np.argsort(-eigenvals_dressed)
 
-    print('argsort',argsort[:])
+    argsort = np.argsort(-eigenvals_dressed)
+    
     cum = 0
     for i in range(nsites):
         cum += abs(eigenvals_dressed[i] - eigenvals_dressed[i + nsites])
 
-    
     if cum > 1e-6:#if half of the spectrum is zero (as for Bog. eigenstate at critical point in xy model, the eigenvectors of zero eigenvalues are messed up. Therefore, construct rotation matric only from other eigenvectors with nonzero eigenvalues (possible by symmetrey). Order in such a way that die diagonalized matrix has first nsitmes eigenvalues eqaul to zero and others the remaining ones)
     #By knowledge of the structure of N, i.e. (N = [[A, B^*],[B, A^*]]), we can construct the right part of the matrix from the left part of the matrix "eigenvecs_dressed", such that all phase factors and the order of eigenvectors are as desired.
         N_t =  np.bmat([[eigenvecs_dressed[nsites:2*nsites,argsort[:nsites]].conj(), eigenvecs_dressed[0:nsites,argsort[:nsites]]],[eigenvecs_dressed[0:nsites,argsort[:nsites]].conj(),eigenvecs_dressed[nsites:2*nsites,argsort[:nsites]]]]) 
+        eigenvals_dressed[:] = np.concatenate((eigenvals_dressed[argsort[2*nsites-1:nsites-1:-1]],eigenvals_dressed[argsort[:nsites]]))
     else: 
-        N_t = np.identity(eigenvecs_dressed.shape[0]) 
+        N_t = np.identity(eigenvecs_dressed.shape[0])
 
+    """#print ('N_t\n',N_t)
+    N_t = eigenvecs_dressed
+    diag_check = N_t.T.conj() @ rho_dressed @ N_t
 
     
-      
-    #print ('N_t\n',N_t)
-    #diag_check = N_t.T.conj() @ rho_dressed @ N_t
-    #diag_check = linalg.inv(M[0]) @ rho_dressed @ linalg.inv(M_1_shift.T)
-    #eigenvals_dressed = -np.diag(diag_check)
-    #print('Dressed density matrix diagonalized \n', diag_check)
-    #print('Eigenvalues of exponent:', eigenvals_dressed)
+    eigenvals_dressed = np.real(np.diag(diag_check))
+    print('Dressed density matrix diagonalized \n', diag_check)
+    #print('Eigenvalues of exponent:', eigenvals_dressed)"""
 
     np.set_printoptions(linewidth=np.nan, precision=6, suppress=True)
     n_expect = np.zeros((2 * nsites))#fermi-Dirac distribution for modes (basis in which dressed density matrix is diagonal). 
     #Note that the true expectation value has an additional factor Z_over_Z0. This factor, however, does not enter the exponent of the IM and is therefore not included here.
     for k in range (nsites):
-        eval = eigenvals_dressed[k]
         #n_expect[k] = np.exp(+eval)  / (2 * np.cosh(eval) ) # for < c^dagger c >
-        n_expect[k] = eval  / (eval + 1/eval ) # for < c^dagger c >
-        n_expect[k + nsites] = 1 - n_expect[k] # for < c c^dagger >
+       
+        n_expect[k] = eigenvals_dressed[k] / (eigenvals_dressed[k] + eigenvals_dressed[k+nsites]) ## for < c^dagger c >
+        n_expect[k + nsites] =  eigenvals_dressed[k+nsites]  / (eigenvals_dressed[k]+ eigenvals_dressed[k+nsites])# # for < c c^dagger >
         #print(eval)
     #print('nexpext',n_expect)
-
-    return n_expect, N_t
+    #Z_t =0
+    #for i in range (nsites):
+    #    Z_t += (eigenvals_dressed[k] + eigenvals_dressed[k+nsites])
+    #Z_t = 0np.trace(rho_dressed)
+   
+    return n_expect, N_t, rho_dressed#/Z_t
