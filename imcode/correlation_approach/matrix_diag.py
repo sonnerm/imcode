d723f1723090281e7e3c2bb1ab297f145d9679d1
Julian Thoenniss <julthoe@gmail.com>
2022-02-22 16:03:01 +0100

    crucial stabilization for large systems

--- imcode/correlation_approach/matrix_diag.py
+++ imcode/correlation_approach/matrix_diag.py
@@ -28,15 +28,14 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     #print(G_g)
      # unitary gate is exp-product of exponentiated generators
     # gate that describes evolution non disconnected environment. first dimension: 0 = forward branch, 1 = backward branch
-    U_E = np.zeros((2, 2 * nsites, 2 * nsites), dtype=np.complex_)
+    U_E = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
     # gate that governs time evolution on both branches. first dimension: 0 = forward branch, 1 = backward branch
     U_eff = np.zeros((2, 2 * nsites, 2 * nsites), dtype=np.complex_)
 
 
     evol = expm(1.j*G_g) @ expm(1.j * G_XY_even) @ expm(1.j * G_XY_odd)   
 
-    U_E[0] = evol.T.conj()
-    U_E[1] = evol.T.conj()
+    U_E = evol.T.conj()
 
     U_eff[0] = expm(-G_1) @ evol.T.conj()  # forward branch
     U_eff[1] = expm(G_1) @ evol.T.conj()   # backward branch
@@ -47,7 +46,7 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     #print( 'U_E[1] = expm(1j*G_g) @ U_E[1]')
    
     # generator of environment (always unitary)
-    G_eff_E = -1j * linalg.logm(U_E[0])
+    G_eff_E = -1j * linalg.logm(U_E)
     #print('G_eff_E')
     #print(G_eff_E)
    
@@ -62,12 +61,13 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     #print(G_eff[1]-G_eff[0])
     
     # add small random part to G_eff to lift degenaracies, such that numerical diagnoalization is more stable
-    rand_magn = 1e-8
-    rand_A = np.random.rand(nsites,nsites) * rand_magn
+    rand_magn = 1.e-7
+    rand_A = (np.random.rand(nsites,nsites) + 1.j * np.random.rand(nsites,nsites)) * rand_magn
     rand_B = add_cmplx_random_antisym(np.zeros((nsites,nsites), dtype=np.complex_), rand_magn)
     rand_C = add_cmplx_random_antisym(np.zeros((nsites,nsites), dtype=np.complex_), rand_magn)
     random_part = np.bmat([[rand_A,rand_B], [rand_C, -rand_A.T]])
 
+    
     #G_eff_E += random_part
     G_eff[0] += random_part
     G_eff[1] += random_part
@@ -77,7 +77,7 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     eigenvalues_G_eff = np.zeros((2, 2 * nsites), dtype=np.complex_)
     #eigenvalues_G_eff_bw = np.zeros(2 * nsites, dtype=np.complex_)
     # no need for seperate forward and backward branch since F_E is always unitary, time evolution is equivalent on both branches
-    eigenvalues_G_eff_E = np.zeros(2 * nsites, dtype=np.complex_)
+    #eigenvalues_G_eff_E = np.zeros(2 * nsites, dtype=np.complex_)
     # first dimension: foward branch (index 0) and backward branch (index 1)
     eigenvectors_G_eff = np.zeros(
         (2, 2 * nsites, 2 * nsites), dtype=np.complex_)
@@ -93,14 +93,16 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
         eigenvalues_G_eff[1], eigenvectors_G_eff[1] = linalg.eig(
             0.5 * (G_eff[1] + G_eff[1].conj().T))
         # take superposition with hermitian conjugate to stabilize numerical diagonalization (works only in unitary case, e.g. Ising-type coupling)
-        eigenvalues_G_eff_E, eigenvectors_G_eff_E = linalg.eig(
+        eigenvalues_G_eff_E, eigenvectors_G_eff_E = linalg.eigh(
             0.5 * (G_eff_E + G_eff_E.conj().T))
 
     else:
         eigenvalues_G_eff[0], eigenvectors_G_eff[0] = linalg.eig(G_eff[0])
         eigenvalues_G_eff[1], eigenvectors_G_eff[1] = linalg.eig(G_eff[1])
         #eigenvalues_G_eff_bw, eigenvectors_G_eff_bw = linalg.eig(G_eff_bw)
-        eigenvalues_G_eff_E, eigenvectors_G_eff_E = linalg.eigh(G_eff_E)
+      
+        eigenvalues_G_eff_E, eigenvectors_G_eff_E = linalg.eigh( G_eff_E + random_part + random_part.T.conj())
+        #eigenvalues_G_eff_E, eigenvectors_G_eff_E = linalg.eig(U_E)
 
     # check if found eigenvectors indeed fulfill eigenvector equation (trivial check)
     eigenvector_check = 0
@@ -130,7 +132,8 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     argsort_fw = np.argsort(- np.real(eigenvalues_G_eff[0]))
     argsort_bw = np.argsort(- np.real(eigenvalues_G_eff[1]))
     #argsort_bw = np.argsort(- np.real(eigenvalues_G_eff_bw))
-    argsort_E = np.argsort(- np.real(eigenvalues_G_eff_E))
+    argsort_E = np.argsort(- eigenvalues_G_eff_E)
+    #argsort_E = np.argsort(1.j*np.log(eigenvalues_G_eff_E))
 
     #eigenvalues_G_eff[0] = eigenvalues_G_eff[0,argsort_fw[:]]
     #eigenvalues_G_eff[1] = eigenvalues_G_eff[1,argsort_bw[:]]
@@ -141,14 +144,16 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
 
     M = np.zeros((2, 2 * nsites, 2 * nsites), dtype=np.complex_)
     #M_bw = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
-    M_E = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
+    #M_E = np.zeros((2 * nsites, 2 * nsites), dtype=np.complex_)
     for i in range(nsites):  # sort eigenvectors and eigenvalues such that the first half are the ones with positive real part, and the second half have negative real parts
         M[0, :, i] = eigenvectors_G_eff[0, :, argsort_fw[i]]
         M[0, :, 2 * nsites - 1 - i] = eigenvectors_G_eff[0, :, argsort_fw[i + nsites]]
         M[1, :, i] = eigenvectors_G_eff[1, :, argsort_bw[i]]
         M[1, :, 2 * nsites - 1 - i] = eigenvectors_G_eff[1, :, argsort_bw[i + nsites]]
-        M_E[:, i] = eigenvectors_G_eff_E[:, argsort_E[i]]
-        M_E[:, 2 * nsites - 1 - i] = eigenvectors_G_eff_E[:, argsort_E[i + nsites]]
+        #M_E[:, i] = eigenvectors_G_eff_E[:, argsort_E[i]]
+        #M_E[:, 2 * nsites - 1 - i] = eigenvectors_G_eff_E[:, argsort_E[i + nsites]]
+    M_E =  np.bmat([[eigenvectors_G_eff_E[nsites:2*nsites,argsort_E[:nsites]].conj(), eigenvectors_G_eff_E[0:nsites,argsort_E[:nsites]]],[eigenvectors_G_eff_E[0:nsites,argsort_E[:nsites]].conj(),eigenvectors_G_eff_E[nsites:2*nsites,argsort_E[:nsites]]]]) 
+
 
     #M1_inv = linalg.inv(M[1])
     #M0_inv = linalg.inv(M[0])
@@ -207,21 +212,29 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     # diagonalize G_eff with eigenvectors to check:
     M_inverse[0] = linalg.inv(M[0])
     M_inverse[1] = linalg.inv(M[1])
-    M_E_inverse = linalg.inv(M_E)
+    #M_E_inverse = linalg.inv(M_E)
 
     D = np.zeros((2, 2 * nsites, 2 * nsites), dtype=np.complex_)
     for branch in range(2):
         D[branch] = M_inverse[branch] @ G_eff[branch] @ M[branch]
+        #print(M[(branch+1)%2].T.conj() @ G_eff[branch] @ M_inverse[(branch+1)%2].T.conj())
         #print(branch, '\n',D[branch])
         # this is the diagonal matrix with eigenvalues of G_eff on the diagonal
         # this makes sure that the order of the eigenvalues corresponds to the order of the eigenvectors in the matrix M
         eigenvalues_G_eff[branch] = D[branch].diagonal()
-
-    D_E = M_E_inverse @ G_eff_E @ M_E
-    #print(D_E)
+ 
+    
     # this is the diagonal matrix with eigenvalues of G_eff on the diagonal
     # this makes sure that the order of the eigenvalues corresponds to the order of the eigenvectors in the matrix M
-    eigenvalues_G_eff_E = D_E.diagonal()
+    eigenvalues_G_eff_E =np.concatenate((eigenvalues_G_eff_E[argsort_E[2*nsites-1:nsites-1:-1]],eigenvalues_G_eff_E[argsort_E[:nsites]]))
+    #print(eigenvalues_G_eff_E)
+    #D_E = M_E.T.conj() @ G_eff_E @ M_E
+    #print(D_E)
+    #print( linalg.inv(M_E)  - M_E.T.conj())
+    #eigenvalues_G_eff_E = D_E.diagonal()
+    #print(np.real(eigenvalues_G_eff_E))
+    #eigenvalues_G_eff_E, M_E = linalg.eig(U_E)
+    #print(np.real(-1.j*np.log(eigenvalues_G_eff_E)))
     """
     #ax[1].plot(k_vals,eigenvalues_G_eff_E[0:nsites],color='C0')
     #ax[1].plot(k_vals,eigenvalues_G_eff_E[nsites:2*nsites],color='C0')
@@ -265,7 +278,7 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     #print(D[0])
     #print('D_E= ')
     #print(D_E)
-
+    """
     # check if diagonalization worked
     diag_check = 0
     #diag_check_bw = 0
@@ -283,7 +296,7 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     for k in range(nsites):
         f += abs(M_E[0, k])**2 - abs(M_E[nsites, k])**2 + \
             2j * imag(M_E[0, k]*M_E[nsites, k].conj())
-    print ('f', f)
+    print ('f', f)"""
     #eigenvalues_G_eff = ews_sorted
     #print('eigenvalues of G_eff_fw: ')
     #print(eigenvalues_G_eff[0])
@@ -301,4 +314,4 @@ def matrix_diag(nsites, G_XY_even, G_XY_odd, G_g, G_1, Jx=0, Jy=0, g=0):
     
     # return M_fw, M_fw_inverse, M_bw, M_bw_inverse,  eigenvalues_G_eff_fw, eigenvalues_G_eff_bw, f
     
-    return M, M_E, eigenvalues_G_eff, eigenvalues_G_eff_E, f
+    return M, M_E, eigenvalues_G_eff, eigenvalues_G_eff_E
