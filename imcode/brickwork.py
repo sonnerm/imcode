ising_F runs now, wrong result

imcode/brickwork.py
@@ -8,17 +8,15 @@ def brickwork_Fe(L,gatese):
     if L%2==0:
         Bs=gatese
     else:
-        gatebound=np.eye(2)[None,...,None]
-        Bs=list(gatese)+[gatebound]
-    return tt.frommatrices(Bs).recluster(((2,),)*L)
+        Bs=list(gatese)+[np.eye(2)]
+    return tt.fromproduct(Bs).recluster(((2,2),)*L)
 
 def brickwork_Fo(L,gateso):
-    gatebound=np.eye(2)[None,...,None]
     if L%2==0:
-        Bs=[gatebound]+gateso+[gatebound]
+        Bs=[np.eye(2)]+gateso+[np.eye(2)]
     else:
-        Bs=[gatebound]+gateso
-    return tt.frommatrices(Bs).recluster(((2,),)*L)
+        Bs=[np.eye(2)]+gateso
+    return tt.fromproduct(Bs).recluster(((2,2),)*L)
 
 def brickwork_F(L,gates,reversed=False):
     if not reversed:

imcode/ising.py
@@ -1,6 +1,7 @@
 import numpy as np
 import ttarray as tt
-from . import SX,SZ,ID
+from . import SX,SZ,ID,ZE
+from . import brickwork
 def ising_H(L,J,g,h):
     J=np.asarray(J)
     g=np.asarray(g)
@@ -30,33 +31,21 @@ def ising_F(L,J,g,h):
         g=np.tile(g,L)
     if len(h.shape)==0:
         h=np.tile(h,L)
-    Wg=[np.array([[[[np.cos(gc),1.0j*np.sin(gc)],[1.0j*np.sin(gc),np.cos(gc)]]]]) for gc in g]
-    Wh=[np.array([[[[np.exp(1.0j*hc),0],[0,np.exp(-1.0j*hc)]]]]) for hc in h]
+    Wloc=[np.diag([np.exp(1.0j*hc),np.exp(-1.0j*hc)])@np.array([[np.cos(gc),1.0j*np.sin(gc)],[1.0j*np.sin(gc),np.cos(gc)]]) for hc,gc in zip(h,g)]
+    mploc=tt.fromproduct(Wloc)
     if L==1:
-        return multiply_mpos([mph,mpg])
+        return mploc
     if len(J.shape)==0:
         J=np.tile(J,L-1)
-    UP=np.array([[1,0],[0,0]])
-    DO=np.array([[0,0],[0,1]])
-    WJ=[np.array([[UP,DO]])]
-    for Jc in J[:-1]:
-        wjc=np.array([[UP*np.exp(1.0j*Jc),DO*np.exp(-1.0j*Jc)],[UP*np.exp(-1.0j*Jc),DO*np.exp(1.0j*Jc)]])
-        WJ.append(wjc)
-    UI=np.array([[np.exp(1.0j*J[-1]),0],[0,np.exp(-1.0j*J[-1])]])
-    DI=np.array([[np.exp(-1.0j*J[-1]),0],[0,np.exp(1.0j*J[-1])]])
-    wjc=np.array([[UI],[DI]])
-    WJ.append(wjc)
-    mpJ=tt.frommatrices(WJ)
-    return mpJ@mph@mpg
-def ising_gate(J,g,h):
+    mpJ=brickwork.brickwork_F(L,[np.diag([np.exp(1.0j*Jc),np.exp(-1.0j*Jc),np.exp(-1.0j*Jc),np.exp(1.0j*Jc)]) for Jc in J])
+    return mpJ@mploc
+def ising_zoz(J,g,h):
     pass
-def ising_lop(t,ch,init=np.eye(2)/2,final=np.eye(2)):
+
+def ising_lop(t,ch):
     pass
 def ising_J(t,J):
     pass
 
-def ising_T(t,J,g,h,init=np.eye(2)/2,final=np.eye(2)):
-    init=np.array(init)
-    if len(init.shape)==2:
-        init=init[None,...,None]
-    return (ising_J(t,J)@ising_g(t,g,init,final)@ising_h(t,h)).contract()
+def ising_T(t,J,g,h):
+    return ising_J(t,J)@ising_lop(t,g,h)

tests/ising/test_ising_FH.py
@@ -1,16 +1,32 @@
 import imcode
 import scipy.linalg as scla
+from imcode import SX,SZ,ID
 import numpy as np
 import pytest
+import functools
+import ttarray as tt
+
+def simple_ising_F(L, J, g, h):
+    return scla.expm(1.0j * np.array(imcode.ising_H(L,J, [0.0] * L, h)) @ scla.expm(1.0j * np.array(imcode.ising_H(L,[0.0] * L, g, [0.0] * L))))
+
+def simple_ising_H(L,J,g,h):
+    ret = np.zeros((2**L,2**L),dtype=complex)
+    for i in range(L):
+        if i<L-1:
+            ret += J[i]*mkron([ID]*i+[SZ,SZ]+[ID]*(L-i-2))
+        ret += h[i]*mkron([ID]*i+[SZ]+[ID]*(L-i-1))
+        ret += g[i]*mkron([ID]*i+[SX]+[ID]*(L-i-1))
+    return ret
+def mkron(args):
+    return functools.reduce(np.kron,args)
+
 def test_ising_F_complex(seed_rng):
     L=5
-    J = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
+    J = np.random.normal(size=L-1) + 1.0j * np.random.normal(size=L-1)
     g = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
     h = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
     # simple implementation of ising_F, to be compared to future implementations
     miF=imcode.ising_F(L,J,g,h)
-    def simple_ising_F(J, g, h):
-        return scla.expm(1.0j * np.array(imcode.ising_H(L,J, [0.0] * L, h)) @ scla.expm(1.0j * np.array(imcode.ising_H(L,[0.0] * L, g, [0.0] * L))))
     diF=np.array(miF)
     assert diF.dtype==np.complex_
     assert diF.conj().T@diF!=pytest.approx(np.eye(diF.shape[0])) #not unitary
@@ -18,16 +34,14 @@ def test_ising_F_complex(seed_rng):
     assert diF.T.conj()!=pytest.approx(diF)
     assert diF.T!=pytest.approx(diF)
     assert diF.conj()!=pytest.approx(diF)
-    assert simple_ising_F(J, g, h) == pytest.approx(diF)
+    assert simple_ising_F(L, J, g, h) == pytest.approx(diF)
 
 def test_ising_F_real(seed_rng):
     L=5
-    J = np.random.normal(size=L)
+    J = np.random.normal(size=L-1)
     g = np.random.normal(size=L)
     h = np.random.normal(size=L)
     miF=imcode.ising_F(L,J,g,h)
-    def simple_ising_F(J, g, h):
-        return scla.expm(1.0j * np.array(imcode.ising_H(L,J, [0.0] * L, h)) @ scla.expm(1.0j * np.array(imcode.ising_H(L,[0.0] * L, g, [0.0] * L))))
     diF=np.array(miF)
     assert diF.dtype==np.complex_
     assert diF.conj().T@diF==pytest.approx(np.eye(diF.shape[0])) #unitary
@@ -35,16 +49,17 @@ def test_ising_F_real(seed_rng):
     assert diF.T.conj()!=pytest.approx(diF)
     assert diF.T!=pytest.approx(diF)
     assert diF.conj()!=pytest.approx(diF)
-    assert simple_ising_F(J, g, h) == pytest.approx(diF)
+    assert simple_ising_F(L, J, g, h) == pytest.approx(diF)
 
 def test_ising_F_trotter(seed_rng):
     L=5
-    J=np.random.normal(size=(L-1,))+np.random.normal(size=(L-1,))*1.0j
-    g=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    h=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    diF=dense.ising.ising_F(L,J,g,h)
-    miF=mps.ising.ising_F(L,J,g,h)
-    assert diF==pytest.approx(miF.to_dense())
+    J=np.random.normal(size=(L-1,))
+    g=np.random.normal(size=(L,))
+    h=np.random.normal(size=(L,))
+    dt=0.01
+    miF=np.array(imcode.ising_F(L,J*dt,g*dt,h*dt))
+    miH=np.array(imcode.ising_H(L,J*dt,g*dt,h*dt))
+    assert scla.expm(1.0j*miH)==pytest.approx(miF)
 
 def test_ising_H(seed_rng):
     # Build operator manually from basic operators and compare also for complex coefficients <=> non-hermitian operators
@@ -52,63 +67,25 @@ def test_ising_H(seed_rng):
     J = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
     g = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
     h = np.random.normal(size=L) + 1.0j * np.random.normal(size=L)
-    ret = np.zeros_like(dense.one(L),dtype=complex)
-    for i in range(L):
-        ret += dense_sz(L, i) @ dense_sz(L, (i + 1) % L) * J[i]
-        ret += dense_sz(L, i) * h[i]
-        ret += dense_sx(L, i) * g[i]
     miH=imcode.ising_H(L,J, g, h)
-    assert diH.dtype==np.complex_
-    assert ret == pytest.approx(diH)
-    assert (ret - dense.sz(L, 0) @ dense.sz(L, L - 1) * J[-1]) == pytest.approx(dense.ising.ising_H(L, J[:-1], g, h))
-
+    ret=simple_ising_H(L,J,g,h)
+    assert miH.dtype==np.complex_
+    assert isinstance(miH,tt.TensorTrainArray)
+    assert np.array(miH)==pytest.approx(ret)
     # Real coefficients <=> Real Hermitian operator
     J = np.random.normal(size=L)
     g = np.random.normal(size=L)
     h = np.random.normal(size=L)
-    diH=dense.ising.ising_H(L, J, g, h)
-    assert diH.dtype==np.float_
-    assert (diH == diH.T).all()
-    
+    miH=imcode.ising_H(L,J, g, h)
+    ret=simple_ising_H(L,J,g,h)
+    assert miH.dtype==np.float_
+    assert isinstance(miH,tt.TensorTrainArray)
+    assert np.array(miH)==pytest.approx(ret)
+
 def test_single_site_ising_H():
     g = np.random.normal(size=1) + 1.0j * np.random.normal(size=1)
     h = np.random.normal(size=1) + 1.0j * np.random.normal(size=1)
-    diH=dense.ising.ising_H(1,[],g,h)
-    assert diH==pytest.approx(dense.SZ*h+dense.SX*g)
-    diH=dense.ising.ising_H(1,[0.0],g,h)
-    assert diH==pytest.approx(dense.SZ*h+dense.SX*g)
-def test_dense_ising_H_real(seed_rng):
-    L=5
-    J=np.random.normal(size=(L,))
-    g=np.random.normal(size=(L,))
-    h=np.random.normal(size=(L,))
-    diH=dense.ising.ising_H(L,J,g,h)
-    assert diH.dtype==np.float_
-    assert diH.conj()==pytest.approx(diH)
-    assert diH.T.conj()==pytest.approx(diH)
-    assert diH.T==pytest.approx(diH)
-def test_dense_ising_H_complex(seed_rng):
-    L=5
-    J=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    g=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    h=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    diH=dense.ising.ising_H(L,J,g,h)
-    assert diH.dtype==np.complex_
-    #This model is always symmetric
-    assert diH.T==pytest.approx(diH)
-    #ensure that tests cover generic case
-    assert diH.conj()!=pytest.approx(diH)
-    assert diH.T.conj()!=pytest.approx(diH)
-
-def test_dense_ising_H_complex_obc(seed_rng):
-    L=5
-    J=np.random.normal(size=(L-1,))+np.random.normal(size=(L-1,))*1.0j
-    g=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    h=np.random.normal(size=(L,))+np.random.normal(size=(L,))*1.0j
-    diH=dense.ising.ising_H(L,J,g,h)
-    assert diH.dtype==np.complex_
-    #This model is always symmetric
-    assert diH.T==pytest.approx(diH)
-    #ensure that tests cover generic case
-    assert diH.conj()!=pytest.approx(diH)
-    assert diH.T.conj()!=pytest.approx(diH)
+    diH=np.array(imcode.ising_H(1,[],g,h))
+    assert diH==pytest.approx(SZ*h+SX*g)
+    diH=np.array(imcode.ising_H(1,4.0,g,h))
+    assert diH==pytest.approx(SZ*h+SX*g)