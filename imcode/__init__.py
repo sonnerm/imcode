aeef3d05fe979d63bcb6945ddc2525cbd54fdab6
Michael Sonner <sonnerm@gmail.com>
2022-07-06 15:43:35 +0200

    added channel, (un)vectorize

--- imcode/__init__.py
+++ imcode/__init__.py
@@ -7,4 +7,8 @@ ZE=np.array([[0,0],[0,0]])
 from .ising import ising_F,ising_H,ising_T
 from .im_algo import zoz_lcga,brickwork_lcga
 from .brickwork import brickwork_F,brickwork_H
+from .obs import ising_boundary_evolution,ising_embedded_evolution
+from .obs import brickwork_boundary_evolution,brickwork_embedded_evolution
+from .obs import zoz_boundary_evolution,zoz_embedded_evolution
 from .io import savehdf5,loadhdf5
+from .channel import unitary_channel,vectorize_operator,unvectorize_operator
--- imcode/channel.py
+++ imcode/channel.py
@@ -1,31 +1,53 @@
 import numpy as np
 import ttarray as tt
+import math
 
 def unitary_channel(F):
-    Ws=tt.asarray(F).tomatrices_unchecked()
-    Ws=[np.einsum("abcd,efgh->aebfcgdh",W,W.conj()).reshape((W.shape[0]**2,W.shape[1]**2,W.shape[2]**2,W.shape[3]**2)) for W in Ws]
-    return tt.frommatrices(Ws)
+    if len(F.shape)==2:
+        L=math.log2(state.shape[0])
+        F=tt.asarray(F,cluster=((2,2),)*L)
+        Ws=[np.einsum("abcd,efgh->aebfcgdh",W,W.conj()) for W in F.tomatrices_unchecked()]
+        Ws=[W.reshape((W.shape[0]**2,4,4,W.shape[-1]**2)) for W in Ws]
+        return tt.frommatrices(Ws)
+    if len(F.shape)==4:
+        L=math.log2(state.shape[1])
+        F=tt.asslice(F,cluster=((2,2),)*L)
+        Ws=[np.einsum("abcd,efgh->aebfcgdh",W,W.conj()) for W in F.tomatrices_unchecked()]
+        Ws=[W.reshape((W.shape[0]**2,4,4,W.shape[-1]**2)) for W in Ws]
+        return tt.frommatrices_slice(Ws)
+    else:
+        raise ValueError("unitary operator must be either 2D (array) or 4D (slice)")
 
-def dephasing_channel(gamma,basis=np.eye(2)):
-    D=np.diag([1.0,1.0-gamma,1.0-gamma,1.0])
-    U=unitary_channel(basis)
-    return U.T.conj()@D@U
-def depolarizing_channel(p,dm=np.eye(2)/2):
-    d=dm.shape[0]
-    ret=np.eye(d**2)*(1-p)
-    ret+=np.outer(dm.ravel(),np.eye(d).ravel())*p
-    return ret
+# def dephasing_channel(gamma,basis=np.eye(2)):
+#     D=np.diag([1.0,1.0-gamma,1.0-gamma,1.0])
+#     U=unitary_channel(basis)
+#     return U.T.conj()@D@U
+# def depolarizing_channel(p,dm=np.eye(2)/2):
+#     d=dm.shape[0]
+#     ret=np.eye(d**2)*(1-p)
+#     ret+=np.outer(dm.ravel(),np.eye(d).ravel())*p
+#     return ret
 
-# def operator_to_state(op):
-#     L=int(np.log2(op.shape[0]))
-#     op=op.reshape((2,)*(2*L))
-#     tplist=sum(zip(range(L),range(L,2*L)),())
-#     op=op.transpose(tplist)
-#     return op.reshape(4**L)
-#
-# def state_to_operator(state):
-#     L=int(np.log2(state.shape[0])//2)
-#     state=state.reshape((2,)*(2*L))
-#     tplist=list(range(0,2*L,2))+list(range(1,2*L,2))
-#     state=state.transpose(tplist)
-#     return state.reshape(2**L,2**L)
+def vectorize_operator(op):
+    if len(op.shape)==2:
+        L=math.log2(state.shape[0])
+        op=tt.asarray(op,cluster=((2,2),)*L)
+        return tt.frommatrices([o.reshape((o.shape[0],4,o.shape[-1])) for o in op.tomatrices_unchecked()])
+    if len(op.shape)==4:
+        L=math.log2(state.shape[1])
+        op=tt.asslice(op,cluster=((2,2),)*L)
+        return tt.frommatrices_slice([o.reshape((o.shape[0],4,o.shape[-1])) for o in op.tomatrices_unchecked()])
+    else:
+        raise ValueError("Operator must either be 2d (array) or 4d (slice), but has %i dimension"%(len(op.shape)))
+
+def unvectorize_operator(state):
+    if len(state.shape)==1:
+        L=math.log2(state.shape[0])//2
+        state=tt.asarray(state,cluster=((4,),)*L)
+        return tt.frommatrices([o.reshape((o.shape[0],2,2,o.shape[-1])) for o in op.tomatrices_unchecked()])
+    if len(state.shape)==3:
+        L=math.log2(state.shape[1])//2
+        state=tt.asslice(state,cluster=((4,),)*L)
+        return tt.frommatrices_slice([o.reshape((o.shape[0],2,2,o.shape[-1])) for o in op.tomatrices_unchecked()])
+    else:
+        raise ValueError("Vectorized operator must either be 2d (array) or 4d (slice), but has %i dimension"%(len(op.shape)))
