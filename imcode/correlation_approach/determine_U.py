55c3469eb2026ddb2b4c2b4f5c977356da491ee2
Julian Thoenniss <julthoe@gmail.com>
2022-07-07 16:07:39 +0200

    update

--- imcode/correlation_approach/determine_U.py
+++ imcode/correlation_approach/determine_U.py
@@ -26,10 +26,11 @@ def determine_U(sub_corr,eigvec, lower, upper, total_dim):
     for prefac in range (2):
         for i in range (0,sub_dim - 3 - prefac, 2):
             exp_phi = 1
+            theta = 0
             if (abs(eigvec_temp[-1 - i - 2 - prefac] * eigvec_temp[-1 - i- prefac]) > 1.e-30 ):
                 exp_phi = eigvec_temp[-1 - i - 2 - prefac] / eigvec_temp[-1 - i- prefac] * abs(eigvec_temp[-1 - i- prefac] / eigvec_temp[-1 - i -2- prefac])
            
-            theta = np.arctan(abs(eigvec_temp[-1 - i- prefac] / eigvec_temp[-1 - i -2- prefac]))
+                theta = np.arctan(abs(eigvec_temp[-1 - i- prefac] / eigvec_temp[-1 - i -2- prefac]))
            
             D_phi = np.identity(4,dtype=np.complex_)
             if prefac == 0:
@@ -48,17 +49,19 @@ def determine_U(sub_corr,eigvec, lower, upper, total_dim):
 
             U_sub_new = np.bmat([[np.identity(sub_dim - 4-i), np.zeros((sub_dim - 4-i,4 + i))],[np.zeros((4,sub_dim - 4 - i)), G_theta @ D_phi, np.zeros((4, i))],[np.zeros((i, sub_dim -i)),np.identity(i)]])
             eigvec_temp = np.einsum('ij,j->i',U_sub_new,eigvec_temp)
-     
+            print(eigvec_temp)
             U_sub = U_sub_new @ U_sub
 
     #print(np.einsum('ij,j->i',U_sub,eigvec))
+    last_gate_type = 1# 1:Givens, 2:Bog
     if abs(eigvec_temp[0]) > abs(eigvec_temp[1]):#Givens like above
         print('last layer: Givens')
         exp_phi = 1
+        theta = 0
         if abs(eigvec_temp[0] * eigvec_temp[2]) > 1.e-30:
             exp_phi = eigvec_temp[0] / eigvec_temp[2] * abs(eigvec_temp[2] / eigvec_temp[0])
     
-        theta = np.arctan(abs(eigvec_temp[2] / eigvec_temp[0]))
+            theta = np.arctan(abs(eigvec_temp[2] / eigvec_temp[0]))
 
         D_phi = np.identity(4,dtype=np.complex_)
         if prefac == 0:
@@ -82,11 +85,13 @@ def determine_U(sub_corr,eigvec, lower, upper, total_dim):
 
     else:#Bogoliubov
         print('last layer: Bogo')
+        last_gate_type = 2
         exp_phi = 1
+        theta = 0
         if abs(eigvec_temp[1] * eigvec_temp[2]) > 1.e-30:
             exp_phi = eigvec_temp[1] / eigvec_temp[2] * abs(eigvec_temp[2] / eigvec_temp[1])
         
-        theta = np.arctan(- abs(eigvec_temp[2] / eigvec_temp[1]))
+            theta = np.arctan(- abs(eigvec_temp[2] / eigvec_temp[1]))
 
         D_phi = np.identity(4,dtype=np.complex_)
         if prefac == 0:
@@ -108,8 +113,8 @@ def determine_U(sub_corr,eigvec, lower, upper, total_dim):
         U_sub = U_sub_new @ U_sub
 
 
-    #print(np.einsum('ij,j->i',U_sub,eigvec))
+    print(np.einsum('ij,j->i',U_sub,eigvec))
     #print('U-befire',U_sub)
         
     U_sub_full_dim = sparse.csr_matrix(np.block([[np.identity(lower), np.zeros((lower,total_dim - lower))],[np.zeros((sub_dim,lower)), U_sub, np.zeros((sub_dim, total_dim-upper))],[np.zeros((total_dim-upper,upper)),np.identity(total_dim-upper)]]))
-    return U_sub_full_dim
+    return U_sub_full_dim, last_gate_type
