cab4f39840d075ffb2506d91e5d7de41689ddf60
Julian Thoenniss <julthoe@gmail.com>
2022-07-08 13:26:44 +0200

    add test to test channel properties

--- imcode/correlation_approach/tests.py
+++ imcode/correlation_approach/tests.py
@@ -1,4 +1,7 @@
 from correlator import correlator
+from numpy import dtype, linalg
+from scipy.linalg import expm
+import numpy as np
 #check equal-time Majorana self-correlations (correlations < (c + c^dagger)(c + c^dagger) > and <- (c - c^dagger)(c - c^dagger) > at equal times yield partition sum since (c + c^dagger)(c + c^dagger) = -(c - c^dagger)(c - c^dagger) = 1)
 
 def test_identity_correlations(A, n_expect, ntimes):
@@ -48,3 +51,55 @@ def anti_sym_check(matrix):
     
     else:
         print ('Antisymmetry test successfully passed..')
+
+
+def evolve_rho(rho,B):
+    dim_B = B.shape[0]
+    
+    #add local impurity channel
+    #measure (this imposes the IT initial state)
+    for i in range (dim_B//2-1):
+        B[2 * i + 1 ,2 * i+2 ] += 1
+        B[2 * i + 2 ,2 * i+1 ] -= 1
+
+
+    A = B[1:dim_B-1,1:dim_B-1]
+    R = B[[0,dim_B-1],1:dim_B-1]
+    C = np.zeros((2,2),dtype=np.complex_)
+    C[0,1] = B[0,dim_B-1]
+    C[1,0] = B[dim_B-1,0]
+
+    A_inv = linalg.inv(A)
+    rho_exponent_evolved = 0.5*(R @ A_inv @ R.T + C)
+    norm = np.sqrt(linalg.det(A))
+    return rho_exponent_evolved, norm
+
+def evolve_rho_T(rho,B,T,t):
+    dim_B = B.shape[0]
+    
+    #add local impurity channel
+    #measure (this imposes the IT initial state)
+    for i in range (dim_B//2-1):
+        B[2 * i + 1 ,2 * i+2 ] += 1
+        B[2 * i + 2 ,2 * i+1 ] -= 1
+
+    print(B[0:2*(T-t)+1,1 + 2*(T-t) :dim_B-1 - 2*(T-t)].shape)
+    print(B[dim_B-1 - 2*(T-t):dim_B,1 + 2*(T-t) :dim_B-1 - 2*(T-t)].shape)
+    A = B[1 + 2*(T-t):dim_B-1 - 2*(T-t),1 + 2*(T-t):dim_B-1 - 2*(T-t)]
+
+    R = np.bmat([[B[0:2*(T-t)+1,1 + 2*(T-t) :dim_B-1 - 2*(T-t)]],[B[dim_B-1 - 2*(T-t):dim_B,1 + 2*(T-t) :dim_B-1 - 2*(T-t)]]])
+
+    print(2*(T-t)+1,dim_B-(dim_B-1 - 2*(T-t)))
+   
+    C = np.bmat([[B[0:2*(T-t)+1,0:2*(T-t)+1], B[0:2*(T-t)+1,dim_B-1 - 2*(T-t):dim_B]],[B[dim_B-1 - 2*(T-t):dim_B,0:2*(T-t)+1], B[dim_B-1 - 2*(T-t):dim_B,dim_B-1 - 2*(T-t):dim_B]]])
+ 
+    print('Rshape',R.shape)
+    print('Ashape',A.shape)
+    print('Cshape',C.shape)
+    A_inv = linalg.inv(A)
+    rho_exponent_evolved = 0.5*(R @ A_inv @ R.T + C ) 
+    rho_exponent_evolved_result = np.zeros((2,2),dtype=np.complex_)
+    rho_exponent_evolved_result[0,1] = rho_exponent_evolved[2*(T-t),C.shape[0] - 1 - 2*(T-t)]
+    rho_exponent_evolved_result[1,0] = rho_exponent_evolved[C.shape[0] - 1 - 2*(T-t), 2*(T-t)]
+    norm = np.sqrt(linalg.det(A))
+    return rho_exponent_evolved_result, norm 
