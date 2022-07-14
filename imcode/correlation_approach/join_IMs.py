364046a3a9f4aff5e1800ac3b5a05a4c31e26340
Julian Thoenniss <julthoe@gmail.com>
2022-07-14 12:55:42 +0200

    update

--- imcode/correlation_approach/join_IMs.py
+++ imcode/correlation_approach/join_IMs.py
@@ -10,20 +10,27 @@ np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(linewidth=470)
 
 conv = 'J' # 'J': my convention, 'M': Michael's convention
+conv_out = 'M' # 'J': my convention, 'M': Michael's convention
 
 B_l = []
 B_r = [] 
-filename_l = '/Users/julianthoenniss/Documents/PhD/data/compmode=C_o=2_Jx=0.1_Jy=0.1_g=0.0mu=0.0_del_t=-1.0_beta=0.0_L=200_init=3'
-filename_r = '/Users/julianthoenniss/Documents/PhD/data/compmode=C_o=1_Jx=0.1_Jy=0.1_g=0.0mu=0.0_del_t=1.0_beta=0.0_L=200_init=2'
-filename = '/Users/julianthoenniss/Documents/PhD/data/interleaved_Jx=0.1_Jy=0.1_g=0.0mu=0.0_del_t=1.0_L=200InfTemp-FermiSea'
+filename_l = '/Users/julianthoenniss/Documents/PhD/data/Millis_mu=-.2_timestep=0.05_T=100_exact2_Michaels_conv'
+filename_r = '/Users/julianthoenniss/Documents/PhD/data/Millis_mu=.2_timestep=0.05_T=100_exact2_Michaels_conv'
 
-if conv == 'J':
+filename_l = '/Users/julianthoenniss/Documents/PhD/data/compmode=C_o=2_Jx=1.0_Jy=1.0_g=0.0mu=-2.5_del_t=0.1_beta=200.0_L=200_init=4'
+filename_r = '/Users/julianthoenniss/Documents/PhD/data/compmode=C_o=1_Jx=1.0_Jy=1.0_g=0.0mu=2.5_del_t=0.1_beta=200.0_L=200_init=4'
+filename = '/Users/julianthoenniss/Documents/PhD/data/XX_deltamu=5.0'
+
+if conv_out == 'J':
     filename += '_my_conv' 
-    print('using Js convention')
+    print('storing result in Js convention')
+elif conv_out == 'M':
+    filename += '_Michaels_conv' 
+    print('storing result in Ms convention')
 
-time_0 = 2
-max_time1 = 50
-max_time2 = 51
+time_0 = 1
+max_time1 = 100
+max_time2 = 101
 stepsize1 = 1
 stepsize2 = 1
 
@@ -32,25 +39,31 @@ with h5py.File(filename + ".hdf5", 'w') as f:
         dset_temp_entr = f.create_dataset('temp_entr', ((max_time1-time_0)//stepsize1 + (max_time2- max_time1)//stepsize2 + 1, max_time2 + stepsize2),dtype=np.float_)
         dset_IM_exponent = f.create_dataset('IM_exponent', (int((max_time1-time_0)/stepsize1) + (max_time2- max_time1)//stepsize2 + 1, 4 * (max_time2 + stepsize2), 4 * (max_time2 + stepsize2)),dtype=np.complex_)
 
-for iter in range (50):
-
+for iter in range (time_0,100,1):
+    #"""
     #if exponent B is read out
     with h5py.File(filename_l + '.hdf5', 'r') as f:
         times_read = f['temp_entr']
         nbr_Floquet_layers  = int(times_read[iter,0])
-        print('times: ', nbr_Floquet_layers )
+        print('times: ', nbr_Floquet_layers, ' iteration: ', iter )
+    
+    #"""
+    
+    #else:
+    #nbr_Floquet_layers = iter
 
     with h5py.File(filename_l + '.hdf5', 'r') as f:
         print(4*nbr_Floquet_layers,4*nbr_Floquet_layers)
         B_l = f['IM_exponent'][iter,:4*nbr_Floquet_layers,:4*nbr_Floquet_layers]
-
+        #print(f['IM_exponent'][iter,:4*(nbr_Floquet_layers+1),:4*(nbr_Floquet_layers+1)])
     with h5py.File(filename_r + '.hdf5', 'r') as f:
         print(4*nbr_Floquet_layers,4*nbr_Floquet_layers)
         B_r = f['IM_exponent'][iter,:4*nbr_Floquet_layers,:4*nbr_Floquet_layers]
 
     B_r_orig = B_r
     dim_B = B_l.shape[0]
-    #rotate into correct basis with input/output variables
+
+    #rotate into correct basis with input/output variables as specified for Michael
 
     S = np.zeros(B_l.shape,dtype=np.complex_)
     for i in range (dim_B//4):#order plus and minus next to each other
@@ -74,9 +87,10 @@ for iter in range (50):
         U[4*i + 2, dim_B //2 - (2*i) -2] = 1
         U[4*i + 3, dim_B //2 + (2*i) + 1] = 1
                 
+    if conv == 'J':
     #print(U.shape, rot.shape, S.shape,B_l.shape)
-    B_l = U @ rot.T @ S @ B_l @ S.T @ rot @ U.T 
-    B_r = U @ rot.T @ S @ B_r @ S.T @ rot @ U.T 
+        B_l = U @ rot.T @ S @ B_l @ S.T @ rot @ U.T 
+        B_r = U @ rot.T @ S @ B_r @ S.T @ rot @ U.T 
 
 
     #matrix containing all variables not integrated over (output left and input right)
@@ -124,21 +138,24 @@ for iter in range (50):
             B_joined_reshuf[4*i+2 : 4*i+4 , 4*j : 4*j+2] = B_joined[2*i +  dim_B//2: 2*i+2 +  dim_B//2, 2*j : 2*j+2]
 
     
-    #This rotates aways from Michael's basis into theta/zeta-basis
-    if conv == 'J':
+    
+    if conv_out == 'J':#This rotates aways from Michael's basis into theta/zeta-basis
         B_joined_reshuf = S.T @ rot @  U.T @ B_joined_reshuf @ U @ rot.T @ S
+
+
 
     
     with h5py.File(filename + '.hdf5', 'a') as f:
         IM_data = f['IM_exponent']
         IM_data[iter,:B_joined_reshuf.shape[0],:B_joined_reshuf.shape[0]] = B_joined_reshuf[:,:]
-
+    
     correlation_block = create_correlation_block(B_joined_reshuf, nbr_Floquet_layers, filename)
     time_cuts = np.arange(1, nbr_Floquet_layers)
-
-    with h5py.File(filename + '.hdf5', 'a') as f:
-        entr_data = f['temp_entr']
-        entr_data[iter,0] = nbr_Floquet_layers
-        for cut in time_cuts:
-            #print('calculating entropy at time cut:', cut)
-            entr_data[iter,cut] = float(entropy('C', correlation_block, nbr_Floquet_layers, cut, iter, filename))
+    
+    #with h5py.File(filename + '.hdf5', 'a') as f:
+    #    entr_data = f['temp_entr']
+    #    entr_data[iter,0] = nbr_Floquet_layers
+    #    for cut in time_cuts:
+    #        #print('calculating entropy at time cut:', cut)
+    #        entr_data[iter,cut] = float(entropy('C', correlation_block, nbr_Floquet_layers, cut, iter, filename))
+    
