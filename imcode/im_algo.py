brickwork runs again, wrong result though

imcode/im_algo.py
@@ -27,9 +27,10 @@ def brickwork_lcga(Ts,init=np.eye(2)/2,boundary=None,chi_max=128,cutoff=1e-12,yi
     bwobim=np.eye(4).reshape((1,16,1))
     gene=_generator_matrices(init)
     if boundary is None:
-        cmps=tt.frommatrices([zozobim]) # empty ttarrays are not allowed
+        cmps=tt.fromproduct([np.array([1.0])]) # empty ttarrays are not allowed
     else:
         cmps=boundary.copy()
+    idim=1
     for T in Ts:
         # augment
         # contract with initial
@@ -37,13 +38,15 @@ def brickwork_lcga(Ts,init=np.eye(2)/2,boundary=None,chi_max=128,cutoff=1e-12,yi
             init=np.array(tt.frommatrices_slice([next(gene) for _ in range(int(math.log2(T.shape[0]))//2)]))
             init=init.reshape((1,init.shape[0],T.shape[0],init.shape[-1])).transpose([0,3,1,2])
             T=tt.frommatrices([init]+T.tomatrices_unchecked())
+            idim=init.shape[1]
         else:
-            T=T.frommatrices(T.tomatrices_unchecked())
-        tdim=int(math.log2(T.shape[1]//init.shape[2]))//4 #math not numpy since the dimension can be quite large
-        cdim=int(math.log2(cmps.shape[0]//init.shape[2]))//4 #math not numpy since the dimension can be quite large
+            T=tt.frommatrices([np.eye(idim)[None,...,None]]+T.tomatrices_unchecked())
+        tdim=int(math.log2(T.shape[1]//T.cluster[0][1]))//4 #math not numpy since the dimension can be quite large
+        cdim=int(math.log2(cmps.shape[0]//idim))//4 #math not numpy since the dimension can be quite large
         if tdim>cdim:
             cmps=tt.frommatrices(cmps.tomatrices_unchecked()+[bwobim for _ in range(tdim-cdim)])
         # apply
+
         cmps=T@cmps
         # truncate
         cmps.truncate(chi_max=chi_max,cutoff=cutoff)
@@ -59,7 +62,7 @@ def zoz_lcga(Ts,init=np.eye(2)/2,boundary=None,chi_max=128,cutoff=1e-12,yieldcop
     zozobim=np.array([1,0,0,1])[None,...,None]
     gene=_generator_matrices(init)
     if boundary is None:
-        cmps=tt.frommatrices([zozobim]) # empty ttarrays are not allowed
+        cmps=tt.fromproduct([np.array([1.0])]) # empty ttarrays are not allowed
     else:
         cmps=boundary.copy()
     for T in Ts:

tests/heisenberg/test_heisenberg_model.py
@@ -4,6 +4,12 @@ import ttarray as tt
 import numpy as np
 from imcode import SZ,SX,ID,ZE
 from .. import check_model
+def test_simple(seed_rng):
+    Te=imcode.heisenberg_Te(1,0,0,0)
+    To=imcode.heisenberg_To(1,0,0,0)
+    print(Te,To)
+    print(list(imcode.brickwork_lcga([Te,To])))
+# @pytest.mark.skip
 def test_product_homhom(seed_rng):
     L=7
     t=6
@@ -22,7 +28,7 @@ def test_product_homhom(seed_rng):
     ch1=np.array(imcode.unitary_channel(imcode.heisenberg_F(1,Jx,Jy,Jz,hx,hy,hz)))
     ch2=np.array(imcode.unitary_channel(imcode.heisenberg_F(2,Jx,Jy,Jz,hx,hy,hz)))
     #rectangle
-    check_model(L,t,init,Fs,Ts,Ts,imcode.brickwork_lcga,ch1,ch2,ch1,ch2,imcode.brickwork_boundary_evolution,imcode.brickwork_embedded_evolution)
+    check_model(L,t,init,Fs,Ts,Ts,imcode.brickwork_lcga,ch1,ch2,ch1,ch2,imcode.brickwork_boundary_evolution,imcode.brickwork_embedded_evolution,np.eye(4).ravel())
     #lcga
     # Ts=[imcode.heisenberg_T(t,J,g,h) for t in range(1,t+1)]+[T]*(L-t)
     # check_model(L,t,init,Fs,Ts,Ts,imcode.brickwork_lcga,ch1,ch2,ch1,ch2,imcode.brickwork_boundary_evolution,imcode.brickwork_embedded_evolution)

tests/helper.py
@@ -1,9 +1,10 @@
 import pytest
+import math
 import ttarray as tt
 import numpy as np
 def calc_rdm(inds,dm):
     return np.array(tt.frommatrices([x if i in inds else np.trace(x,axis1=1,axis2=2).reshape((x.shape[0],1,1,x.shape[-1])) for i,x in enumerate(dm.M)]))
-def check_model(L,t,init,Fs,Tsl,Tsr,lcga_fun,chl,chr2,che,che2,boundary_obs_fun,embedded_obs_fun):
+def check_model(L,t,init,Fs,Tsl,Tsr,lcga_fun,chl,chr2,che,che2,boundary_obs_fun,embedded_obs_fun,obc):
     #tebd dm
     mzzl,mzzr2,mzze,mzze2=[],[],[],[]
     minit=init
@@ -14,9 +15,9 @@ def check_model(L,t,init,Fs,Tsl,Tsr,lcga_fun,chl,chr2,che,che2,boundary_obs_fun,
         mzze2.append(calc_rdm([L//2,L//2+1],minit))
         minit=(F@minit@F.T.conj())
         minit.truncate(chi_max=64)
-    iml=list(lcga_fun(Tsl,init,boundary=tt.fromproduct([np.array([1,1,1,1])]*Tsl[0].L),chi_max=64))
+    iml=list(lcga_fun(Tsl,init,boundary=tt.fromproduct([obc]*int(math.log2(Tsl[0].shape[1])/math.log2(obc.shape[0]))),chi_max=64))
     revinit=tt.frommatrices([i.transpose([3,1,2,0]) for i in init.tomatrices()[::-1]])
-    imr=list(lcga_fun(Tsr,revinit,boundary=tt.fromproduct([np.array([1,1,1,1])]*Tsr[0].L),chi_max=64))
+    imr=list(lcga_fun(Tsr,revinit,boundary=tt.fromproduct([obc]*int(math.log2(Tsl[0].shape[1])/math.log2(obc.shape[0]))),chi_max=64))
     if init.M[0].shape[0]==1 and init.M[0].shape[-1]==1:
         izzl=list(boundary_obs_fun(imr[L-2],chl,init=init.M[0][0,...,0]))
     else:

tests/ising/test_ising_model.py
@@ -131,9 +131,9 @@ def test_mps_hethet():
     che=[np.array(imcode.unitary_channel(imcode.ising_F(1,0,g[L//2,i],h[L//2,i]))) for i in range(t)]
     che2=[np.array(imcode.unitary_channel(imcode.ising_F(2,J[L//2,i],g[L//2:L//2+2,i],h[L//2:L//2+2,i]))) for i in range(t)]
     #rectangle
-    check_model(L,t,init,Fs,Tsl,Tsr,imcode.zoz_lcga,chl,chr2,che,che2,imcode.ising_boundary_evolution,imcode.ising_embedded_evolution)
+    check_model(L,t,init,Fs,Tsl,Tsr,imcode.zoz_lcga,chl,chr2,che,che2,imcode.ising_boundary_evolution,imcode.ising_embedded_evolution,np.array([1,1,1,1]))
     #lcga
     Tsl=[imcode.ising_T(min(i+1,t),J[i],g[i],h[i]) for i in range(L-1)]
     print([T.shape for T in Tsl])
     Tsr=[imcode.ising_T(min(L-i,t),J[i-1],g[i],h[i]) for i in range(L-1,0,-1)]
-    check_model(L,t,init,Fs,Tsl,Tsr,imcode.zoz_lcga,chl,chr2,che,che2,imcode.ising_boundary_evolution,imcode.ising_embedded_evolution)
+    check_model(L,t,init,Fs,Tsl,Tsr,imcode.zoz_lcga,chl,chr2,che,che2,imcode.ising_boundary_evolution,imcode.ising_embedded_evolution,np.array([1,1,1,1]))