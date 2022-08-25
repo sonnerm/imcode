30a9f3d97f5630ef29fb052c959d87fb161bd36e
Michael Sonner <sonnerm@gmail.com>
2022-08-25 22:10:49 +0200

    use optimzed einsum

--- imcode/obs.py
+++ imcode/obs.py
@@ -10,14 +10,14 @@ def ising_tracevalues(im):
     im=im.tomatrices_unchecked()[1:]
     postval=[np.array([1.0])]
     for m in im[::-1]:
-        postval.append(np.einsum("c,abc,b->a",postval[-1],m,[0.5,0.0,0.0,0.5]))
+        postval.append(np.einsum("c,abc,b->a",postval[-1],m,[0.5,0.0,0.0,0.5],optimize=True))
     return postval[::-1]
 
 def brickwork_tracevalues(im):
     im=im.tomatrices_unchecked()[1:]
     postval=[np.array([1.0])]
     for m in im[::-1]:
-        postval.append(np.einsum("d,abcd,b,c->a",postval[-1],m.reshape((m.shape[0],4,4,m.shape[-1])),[0.5,0,0,0.5],[1,0,0,1]))
+        postval.append(np.einsum("d,abcd,b,c->a",postval[-1],m.reshape((m.shape[0],4,4,m.shape[-1])),[0.5,0,0,0.5],[1,0,0,1],optimize=True))
     return postval[::-1]
 
 def brickwork_boundary_evolution(im,chs,init=np.eye(2)/2):
@@ -34,17 +34,17 @@ def brickwork_boundary_evolution(im,chs,init=np.eye(2)/2):
     im.recluster(((init.shape[0],),)+((16,),)*int(math.log2(im.shape[0]//init.shape[0])/4))
     dm=np.tensordot(im.M[0],vectorize_operator(init),axes=((1,),(0,)))[0,:,:,0]
     pvals=brickwork_tracevalues(im)
-    yield unvectorize_operator(np.einsum("ba,b->a",dm,pvals[0]))
+    yield unvectorize_operator(np.einsum("ba,b->a",dm,pvals[0],optimize=True))
     imm=im.tomatrices_unchecked()[1:]
     if isinstance(chs,np.ndarray) and len(chs.shape)==2:
         chs=[chs for _ in range(im.L)]
     for ch,m,pva,pvb in zip(chs,imm,pvals[:-1],pvals[1:]):
-        dm=np.einsum("ba,ca->bc",dm,ch)
-        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pva)))
+        dm=np.einsum("ba,ca->bc",dm,ch,optimize=True)
+        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pva,optimize=True)))
         dm=dm.reshape((dm.shape[0],4,dm.shape[1]//4))
-        dm=np.einsum("bax,bacd->dcx",dm,m.reshape((m.shape[0],4,4,m.shape[-1])))
+        dm=np.einsum("bax,bacd->dcx",dm,m.reshape((m.shape[0],4,4,m.shape[-1])),optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[2]*4))
-        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pvb)))
+        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pvb,optimize=True)))
 
 
 def ising_boundary_evolution(im,chs,init=np.eye(2)/2):
@@ -61,17 +61,17 @@ def ising_boundary_evolution(im,chs,init=np.eye(2)/2):
     im.recluster(((init.shape[0],),)+((4,),)*int(math.log2(im.shape[0]//init.shape[0])/2))
     dm=np.tensordot(im.M[0],vectorize_operator(init),axes=((1,),(0,)))[0,:,:,0]
     pvals=ising_tracevalues(im)
-    yield unvectorize_operator(np.einsum("ba,b->a",dm,pvals[0]))
+    yield unvectorize_operator(np.einsum("ba,b->a",dm,pvals[0],optimize=True))
     imm=im.tomatrices_unchecked()[1:]
     if isinstance(chs,np.ndarray) and len(chs.shape)==2:
         chs=[chs for _ in range(im.L)]
     for ch,m,pva,pvb in zip(chs,imm,pvals[:-1],pvals[1:]):
-        dm=np.einsum("ba,ca->bc",dm,ch)
-        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pva)))
+        dm=np.einsum("ba,ca->bc",dm,ch,optimize=True)
+        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pva,optimize=True)))
         dm=dm.reshape((dm.shape[0],4,dm.shape[1]//4))
-        dm=np.einsum("bax,bad->dax",dm,m)
+        dm=np.einsum("bax,bad->dax",dm,m,optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[2]*4))
-        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pvb)))
+        yield np.array(unvectorize_operator(np.einsum("ba,b->a",dm,pvb,optimize=True)))
 
 def zoz_boundary_evolution(im,ozs,init=np.eye(2)/2):
     dm=np.array(vectorize_operator(init))[:,None]
@@ -82,8 +82,8 @@ def zoz_boundary_evolution(im,ozs,init=np.eye(2)/2):
     if isinstance(ozs,np.ndarray) and len(chs.shape)==2:
         chs=[ozs for _ in range(im.L)]
     for ch,m,pv in zip(chs,imm,pvals[1:]):
-        dm=np.einsum("ab,bcd,eac->ed",dm,m,ch)
-        yield np.array(unvectorize_operator(np.einsum("ab,b->a",dm,pv)))
+        dm=np.einsum("ab,bcd,eac->ed",dm,m,ch,optimize=True)
+        yield np.array(unvectorize_operator(np.einsum("ab,b->a",dm,pv,optimize=True)))
 
 def brickwork_embedded_evolution(iml,chs,imr,init=np.eye(2)/2):
     '''
@@ -102,23 +102,23 @@ def brickwork_embedded_evolution(iml,chs,imr,init=np.eye(2)/2):
         raise ValueError("Right Influence matrix length %i must be compatible with left index of init %i!"%(imr.shape[0],init.shape[3]))
     iml.recluster(((init.shape[0],),)+((16,),)*int(math.log2(iml.shape[0]//init.shape[0])/4))
     imr.recluster(((init.shape[-1],),)+((16,),)*int(math.log2(imr.shape[0]//init.shape[-1])/4))
-    dm=np.einsum("abc,def,bge->cgf",iml.M[0],imr.M[0],vectorize_operator(init))
+    dm=np.einsum("abc,def,bge->cgf",iml.M[0],imr.M[0],vectorize_operator(init),optimize=True)
     pvalsl=brickwork_tracevalues(iml)
     pvalsr=brickwork_tracevalues(imr)
-    yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvalsl[0],pvalsr[0]))
+    yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvalsl[0],pvalsr[0],optimize=True))
     imml=iml.tomatrices_unchecked()[1:]
     immr=imr.tomatrices_unchecked()[1:]
     if isinstance(chs,np.ndarray) and len(chs.shape)==2:
         chs=[chs for _ in range(iml.L)]
     for ch,ml,mr,pvla,pvra,pvlb,pvrb in zip(chs,imml,immr,pvalsl[:-1],pvalsr[:-1],pvalsl[1:],pvalsr[1:]):
-        dm=np.einsum("bac,da->bdc",dm,ch)
-        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvla,pvra))
+        dm=np.einsum("bac,da->bdc",dm,ch,optimize=True)
+        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvla,pvra,optimize=True))
         dm=dm.reshape((dm.shape[0],4,dm.shape[1]//4,dm.shape[2]))
-        dm=np.einsum("baxc,bane->enxc",dm,ml.reshape((ml.shape[0],4,4,ml.shape[-1])))
+        dm=np.einsum("baxc,bane->enxc",dm,ml.reshape((ml.shape[0],4,4,ml.shape[-1])),optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[2],4,dm.shape[3]))
-        dm=np.einsum("bxac,cane->bxne",dm,mr.reshape((mr.shape[0],4,4,mr.shape[-1])))
+        dm=np.einsum("bxac,cane->bxne",dm,mr.reshape((mr.shape[0],4,4,mr.shape[-1])),optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[1]*4,dm.shape[3]))
-        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvlb,pvrb))
+        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvlb,pvrb,optimize=True))
 
 def ising_embedded_evolution(iml,chs,imr,init=np.eye(2)/2):
     '''
@@ -137,23 +137,23 @@ def ising_embedded_evolution(iml,chs,imr,init=np.eye(2)/2):
         raise ValueError("Right Influence matrix length %i must be compatible with left index of init %i!"%(imr.shape[0],init.shape[3]))
     iml.recluster(((init.shape[0],),)+((4,),)*int(math.log2(iml.shape[0]//init.shape[0])/2))
     imr.recluster(((init.shape[-1],),)+((4,),)*int(math.log2(imr.shape[0]//init.shape[-1])/2))
-    dm=np.einsum("abc,def,bge->cgf",iml.M[0],imr.M[0],vectorize_operator(init))
+    dm=np.einsum("abc,def,bge->cgf",iml.M[0],imr.M[0],vectorize_operator(init),optimize=True)
     pvalsl=ising_tracevalues(iml)
     pvalsr=ising_tracevalues(imr)
-    yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvalsl[0],pvalsr[0]))
+    yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvalsl[0],pvalsr[0]),optimize=True)
     imml=iml.tomatrices_unchecked()[1:]
     immr=imr.tomatrices_unchecked()[1:]
     if isinstance(chs,np.ndarray) and len(chs.shape)==2:
         chs=[chs for _ in range(iml.L)]
     for ch,ml,mr,pvla,pvra,pvlb,pvrb in zip(chs,imml,immr,pvalsl[:-1],pvalsr[:-1],pvalsl[1:],pvalsr[1:]):
-        dm=np.einsum("bac,da->bdc",dm,ch)
+        dm=np.einsum("bac,da->bdc",dm,ch,optimize=True)
         yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvla,pvra))
         dm=dm.reshape((dm.shape[0],4,dm.shape[1]//4,dm.shape[2]))
-        dm=np.einsum("baxc,bae->eaxc",dm,ml)
+        dm=np.einsum("baxc,bae->eaxc",dm,ml,optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[2],4,dm.shape[3]))
-        dm=np.einsum("bxac,cae->bxae",dm,mr)
+        dm=np.einsum("bxac,cae->bxae",dm,mr,optimize=True)
         dm=dm.reshape((dm.shape[0],dm.shape[1]*4,dm.shape[3]))
-        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvlb,pvrb))
+        yield unvectorize_operator(np.einsum("bac,b,c->a",dm,pvlb,pvrb,optimize=True))
 
 def zoz_embedded_evolution(iml,zozs,imr,init=np.eye(2)/2):
     dm=np.array(vectorize_operator(init))[:,None,None]
@@ -167,5 +167,5 @@ def zoz_embedded_evolution(iml,zozs,imr,init=np.eye(2)/2):
     if isinstance(zozs,np.ndarray) and len(zozs.shape)==4:
         zozs=[zozs for _ in range(iml.L)]
     for ch,ml,mr,pvl,pvr in zip(zozs,imml,immr,pvalsl[1:],pvalsr[1:]):
-        dm=np.einsum("abc,bae,cae,->aec",dm,ml,mr,ch)
-        yield np.array(unvectorize_operator(np.einsum("abc,b,c->a",dm,pvl,pvr)))
+        dm=np.einsum("abc,bae,cae,->aec",dm,ml,mr,ch,optimize=True)
+        yield np.array(unvectorize_operator(np.einsum("abc,b,c->a",dm,pvl,pvr,optimize=True)))
