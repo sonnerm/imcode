e38b24d3ef42d62b12a0a27076dd71a9b26817f2
Julian Thoenniss <julthoe@gmail.com>
2023-01-17 18:16:08 +0100

    introduced substeps in IF to compute exact noniteracting propagator

--- imcode/correlation_approach/imag_time_IM.py
+++ imcode/correlation_approach/imag_time_IM.py
@@ -5,7 +5,7 @@ import pandas as pd
 import scipy.integrate as integrate
 from pfapack import pfaffian as pf
 from imag_time_IM_funcs import g_lesser, g_greater, spec_dens, create_correlation_matrix
-
+np.set_printoptions(suppress=False, linewidth=np.nan)
 
 #___________Set parameters_______________________________________________________________________________________
 global_gamma = 1.#global energyscale -> set to 1 
@@ -18,8 +18,11 @@ t = 0
 mu_up =0.
 mu_down =0.
 
-dim_B = 500# Size of exponent matrix in the IF. The variable dim_B is equal to 2*nbr_Floquet_layers
-delta_t = beta / (dim_B/2)#timestep
+nbr_steps = 5
+T_ren = 50#specify how many "substeps" should be included in one time-step of the environment evolution. This is only meaningful for method "a", i.e. for successive environment-impurity evolution
+dim_B = 2 * nbr_steps # Size of exponent matrix in the IF. The variable dim_B is equal to 2*nbr_Floquet_layers
+dim_B_temp = dim_B * T_ren
+delta_t = beta / (dim_B_temp/2 )#timestep
 
 #for spec-dens matrix (written in convention of the "guide")
 int_lim_low = -1.#integration limits for spectral density
@@ -28,14 +31,17 @@ int_lim_up = 1.
 #trotter convention:
 trotter_convention = 'a' #choose between 'a' (for successive evolution of bath and impurity, see DMFT-guide) or 'b' for the simultaneous impurity-bath evolution
 
-alpha = 0.5 #set this to a values in the interval [0,1]
+alpha = 1 #set this to a values in the interval [0,1]
 if trotter_convention == 'b':#for simultaneous evolution, set alpha = 1
     alpha = 1.
+    T_ren = 1
+    dim_B_temp = dim_B
+    delta_t = beta / (dim_B_temp/2 )#timestep
 
 ###################COMPUTE INFLUENCE FUNCTIONAL AND ITS CORRELATION MATRIX########################################
 
 #________________Compute the matrix B_spec_dens which defines the Influence functional (this corresponds to the equations in this DMFT guide)_____________________________________________________
-B_spec_dens = np.zeros((dim_B,dim_B),dtype=np.complex_)
+B_spec_dens = np.zeros((dim_B_temp,dim_B_temp),dtype=np.complex_)
 for m in range (B_spec_dens.shape[0]//2):
     tau = m * delta_t
     for n in range (m+1,B_spec_dens.shape[0]//2):
@@ -47,7 +53,40 @@ for m in range (B_spec_dens.shape[0]//2):
     
 B_spec_dens -= B_spec_dens.T#antisymmetrize. This is twice the exponent matrix (this is assumed for later part in the code)
 
-if trotter_convention == 'b':#for simultaneous impurity-bath evolution. Note that the input here is the matrix B_specdens, computed with successive-application-trotter-scheme  
+if trotter_convention == 'a' and T_ren > 1: 
+    #add intermediate integration measure to integrate out internal legs
+    for i in range (dim_B//2 ):
+        for j in range (T_ren-1):
+            B_spec_dens[2*i*T_ren + 1 + 2*j,2*i*T_ren+2+ 2*j] += -1  
+            B_spec_dens[2*i*T_ren+2+ 2*j,2*i*T_ren + 1 + 2*j] += 1  
+
+    #select submatrix that contains all intermediate times that are integrated out
+    B_spec_dens_sub =  np.zeros((dim_B_temp - dim_B, dim_B_temp - dim_B),dtype=np.complex_)
+    for i in range (dim_B//2 ):
+        for j in range (dim_B//2 ):
+            B_spec_dens_sub[i*(2*T_ren-2):i*(2*T_ren-2 )+2*T_ren-2,j*(2*T_ren-2):j*(2*T_ren-2 )+2*T_ren-2] = B_spec_dens[2*i*T_ren+1:2*(i*T_ren + T_ren)-1,2*j*T_ren+1:2*(j*T_ren + T_ren)-1]
+
+    #matrix coupling external legs to integrated (internal) legs
+    B_spec_dens_coupl =  np.zeros((dim_B_temp - dim_B,dim_B),dtype=np.complex_)
+    for i in range (dim_B//2 ):
+        for j in range (dim_B//2 ):
+            B_spec_dens_coupl[i*(2*T_ren-2):i*(2*T_ren-2 )+2*T_ren-2,2*j] = B_spec_dens[2*i*T_ren+1:2*(i*T_ren + T_ren)-1,2*j*T_ren]
+            B_spec_dens_coupl[i*(2*T_ren-2):i*(2*T_ren-2 )+2*T_ren-2,2*j+1] = B_spec_dens[2*i*T_ren+1:2*(i*T_ren + T_ren)-1,2*(j+1)*T_ren-1]
+
+    #part of matriy that is neither integrated nor coupled to integrated variables
+    B_spec_dens_ext = np.zeros((dim_B,dim_B),dtype=np.complex_)
+    for i in range (dim_B//2 ):
+        for j in range (dim_B//2 ):
+            B_spec_dens_ext[2*i,2*j] = B_spec_dens[2*i*T_ren,2*j*T_ren]
+            B_spec_dens_ext[2*i+1,2*j] = B_spec_dens[2*(i+1)*T_ren-1,2*j*T_ren]
+            B_spec_dens_ext[2*i,2*j+1] = B_spec_dens[2*i*T_ren,2*(j+1)*T_ren-1]
+            B_spec_dens_ext[2*i+1,2*j+1] = B_spec_dens[2*(i+1)*T_ren-1,2*(j+1)*T_ren-1]
+
+    B_spec_dens = B_spec_dens_ext + B_spec_dens_coupl.T @ linalg.inv(B_spec_dens_sub) @ B_spec_dens_coupl
+
+
+
+elif trotter_convention == 'b':#for simultaneous impurity-bath evolution. Note that the input here is the matrix B_specdens, computed with successive-application-trotter-scheme  
     #remove overlap, it will be included in the impurity gates
     for m in range(B_spec_dens.shape[0]//2):
         B_spec_dens[2*m, 2*m + 1] -= - 1. #constant from overlap at m=n
@@ -168,26 +207,32 @@ exponent_inv = linalg.inv(exponent)#this is the matrix whose elements yield the 
 
 #________store propagator in hdf5 format_________
 filename = '/Users/julianthoenniss/Documents/PhD/data/B_imag_specdens'
-with h5py.File(filename + "_propag_GM_dim_B={}_trotter_{}.hdf5".format(dim_B,trotter_convention), 'w') as f:
+with h5py.File(filename + "_propag_GM_steps={}_trotter_{}_T_ren={}.hdf5".format(nbr_steps,trotter_convention,int(T_ren)), 'w') as f:
     #store exponent for benchmark 
     dset_B = f.create_dataset('B', ((dim_B,dim_B)),dtype=np.complex_)
     dset_B[:,:] = B_spec_dens[:,:] #store the matrix B in the convention that is given in the DMFT guide (with the first leg moved to the last position for case of succesive evolution of impurity and bath)
 
     #store propagators
-    dset_propag = f.create_dataset('propag', ((2,dim_B//2)),dtype=np.complex_)
+    dset_propag = f.create_dataset('propag', ((2,dim_B//2+1)),dtype=np.complex_)
+    dset_times = f.create_dataset('times', ((dim_B//2+1,)),dtype=np.float_)
 
     for tau in range (0,dim_B//2):
+        dset_times[tau] = delta_t * T_ren * tau
         if trotter_convention == 'a':#for successive impurity-bath evolution
             #spin up
             dset_propag[0,tau] = pf.pfaffian(np.array(pd.DataFrame(exponent_inv.T).iloc[[0,3*dim_B -1 -2*tau], [0,3*dim_B -1 -2*tau]]))
             #spin down
             dset_propag[1,tau] = pf.pfaffian(np.array(pd.DataFrame(exponent_inv.T).iloc[[2*dim_B -1 -2*tau,3*dim_B], [2*dim_B -1 -2*tau,3*dim_B]]))
+
         elif trotter_convention == 'b':#for simultaneous impurity-bath evolution
             #spin up
             dset_propag[0,tau] = pf.pfaffian(np.array(pd.DataFrame(exponent_inv.T).iloc[[0,dim_B -1 -2*tau], [0,dim_B -1 -2*tau]]))
             #spin down
             dset_propag[1,tau] = pf.pfaffian(np.array(pd.DataFrame(exponent_inv.T).iloc[[3*dim_B,4*dim_B-1-2*tau], [3*dim_B,4*dim_B-1-2*tau]]))
-
+    
+    dset_times[dim_B//2] = delta_t * T_ren * dim_B//2
+    dset_propag[0,dim_B//2] = 1 - dset_propag[0,0]
+    dset_propag[1,dim_B//2] = 1 - dset_propag[1,0]
             #for mode 'c', test if propagator directly computed from matrix B gives the same as overlap if impurity is set to zero. For this comment out the elimination of overlap-contants
             #dset_propag[1,tau] = np.sqrt(linalg.det(np.array(pd.DataFrame(B_spec_dens.T).iloc[[2*tau,dim_B-1], [2*tau,dim_B-1]])))
 
